68b949ff533a2034836a866a75846c882d76cd01
julienq <julienq@f04d5631-4067-4348-862e-39f3aacb62c1>
2006-05-01 09:13:18 +0000

    Editing title of NCX

--- trunk/Obi/Obi/ObiForm.Designer.cs
+++ trunk/Obi/Obi/ObiForm.Designer.cs
@@ -34,8 +34,11 @@ namespace Obi
             this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.mUndoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.mRedoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.mStrip = new Obi.UserControls.ContainerStrip();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.metadataPanel1 = new Obi.UserControls.MetadataPanel();
             this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -45,7 +48,7 @@ namespace Obi
             this.editToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(292, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(775, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -91,26 +94,49 @@ namespace Obi
             this.mRedoToolStripMenuItem.Text = "&Redo";
             this.mRedoToolStripMenuItem.Click += new System.EventHandler(this.mRedoToolStripMenuItem_Click);
             // 
-            // mStrip
+            // statusStrip1
             // 
-            this.mStrip.Location = new System.Drawing.Point(12, 27);
-            this.mStrip.MinimumSize = new System.Drawing.Size(22, 0);
-            this.mStrip.Name = "mStrip";
-            this.mStrip.Size = new System.Drawing.Size(141, 75);
-            this.mStrip.TabIndex = 2;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 480);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.ManagerRenderMode;
+            this.statusStrip1.Size = new System.Drawing.Size(775, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(37, 17);
+            this.toolStripStatusLabel1.Text = "Ready";
+            // 
+            // metadataPanel1
+            // 
+            this.metadataPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.metadataPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.metadataPanel1.Location = new System.Drawing.Point(0, 24);
+            this.metadataPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.metadataPanel1.Name = "metadataPanel1";
+            this.metadataPanel1.Size = new System.Drawing.Size(775, 60);
+            this.metadataPanel1.TabIndex = 4;
             // 
             // ObiForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 266);
-            this.Controls.Add(this.mStrip);
+            this.ClientSize = new System.Drawing.Size(775, 502);
+            this.Controls.Add(this.metadataPanel1);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "ObiForm";
             this.Text = "Obi";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -124,7 +150,9 @@ namespace Obi
         private System.Windows.Forms.ToolStripMenuItem mUndoToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem mRedoToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
-        private Obi.UserControls.ContainerStrip mStrip;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private Obi.UserControls.MetadataPanel metadataPanel1;
 
 
     }
