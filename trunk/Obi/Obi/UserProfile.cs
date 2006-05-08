f8b229e86f486546c775a7cfaf7b451d8dd8192c
julienq <julienq@f04d5631-4067-4348-862e-39f3aacb62c1>
2006-05-08 17:28:59 +0000

    * Made some small changes in prototypes for delivery. * updated IAudioRecorder interface

---
+++ trunk/Obi/Obi/UserProfile.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Obi
+{
+    public class UserProfile
+    {
+        private string mName;
+        private string mOrganization;
+        private CultureInfo mLanguage;
+        private string mIdTemplate;
+    }
+}
--- trunk/UrakawaApplicationBackend/IAudioRecorder.cs
+++ trunk/UrakawaApplicationBackend/IAudioRecorder.cs
@@ -6,39 +6,104 @@ using Microsoft.DirectX.DirectSound;
 
 namespace UrakawaApplicationBackend
 {
+	/// <summary>
+	/// The three states of the audio recorder.
+	/// Initializing: the recorder is not yet ready to record.
+	/// Idle: the recorder is ready to record.
+	/// Recording: sound is currently being recorded.
+	/// </summary>
+	public enum AudioRecorderState { Initializing, Idle, Recording };
+
 	public interface IAudioRecorder
 	{
+		/// <summary>
+		/// Get and set the current input device.
+		/// </summary>
+		// I guess that Device is the actual audio device class
+		// set may not be enough, so be free to replace it with SetInputDevice(...) if necessary.
+		Device InputDevice
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Get and set the number of channels (mono = 1, stereo = 2) for recording.
+		/// Throw an exception in case of illegal value.
+		/// </summary>
+		int Channels
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Get and set the sample rate for recording.
+		/// Throw an exception in case of illegal value.
+		/// </summary>
+		int SampleRate
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Get and set the bit depth (8 or 16) for recording.
+		/// Throw an exception in case of illegal value.
+		/// </summary>
+		int BitDepth
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Get the current recorder state (initializing, recording, or idle.)
+		/// </summary>
+		AudioRecorderState State
+		{
+			get;
+		}
+
 		// get the capture devices 
-		// this will return the capture devices list 
-		ArrayList GetInputDevice();
+		// this will return the capture devices list
+		// fixed a typo in the name
+		ArrayList GetInputDevices();
 		
 		// return the capture device guid for a selected device
-		Guid SetInputDeviceGuid();
+		// should it be in the interface?
+		// Guid SetInputDeviceGuid();
 
 		// get the wave formats based on the sample rate and bit depth
 		// pass the index  and ref WaveFormat as the parameters
 		//allowed values: 1 and 2 for the channels
 		//allowed values for the sample rates: 11025, 22050, 44100
 		//allowed value for the bit depth are 8 and 16
-		void GetWaveFormatFromIndex(int Index, ref WaveFormat format);
+		// should it be in the interface?
+		// void GetWaveFormatFromIndex(int Index, ref WaveFormat format);
 
 		// returns the array list of the formats based on the sample rate, channel  and the bit depth
-		ArrayList GetFormatList();
-
+		// should it be in the interface?
+		// ArrayList GetFormatList();
 		
-		//throws AudioRecorderException 
-		//		void record(IAudioMediaAsset wave);
-		//		void stopRecording();
-		//void setAudioRecorderListener(eventListeners.IAudioRecorderEventListener listener);
+		/// <summary>
+		/// Start audio recording. Record to a given asset; if it contains data already, the new data is appended.
+		/// Throws an exception if the device is unset, or if the asset has the wrong format (no format conversion is done.)
+		/// The state after Record() is Recording if succesful, Idle or Initializing otherwise.
+		/// </summary>
+		/// <param name="asset">The asset in which the audio is recorded.</param>
+		void Record(IAudioMediaAsset asset);
 
+		/// <summary>
+		/// Stop recording. The state after Stop() is Idle. If the recorder was not recording, there is no effect.
+		/// </summary>
+		void Stop();
+
+		// the event stuff should be unnecessary since we use .Net's native events.
+		//void setAudioRecorderListener(eventListeners.IAudioRecorderEventListener listener);
 		//return value should be of type Collection<IAudioRecorderListener>
 		//ArrayList getAudioRecorderListeners();
-
 		//eventListeners.IAudioPlayerEventListener getAudioRecorderListener(int i);        
-
-		//returns states such as recording, idle, initializing
-		//the return value type will be something like AudioRecorderStateType instead of Object
-		//		Object getState();
 	}
 }
 	
