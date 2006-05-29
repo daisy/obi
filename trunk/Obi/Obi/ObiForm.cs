4160a16d0443cfc3e29373e9a185368dcc63751b
julienq <julienq@f04d5631-4067-4348-862e-39f3aacb62c1>
2006-05-29 06:10:56 +0000

    Added events to Obi

--- trunk/Obi/Obi/ObiForm.cs
+++ trunk/Obi/Obi/ObiForm.cs
@@ -12,6 +12,28 @@ namespace Obi
 {
     public partial class ObiForm : Form
     {
+        private Project mProject;          // the project currently being authored
+        private UndoRedoStack mUndoStack;  // the undo stack for this project [should belong to the project; saved together]
+
+        private event Events.Project.StateChangedHandler ProjectStateChanged;
+
+        /// <summary>
+        /// Initialize a new form. No project is opened at creation time.
+        /// </summary>
+        public ObiForm()
+        {
+            InitializeComponent();
+            
+            mProject = null;
+            ProjectStateChanged += new Events.Project.StateChangedHandler(mProject_StateChanged);
+            GUIUpdateNoProject();
+
+            mUndoStack = new UndoRedoStack();
+            undo_label = mUndoToolStripMenuItem.Text;
+            redo_label = mRedoToolStripMenuItem.Text;
+            UndoStackChanged += new UndoStackChangedHandler(mUndoStack_UndoStackChanged);
+        }
+
         /// <summary>
         /// Create a new project if the correct one was closed properly.
         /// </summary>
@@ -21,11 +43,12 @@ namespace Obi
         {
             if (ClosedProject())
             {
-                ProjectManager.Instance.CreateProject();
+                mProject = new Project();
+                OnProjectCreated();
             }
             else
             {
-                Console.WriteLine(Localizer.Message("cancelled") + ".");
+                Ready();
             }
         }
 
@@ -43,11 +66,25 @@ namespace Obi
                 DialogResult result = dialog.ShowDialog();
                 if (result == DialogResult.OK)
                 {
-                    ProjectManager.Instance.OpenXUK(dialog.FileName);
+                    mProject = new Project(dialog.FileName);
+                    OnProjectOpened();
                     return;
                 }
             }
-            Console.WriteLine(Localizer.Message("cancelled") + ".");    
+            Ready();
+        }
+
+        /// <summary>
+        /// Close the current project.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void closeProjectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (ClosedProject())
+            {
+                mProject = null;
+            }
         }
 
         /// <summary>
@@ -63,7 +100,7 @@ namespace Obi
             }
             else
             {
-                Console.WriteLine(Localizer.Message("cancelled") + ".");
+                Ready();
             }
         }
 
@@ -73,43 +110,161 @@ namespace Obi
         /// <returns>True if there is no open project or the currently open project could be closed.</returns>
         private bool ClosedProject()
         {
-            if (ProjectManager.Instance.Unsaved)
+            if (mProject != null && mProject.Unsaved)
             {
                 DialogResult result = MessageBox.Show(Localizer.Message("closed_project_text"),
                     Localizer.Message("close_project_caption"), MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
                 if (result == DialogResult.Yes)
                 {
-                    // save the project
+                    mProject.Save();
+                    OnProjectSaved();
                 }
-                return result != DialogResult.Cancel;
+                if (result == DialogResult.Cancel)
+                {
+                    Ready();
+                    return false;
+                }
+                else
+                {
+                    OnProjectClosed();
+                    return true;
+                }
             }
             else
             {
+                Ready();
                 return true;
             }
         }
 
+        /// <summary>
+        /// Touch the project so that it seems that it was modified (used for debugging.)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void touchProjectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mProject.Touch();
+            OnProjectModified();
+        }
+
+        private void OnProjectClosed()
+        {
+            ProjectStateChanged(this, new Events.Project.StateChangedEventArgs(mProject, Obi.Events.Project.StateChange.Closed));
+        }
+
+        private void OnProjectCreated()
+        {
+            ProjectStateChanged(this, new Events.Project.StateChangedEventArgs(mProject, Obi.Events.Project.StateChange.Created));
+        }
+
+        private void OnProjectModified()
+        {
+            ProjectStateChanged(this, new Events.Project.StateChangedEventArgs(mProject, Obi.Events.Project.StateChange.Modified));
+        }
+
+        private void OnProjectOpened()
+        {
+            ProjectStateChanged(this, new Events.Project.StateChangedEventArgs(mProject, Obi.Events.Project.StateChange.Opened));
+        }
+
+        private void OnProjectSaved()
+        {
+            ProjectStateChanged(this, new Events.Project.StateChangedEventArgs(mProject, Obi.Events.Project.StateChange.Saved));
+        }
+
+        private void mProject_StateChanged(object sender, Events.Project.StateChangedEventArgs e)
+        {
+            switch (e.Change)
+            {
+                case Obi.Events.Project.StateChange.Closed:
+                    GUIUpdateNoProject();
+                    break;
+                case Obi.Events.Project.StateChange.Created:
+                    GUIUpdateSavedProject();
+                    Debug(Localizer.Message("debug_created_project"));
+                    break;
+                case Obi.Events.Project.StateChange.Modified:
+                    GUIUpdateUnsavedProject();
+                    Debug(Localizer.Message("debug_touched_project"));
+                    break;
+                case Obi.Events.Project.StateChange.Opened:
+                    GUIUpdateSavedProject();
+                    Debug(String.Format(Localizer.Message("debug_opened_project"), e.Project.XUKPath));
+                    break;
+                case Obi.Events.Project.StateChange.Saved:
+                    GUIUpdateSavedProject();
+                    Debug(String.Format(Localizer.Message("debug_saved_project"), e.Project.XUKPath));
+                    break;
+                default:
+                    Debug("******");
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Utility method that prints to the console and outputs to the status bar.
+        /// </summary>
+        /// <param name="message">The message to print.</param>
+        private void Debug(string message)
+        {
+            Console.WriteLine(message);
+            toolStripStatusLabel1.Text = message;
+        }
+
+        /// <summary>
+        /// Update the GUI to reflect enabled/disabled actions when no project is opened.
+        /// </summary>
+        private void GUIUpdateNoProject()
+        {
+            closeProjectToolStripMenuItem.Enabled = false;
+            saveProjectToolStripMenuItem.Enabled = false;
+            saveProjectasToolStripMenuItem.Enabled = false;
+            touchProjectToolStripMenuItem.Enabled = false;
+            revertToolStripMenuItem.Enabled = false;
+            Ready();
+        }
+
+        /// <summary>
+        /// Update the GUI to reflect enabled/disabled actions when the project is saved.
+        /// </summary>
+        private void GUIUpdateSavedProject()
+        {
+            closeProjectToolStripMenuItem.Enabled = true;
+            saveProjectToolStripMenuItem.Enabled = false;
+            saveProjectasToolStripMenuItem.Enabled = true;
+            touchProjectToolStripMenuItem.Enabled = true;
+            revertToolStripMenuItem.Enabled = false;
+        }
+
+        /// <summary>
+        /// Update the GUI to reflect enabled/disabled actions when the project is unsaved.
+        /// </summary>
+        private void GUIUpdateUnsavedProject()
+        {
+            closeProjectToolStripMenuItem.Enabled = true;
+            saveProjectToolStripMenuItem.Enabled = true;
+            saveProjectasToolStripMenuItem.Enabled = true;
+            touchProjectToolStripMenuItem.Enabled = true;
+            revertToolStripMenuItem.Enabled = true;
+            Ready();
+        }
+
+        private void Ready()
+        {
+            toolStripStatusLabel1.Text = Localizer.Message("ready");
+        }
+
 
         
-        
-        
-        private UndoRedoStack mUndoStack;
+
+
 
         private string undo_label;
         private string redo_label;
 
         private delegate void UndoStackChangedHandler(object sender, EventArgs e);
         private event UndoStackChangedHandler UndoStackChanged;
-
-        public ObiForm()
-        {
-            InitializeComponent();
-            //mStrip.Resized += new EmptyStrip.ResizedHandler(mStrip_Resized);
-            UndoStackChanged += new UndoStackChangedHandler(mUndoStack_UndoStackChanged);
-            mUndoStack = new UndoRedoStack();
-            undo_label = mUndoToolStripMenuItem.Text;
-            redo_label = mRedoToolStripMenuItem.Text;
-        }
 
         private void mStrip_Resized(object sender, ResizedEventArgs e)
         {
