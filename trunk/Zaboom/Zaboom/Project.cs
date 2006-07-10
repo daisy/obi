44cfcdc64a24f1855d70f5cec6e61f315707c30d
julienq <julienq@f04d5631-4067-4348-862e-39f3aacb62c1>
2006-07-10 14:27:34 +0000

    Renaming (not done yet)

--- trunk/Zaboom/Zaboom/Project.cs
+++ trunk/Zaboom/Zaboom/Project.cs
@@ -14,16 +14,17 @@ namespace Zaboom
         private AssetManager mAssManager;
         private Channel mAudioChannel;
         private Channel mTextChannel;
+        private List<CoreNode> mAssetNodes;
 
-        public Project(AssetManager manager)
+        public Project(AssetManager manager): base()
         {
             mAssManager = manager;
-            
             mAudioChannel = getPresentation().getChannelFactory().createChannel("audio channel");
             getPresentation().getChannelsManager().addChannel(mAudioChannel);
             mTextChannel = getPresentation().getChannelFactory().createChannel("text channel");
             getPresentation().getChannelsManager().addChannel(mTextChannel);
             getPresentation().setMediaFactory(new AssetMediaFactory());
+            mAssetNodes = new List<CoreNode>();
         }
 
         /// <summary>
@@ -50,7 +51,48 @@ namespace Zaboom
             text.setText(asset.Name);
             chprop.setMedia(mTextChannel, text);
             getPresentation().getRootNode().appendChild(node);
+            mAssetNodes.Add(node);
             return asset.Name;
+        }
+
+        public void SaveXUK(string path)
+        {
+            Uri uri = new Uri(path);
+            path = Path.GetDirectoryName(Path.GetFullPath(path));
+            getPresentation().getRootNode().acceptDepthFirst(new AudioVisitor(path, mAudioChannel));
+            saveXUK(uri);
+        }
+
+        public AudioMediaAsset AssetAt(int index)
+        {
+            CoreNode node = mAssetNodes[index];
+            ChannelsProperty prop = (ChannelsProperty)node.getProperty(typeof(ChannelsProperty));
+            AudioAsset audio = prop.getMedia(mAudioChannel) as AudioAsset;
+            return audio.Asset;
+        }
+
+        /// <summary>
+        /// Rename the node/asset at the given position.
+        /// </summary>
+        /// <param name="index">The index in the playlist.</param>
+        /// <param name="name">The new name.</param>
+        /// <returns>True if the name could be changed.</returns>
+        public bool RenameAt(int index, string name)
+        {
+            CoreNode node = mAssetNodes[index];
+            if (mAssManager.IsAssetNameAvailable(name))
+            {
+                ChannelsProperty prop = (ChannelsProperty)node.getProperty(typeof(ChannelsProperty));
+                AudioAsset audio = prop.getMedia(mAudioChannel) as AudioAsset;
+                mAssManager.RenameAsset(audio.Asset, name);                
+                TextMedia text = (TextMedia)prop.getMedia(mTextChannel);
+                text.setText(name);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
--- trunk/Zaboom/Zaboom/Zaboom.cs
+++ trunk/Zaboom/Zaboom/Zaboom.cs
@@ -1,26 +1,50 @@
 using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
-
+using Microsoft.DirectX.DirectSound;
 using UrakawaApplicationBackend;
+using UrakawaApplicationBackend.events.audioPlayerEvents;
 using UrakawaApplicationBackend.events.assetManagerEvents;
 
 namespace Zaboom
 {
     public partial class Zaboom : Form
     {
+        private AudioPlayer mPlayer;
         private Project mProject;
 
         public Zaboom()
         {
             InitializeComponent();
+            // Initialize the player
+            mPlayer = new AudioPlayer();
+            ArrayList devices = mPlayer.GetOutputDevices();
+            if (devices.Count == 0)
+            {
+                MessageBox.Show("Could not find any output device, will abort.", "No output device",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+            mPlayer.SetDevice(this, 0);
+            mPlayer.StateChangedEvent += new DStateChangedEvent(OnStateChangedEvent);
+            OnStateChangedEvent(this, null);
+            NoAsset();
+            // Initialize the project
             AssetManager assmanager = new AssetManager(System.IO.Path.GetTempPath());
             assmanager.AssetRenamedEvent += new DAssetRenamedEvent(OnAssetRenamedEvent);
             mProject = new Project(assmanager);
+        }
+
+        private void NoAsset()
+        {
+            playToolStripMenuItem.Enabled = false;
+            stopToolStripMenuItem.Enabled = false;
+            mPlayOneButton.Enabled = false;
+            mStopButton.Enabled = false;
         }
 
         #region Menu Items
@@ -37,7 +61,24 @@ namespace Zaboom
                     string name = mProject.AddFile(filename);
                     mAssBox.Items.Add(name);
                     mAssBox.SelectedIndex = mAssBox.Items.Count - 1;
+                    if (mAssBox.Items.Count == 1)
+                    {
+                        playToolStripMenuItem.Enabled = false;
+                        stopToolStripMenuItem.Enabled = false;
+                        mPlayOneButton.Enabled = true;
+                        mStopButton.Enabled = true;
+                    }
                 }
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "XUK project (*.xuk)|*.xuk";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                mProject.SaveXUK(dialog.FileName);
             }
         }
 
@@ -46,14 +87,115 @@ namespace Zaboom
             Application.Exit();
         }
 
+        private void renameAssetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (mAssBox.SelectedIndex >= 0)
+            {
+                //NameDialog dialog = new NameDialog();
+            }
+        }
+
+        private void playToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PlayOne();
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            StopPlaying();
+        }
+
+        #endregion
+
+        #region Buttons
+
+        private void mPlayOneButton_Click(object sender, EventArgs e)
+        {
+            PlayOne();
+        }
+
+        private void mStopButton_Click(object sender, EventArgs e)
+        {
+            StopPlaying();
+        }
+
         #endregion
 
         #region Event handlers
+
+        public void OnEndOfAudioStop(object sender, EndOfAudioData e)
+        {
+        }
+
+        private delegate void UpdateOnStateChangedDelegate();
+
+        private void UpdateOnStateChanged()
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new UpdateOnStateChangedDelegate(UpdateOnStateChanged));
+            }
+            else
+            {
+                toolStripStatusLabel1.Text = "Audio player currently " + mPlayer.State + ".";
+                playToolStripMenuItem.Text = mPlayOneButton.Text = mPlayer.State == AudioPlayerState.playing ? "&Pause" : "&Play";
+                stopToolStripMenuItem.Enabled = mPlayer.State != AudioPlayerState.stopped;
+            }
+        }
+
+        public void OnStateChangedEvent(object sender, StateChanged e)
+        {
+            UpdateOnStateChanged();
+        }
 
         public void OnAssetRenamedEvent(object sender, AssetRenamed e)
         {
         }
 
+        private void mAssBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (mPlayer.State == AudioPlayerState.playing)
+            {
+                mPlayer.Stop();
+                PlayOne();
+            }
+            else if (mPlayer.State == AudioPlayerState.paused)
+            {
+                mPlayer.Stop();
+            }
+        }
+
         #endregion
+
+        /// <summary>
+        /// Play the currently selected asset.
+        /// </summary>
+        private void PlayOne()
+        {
+            switch (mPlayer.State)
+            {
+                case AudioPlayerState.paused:
+                    mPlayer.Resume();
+                    break;
+                case AudioPlayerState.playing:
+                    mPlayer.Pause();
+                    break;
+                case AudioPlayerState.stopped:
+                    if (mAssBox.SelectedIndex >= 0)
+                    {
+                        mPlayer.EndOfAudioAsset.EndOfAudioAssetEvent += new DEndOfAudioAssetEvent(OnEndOfAudioStop);
+                        mPlayer.Play(mProject.AssetAt(mAssBox.SelectedIndex));
+                    }
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Stop playing.
+        /// </summary>
+        private void StopPlaying()
+        {
+            if (mPlayer.State != AudioPlayerState.stopped) mPlayer.Stop();
+        }
     }
 }
