c5a2ff8ff49f0f85b00b1b8f9a2994f5a42bc5a1
julienq <julienq@f04d5631-4067-4348-862e-39f3aacb62c1>
2006-07-11 14:07:20 +0000

    AddSiblingSectionEvent added

---
+++ trunk/Obi/Obi/Events/Node/AddSiblingSectionEventArgs.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using urakawa.core;
+
+namespace Obi.Events.Node
+{
+    public delegate void AddSiblingSectionHandler(object sender, AddSiblingSectionEventArgs e);
+
+    /// <summary>
+    /// This event is fired when a view wants to add a new sibling section node in the core tree.
+    /// </summary>
+    class AddSiblingSectionEventArgs : EventArgs
+    {
+        private CoreNode mContextNode;  // the node after which to add the new sibling
+
+        public CoreNode ContextNode
+        {
+            get
+            {
+                return mContextNode;
+            }
+        }
+
+        public AddSiblingSectionEventArgs(CoreNode contextNode)
+        {
+            mContextNode = contextNode;
+        }
+    }
+}
