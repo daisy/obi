a3c83f1a5c324ebceee3daea2c30c74c4a45ce3e
marisademeglio <marisademeglio@f04d5631-4067-4348-862e-39f3aacb62c1>
2006-07-11 15:32:43 +0000

    Started adding functions for the TOCPanel user control

--- trunk/Obi/Obi/UserControls/TOCPanel.Designer.cs
+++ trunk/Obi/Obi/UserControls/TOCPanel.Designer.cs
@@ -28,29 +28,111 @@ namespace Obi.UserControls
         /// </summary>
         private void InitializeComponent()
         {
-            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.components = new System.ComponentModel.Container();
+            this.tocTree = new System.Windows.Forms.TreeView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.addSectionAtSameLevelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addSubSectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.moveUpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.moveDownToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.increaseLevelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.decreaseLevelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editLabelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // treeView1
+            // tocTree
             // 
-            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.treeView1.Location = new System.Drawing.Point(0, 0);
-            this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(150, 150);
-            this.treeView1.TabIndex = 0;
+            this.tocTree.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tocTree.Location = new System.Drawing.Point(0, 0);
+            this.tocTree.Name = "tocTree";
+            this.tocTree.Size = new System.Drawing.Size(129, 133);
+            this.tocTree.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addSectionAtSameLevelToolStripMenuItem,
+            this.addSubSectionToolStripMenuItem,
+            this.moveUpToolStripMenuItem,
+            this.moveDownToolStripMenuItem,
+            this.increaseLevelToolStripMenuItem,
+            this.decreaseLevelToolStripMenuItem,
+            this.editLabelToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(197, 158);
+            // 
+            // addSectionAtSameLevelToolStripMenuItem
+            // 
+            this.addSectionAtSameLevelToolStripMenuItem.Name = "addSectionAtSameLevelToolStripMenuItem";
+            this.addSectionAtSameLevelToolStripMenuItem.Size = new System.Drawing.Size(196, 22);
+            this.addSectionAtSameLevelToolStripMenuItem.Text = "Add section at same level";
+            this.addSectionAtSameLevelToolStripMenuItem.Click += new System.EventHandler(this.addSectionAtSameLevelToolStripMenuItem_Click);
+            // 
+            // addSubSectionToolStripMenuItem
+            // 
+            this.addSubSectionToolStripMenuItem.Name = "addSubSectionToolStripMenuItem";
+            this.addSubSectionToolStripMenuItem.Size = new System.Drawing.Size(196, 22);
+            this.addSubSectionToolStripMenuItem.Text = "Add sub-section";
+            this.addSubSectionToolStripMenuItem.Click += new System.EventHandler(this.addSubSectionToolStripMenuItem_Click);
+            // 
+            // moveUpToolStripMenuItem
+            // 
+            this.moveUpToolStripMenuItem.Name = "moveUpToolStripMenuItem";
+            this.moveUpToolStripMenuItem.Size = new System.Drawing.Size(196, 22);
+            this.moveUpToolStripMenuItem.Text = "Move up";
+            this.moveUpToolStripMenuItem.Click += new System.EventHandler(this.moveUpToolStripMenuItem_Click);
+            // 
+            // moveDownToolStripMenuItem
+            // 
+            this.moveDownToolStripMenuItem.Name = "moveDownToolStripMenuItem";
+            this.moveDownToolStripMenuItem.Size = new System.Drawing.Size(196, 22);
+            this.moveDownToolStripMenuItem.Text = "Move down";
+            this.moveDownToolStripMenuItem.Click += new System.EventHandler(this.moveDownToolStripMenuItem_Click);
+            // 
+            // increaseLevelToolStripMenuItem
+            // 
+            this.increaseLevelToolStripMenuItem.Name = "increaseLevelToolStripMenuItem";
+            this.increaseLevelToolStripMenuItem.Size = new System.Drawing.Size(196, 22);
+            this.increaseLevelToolStripMenuItem.Text = "Increase level";
+            this.increaseLevelToolStripMenuItem.Click += new System.EventHandler(this.increaseLevelToolStripMenuItem_Click);
+            // 
+            // decreaseLevelToolStripMenuItem
+            // 
+            this.decreaseLevelToolStripMenuItem.Name = "decreaseLevelToolStripMenuItem";
+            this.decreaseLevelToolStripMenuItem.Size = new System.Drawing.Size(196, 22);
+            this.decreaseLevelToolStripMenuItem.Text = "Decrease level";
+            this.decreaseLevelToolStripMenuItem.Click += new System.EventHandler(this.decreaseLevelToolStripMenuItem_Click);
+            // 
+            // editLabelToolStripMenuItem
+            // 
+            this.editLabelToolStripMenuItem.Name = "editLabelToolStripMenuItem";
+            this.editLabelToolStripMenuItem.Size = new System.Drawing.Size(196, 22);
+            this.editLabelToolStripMenuItem.Text = "Edit label";
+            this.editLabelToolStripMenuItem.Click += new System.EventHandler(this.editLabelToolStripMenuItem_Click);
             // 
             // TOCPanel
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.tocTree);
             this.Name = "TOCPanel";
+            this.Size = new System.Drawing.Size(129, 133);
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TreeView tocTree;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem addSectionAtSameLevelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addSubSectionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem moveUpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem moveDownToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem increaseLevelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem decreaseLevelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editLabelToolStripMenuItem;
     }
 }
