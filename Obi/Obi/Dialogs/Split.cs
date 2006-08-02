463bfde93c46b89c6916dc4d5dba1e2cf3f58c33
Avneesh Singh <13777742+avneeshsingh@users.noreply.github.com>
2006-08-02 06:48:37 +0000

    Update to Split Dialog

--- Obi/Obi/Dialogs/Split.cs
+++ Obi/Obi/Dialogs/Split.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Windows.Forms;
 
 using VirtualAudioBackend;
+using VirtualAudioBackend.events.AudioPlayerEvents;
 
 namespace Obi.Dialogs
 {
@@ -15,6 +16,13 @@ namespace Obi.Dialogs
         public Split()
         {
             InitializeComponent();
+            AudioPlayer.Instance.StateChanged += new StateChangedHandler(AudioPlayer_StateChanged);
+                        AudioPlayer.Instance.EndOfAudioAsset += new EndOfAudioAssetHandler(AudioPlayer_EndOfAudioAsset);
+            AudioPlayer.Instance.EndOfAudioBuffer += new EndOfAudioBufferHandler(AudioPlayer_EndOfAudioBuffer);
+            AudioPlayer.Instance.UpdateVuMeter += new UpdateVuMeterHandler(AudioPlayer_UpdateVuMeter);
+            tmUpdateTimePosition.Enabled = true;
+        
+
             AudioClip ob_Clip = new AudioClip("c:\\atest\\a\\a1.wav");
         ArrayList al = new ArrayList();
          al.Add(ob_Clip);
@@ -23,8 +31,8 @@ namespace Obi.Dialogs
         
         AudioMediaAsset ob_AudioAsset;
         double m_dSplitTime;
-        int m_Step=10;
-        int m_FineStep = 2;
+        int m_Step=10000;
+        int m_FineStep = 2000;
 
         private void btnPreview_Click(object sender, EventArgs e)
         {
@@ -41,33 +49,34 @@ namespace Obi.Dialogs
 
         private void tmUpdateTimePosition_Tick(object sender, EventArgs e)
         {
-            txtDisplayTime.Text = m_dSplitTime.ToString ();
+            txtDisplayTime.Text = AudioPlayer.Instance.CurrentTimePosition.ToString();
         }
 
         private void btnFastRewind_Click(object sender, EventArgs e)
         {
                     m_dSplitTime = m_dSplitTime - m_Step;
-            
+                    txtDisplayTime.Text = m_dSplitTime.ToString();
 
         }
 
         private void btnFastForward_Click(object sender, EventArgs e)
         {
             m_dSplitTime = m_dSplitTime + m_Step;
-            
+            txtDisplayTime.Text = m_dSplitTime.ToString();
 
         }
 
         private void btnFineRewind_Click(object sender, EventArgs e)
         {
             m_dSplitTime = m_dSplitTime - m_FineStep;
-            
+            txtDisplayTime.Text = m_dSplitTime.ToString();
 
         }
 
         private void btnFineForward_Click(object sender, EventArgs e)
         {
             m_dSplitTime = m_dSplitTime + m_FineStep;
+            txtDisplayTime.Text = m_dSplitTime.ToString();
             //double dTempPosition = AudioPlayer.Instance.CurrentTimePosition;
             //AudioPlayer.Instance.CurrentTimePosition= dTempPosition +m_FineStep;
         }
@@ -86,6 +95,7 @@ namespace Obi.Dialogs
 
         private void Split_Load(object sender, EventArgs e)
         {
+            
             VuMeter ob_VuMeter = new VuMeter();
             ob_VuMeter.LowerThreshold = 50;
             ob_VuMeter.UpperThreshold = 300;
@@ -98,11 +108,47 @@ namespace Obi.Dialogs
         {
             m_dSplitTime = AudioPlayer.Instance.CurrentTimePosition;
             AudioPlayer.Instance.Stop();
+            tmUpdateTimePosition.Enabled = false;
+            txtDisplayTime.Text = m_dSplitTime.ToString();
         }
 
         
             
 
             
-    }
+
+
+
+        private void AudioPlayer_StateChanged(object sender, StateChanged e)
+        {
+        }
+
+        private void AudioPlayer_EndOfAudioAsset(object sender, EndOfAudioAsset e)
+        {
+            
+
+        }
+
+        // Convoluted way to close necessary for debugging (JQ)
+        private delegate void CloseCallback();
+        public new void Close()
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new CloseCallback(Close));
+            }
+            else
+            {
+                base.Close();
+            }
+        }
+
+        private void AudioPlayer_EndOfAudioBuffer(object sender, EndOfAudioBuffer e)
+        {
+        }
+
+        private void AudioPlayer_UpdateVuMeter(object sender, UpdateVuMeter e)
+        {
+        }
+    }// end of class
 }
