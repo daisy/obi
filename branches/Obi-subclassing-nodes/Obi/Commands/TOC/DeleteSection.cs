76e55142ef43130b69de724f2cb1a3b2881e6a02
julienq <julienq@f04d5631-4067-4348-862e-39f3aacb62c1>
2006-09-29 23:09:34 +0000

    Creating new branch for subclassing nodes

---
+++ branches/Obi-subclassing-nodes/Obi/Commands/TOC/DeleteSection.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using urakawa.core;
+
+namespace Obi.Commands.TOC
+{
+    class DeleteSectionNode : Command
+    {
+        private Project mProject;
+        private CoreNode mNode;
+        private CoreNode mParent;
+        private int mIndex;
+        private int mPosition;
+
+        public override string Label
+        {
+            get
+            {
+                return Localizer.Message("delete_section_command_label");
+            }
+        }
+
+        public DeleteSectionNode(Project project, CoreNode node, CoreNode parent, int index, int position)
+        {
+            mProject = project;
+            mNode = node;
+            mParent = parent;
+            mIndex = index;
+            mPosition = position;
+        }
+
+        /// <summary>
+        /// Do: delete the node from the project.
+        /// </summary>
+        public override void Do()
+        {
+            mProject.RemoveNode(mProject, mNode);
+        }
+
+        /// <summary>
+        /// Undo: restore the node and its descendants.
+        /// </summary>
+        public override void Undo()
+        {
+            mProject.UndeleteSectionNode(mNode, mParent, mIndex, mPosition);
+        }
+    }
+}
