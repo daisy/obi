efa84a88898799c60030f9475da99fb6b4ba31e9
julienq <julienq@f04d5631-4067-4348-862e-39f3aacb62c1>
2006-10-31 03:53:16 +0000

    Updated about and help dialogs.

--- trunk/Obi/Obi/Dialogs/About.cs
+++ trunk/Obi/Obi/Dialogs/About.cs
@@ -1,18 +1,30 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
 using System.Windows.Forms;
 
 namespace Obi.Dialogs
 {
+    /// <summary>
+    /// Form for giving info about Obi.
+    /// </summary>
     public partial class About : Form
     {
+        /// <summary>
+        /// Create a new About form.
+        /// </summary>
         public About()
         {
             InitializeComponent();
+            // setup the link data for following the link
+            linkLabel1.Links[0].LinkData = linkLabel1.Text;
+        }
+
+        /// <summary>
+        /// Follow a link (opens a browser externally.)
+        /// </summary>
+        /// <param name="sender">Sender of the event.</param>
+        /// <param name="e">The event arguments.</param>
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start((string)e.Link.LinkData);
         }
 
     }
