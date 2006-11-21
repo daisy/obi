6b27b1ab4faaf1fc0fea1ab5ab9a42daa0c7afa8
Avneesh Singh <13777742+avneeshsingh@users.noreply.github.com>
2006-11-21 13:02:44 +0000

    VuMeter starts working with playList class with integration with TransportPlay dialog, more refinement needs to be added

--- Obi/Obi/Dialogs/TransportPlay.Designer.cs
+++ Obi/Obi/Dialogs/TransportPlay.Designer.cs
@@ -28,6 +28,7 @@ namespace Obi.Dialogs
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.mPlayButton = new System.Windows.Forms.Button();
             this.mStopButton = new System.Windows.Forms.Button();
             this.mPauseButton = new System.Windows.Forms.Button();
@@ -36,6 +37,15 @@ namespace Obi.Dialogs
             this.btnPreviousPhrase = new System.Windows.Forms.Button();
             this.btnRewind = new System.Windows.Forms.Button();
             this.btnForward = new System.Windows.Forms.Button();
+            this.txtOverloadLeft = new System.Windows.Forms.TextBox();
+            this.txtOverloadRight = new System.Windows.Forms.TextBox();
+            this.lblLeftOverload = new System.Windows.Forms.Label();
+            this.lblRithtOverload = new System.Windows.Forms.Label();
+            this.tmUpdateAmplitudeText = new System.Windows.Forms.Timer(this.components);
+            this.lblAmplitudeLeft = new System.Windows.Forms.Label();
+            this.txtAmplitudeLeft = new System.Windows.Forms.TextBox();
+            this.lblAmplitudeRight = new System.Windows.Forms.Label();
+            this.txtAmplitudeRight = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // mPlayButton
@@ -94,7 +104,7 @@ namespace Obi.Dialogs
             this.btnPreviousPhrase.Name = "btnPreviousPhrase";
             this.btnPreviousPhrase.Size = new System.Drawing.Size(75, 23);
             this.btnPreviousPhrase.TabIndex = 5;
-            this.btnPreviousPhrase.Text = "P&revious Phrase";
+            this.btnPreviousPhrase.Text = "Pre&vious Phrase";
             this.btnPreviousPhrase.UseVisualStyleBackColor = true;
             this.btnPreviousPhrase.Click += new System.EventHandler(this.btnPreviousPhrase_Click);
             // 
@@ -118,11 +128,97 @@ namespace Obi.Dialogs
             this.btnForward.UseVisualStyleBackColor = true;
             this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
             // 
+            // txtOverloadLeft
+            // 
+            this.txtOverloadLeft.AccessibleName = "Left Overload";
+            this.txtOverloadLeft.Location = new System.Drawing.Point(73, 110);
+            this.txtOverloadLeft.Name = "txtOverloadLeft";
+            this.txtOverloadLeft.ReadOnly = true;
+            this.txtOverloadLeft.Size = new System.Drawing.Size(100, 20);
+            this.txtOverloadLeft.TabIndex = 8;
+            // 
+            // txtOverloadRight
+            // 
+            this.txtOverloadRight.AccessibleName = "Right OverLload";
+            this.txtOverloadRight.Location = new System.Drawing.Point(255, 110);
+            this.txtOverloadRight.Name = "txtOverloadRight";
+            this.txtOverloadRight.ReadOnly = true;
+            this.txtOverloadRight.Size = new System.Drawing.Size(100, 20);
+            this.txtOverloadRight.TabIndex = 9;
+            // 
+            // lblLeftOverload
+            // 
+            this.lblLeftOverload.AutoSize = true;
+            this.lblLeftOverload.Location = new System.Drawing.Point(0, 112);
+            this.lblLeftOverload.Name = "lblLeftOverload";
+            this.lblLeftOverload.Size = new System.Drawing.Size(71, 13);
+            this.lblLeftOverload.TabIndex = 8;
+            this.lblLeftOverload.Text = "&Left Overload";
+            // 
+            // lblRithtOverload
+            // 
+            this.lblRithtOverload.AutoSize = true;
+            this.lblRithtOverload.Location = new System.Drawing.Point(176, 112);
+            this.lblRithtOverload.Name = "lblRithtOverload";
+            this.lblRithtOverload.Size = new System.Drawing.Size(78, 13);
+            this.lblRithtOverload.TabIndex = 9;
+            this.lblRithtOverload.Text = "&Right Overload";
+            // 
+            // tmUpdateAmplitudeText
+            // 
+            this.tmUpdateAmplitudeText.Enabled = true;
+            this.tmUpdateAmplitudeText.Interval = 2000;
+            this.tmUpdateAmplitudeText.Tick += new System.EventHandler(this.tmUpdateAmplitudeText_Tick);
+            // 
+            // lblAmplitudeLeft
+            // 
+            this.lblAmplitudeLeft.AutoSize = true;
+            this.lblAmplitudeLeft.Location = new System.Drawing.Point(0, 145);
+            this.lblAmplitudeLeft.Name = "lblAmplitudeLeft";
+            this.lblAmplitudeLeft.Size = new System.Drawing.Size(74, 13);
+            this.lblAmplitudeLeft.TabIndex = 10;
+            this.lblAmplitudeLeft.Text = "Lef&t Amplitude";
+            // 
+            // txtAmplitudeLeft
+            // 
+            this.txtAmplitudeLeft.AccessibleName = "Left Amplitude";
+            this.txtAmplitudeLeft.Location = new System.Drawing.Point(75, 144);
+            this.txtAmplitudeLeft.Name = "txtAmplitudeLeft";
+            this.txtAmplitudeLeft.ReadOnly = true;
+            this.txtAmplitudeLeft.Size = new System.Drawing.Size(100, 20);
+            this.txtAmplitudeLeft.TabIndex = 11;
+            // 
+            // lblAmplitudeRight
+            // 
+            this.lblAmplitudeRight.AutoSize = true;
+            this.lblAmplitudeRight.Location = new System.Drawing.Point(177, 146);
+            this.lblAmplitudeRight.Name = "lblAmplitudeRight";
+            this.lblAmplitudeRight.Size = new System.Drawing.Size(81, 13);
+            this.lblAmplitudeRight.TabIndex = 12;
+            this.lblAmplitudeRight.Text = "Ri&ght Amplitude";
+            // 
+            // txtAmplitudeRight
+            // 
+            this.txtAmplitudeRight.AccessibleName = "Right Amplitude";
+            this.txtAmplitudeRight.Location = new System.Drawing.Point(259, 145);
+            this.txtAmplitudeRight.Name = "txtAmplitudeRight";
+            this.txtAmplitudeRight.ReadOnly = true;
+            this.txtAmplitudeRight.Size = new System.Drawing.Size(100, 20);
+            this.txtAmplitudeRight.TabIndex = 13;
+            // 
             // TransportPlay
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(292, 296);
+            this.Controls.Add(this.txtAmplitudeRight);
+            this.Controls.Add(this.lblAmplitudeRight);
+            this.Controls.Add(this.txtAmplitudeLeft);
+            this.Controls.Add(this.lblAmplitudeLeft);
+            this.Controls.Add(this.lblRithtOverload);
+            this.Controls.Add(this.lblLeftOverload);
+            this.Controls.Add(this.txtOverloadRight);
+            this.Controls.Add(this.txtOverloadLeft);
             this.Controls.Add(this.btnForward);
             this.Controls.Add(this.btnRewind);
             this.Controls.Add(this.btnPreviousPhrase);
@@ -135,6 +231,7 @@ namespace Obi.Dialogs
             this.Text = "TransportPlay";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TransportPlay_FormClosing);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -148,5 +245,14 @@ namespace Obi.Dialogs
         private System.Windows.Forms.Button btnPreviousPhrase;
         private System.Windows.Forms.Button btnRewind;
         private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.TextBox txtOverloadLeft;
+        private System.Windows.Forms.TextBox txtOverloadRight;
+        private System.Windows.Forms.Label lblLeftOverload;
+        private System.Windows.Forms.Label lblRithtOverload;
+        private System.Windows.Forms.Timer tmUpdateAmplitudeText;
+        private System.Windows.Forms.Label lblAmplitudeLeft;
+        private System.Windows.Forms.TextBox txtAmplitudeLeft;
+        private System.Windows.Forms.Label lblAmplitudeRight;
+        private System.Windows.Forms.TextBox txtAmplitudeRight;
     }
 }
