c125175efefe25cdab001ec99a7b650891bdab12
Avneesh Singh <13777742+avneeshsingh@users.noreply.github.com>
2006-12-04 10:21:39 +0000

    textBoxes of textVuMeter start working but events still to work

--- Obi/Obi/UserControls/TextVUMeterPanel.Designer.cs
+++ Obi/Obi/UserControls/TextVUMeterPanel.Designer.cs
@@ -28,43 +28,45 @@ namespace Obi.UserControls
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.components = new System.ComponentModel.Container();
+            this.lblLeft = new System.Windows.Forms.Label();
             this.mLeftBox = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.lblRight = new System.Windows.Forms.Label();
             this.mRightBox = new System.Windows.Forms.TextBox();
             this.mResetButton = new System.Windows.Forms.Button();
+            this.tmUpdateText = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
-            // label1
+            // lblLeft
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 8);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(27, 12);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "&Left:";
+            this.lblLeft.AutoSize = true;
+            this.lblLeft.Location = new System.Drawing.Point(3, 9);
+            this.lblLeft.Name = "lblLeft";
+            this.lblLeft.Size = new System.Drawing.Size(28, 13);
+            this.lblLeft.TabIndex = 0;
+            this.lblLeft.Text = "&Left:";
             // 
             // mLeftBox
             // 
             this.mLeftBox.Location = new System.Drawing.Point(36, 5);
             this.mLeftBox.Name = "mLeftBox";
-            this.mLeftBox.Size = new System.Drawing.Size(32, 19);
+            this.mLeftBox.Size = new System.Drawing.Size(32, 20);
             this.mLeftBox.TabIndex = 1;
             // 
-            // label2
+            // lblRight
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(74, 8);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(34, 12);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "&Right:";
+            this.lblRight.AutoSize = true;
+            this.lblRight.Location = new System.Drawing.Point(74, 9);
+            this.lblRight.Name = "lblRight";
+            this.lblRight.Size = new System.Drawing.Size(35, 13);
+            this.lblRight.TabIndex = 2;
+            this.lblRight.Text = "&Right:";
             // 
             // mRightBox
             // 
             this.mRightBox.Location = new System.Drawing.Point(114, 5);
             this.mRightBox.Name = "mRightBox";
-            this.mRightBox.Size = new System.Drawing.Size(32, 19);
+            this.mRightBox.Size = new System.Drawing.Size(32, 20);
             this.mRightBox.TabIndex = 3;
             // 
             // mResetButton
@@ -74,23 +76,30 @@ namespace Obi.UserControls
             this.mResetButton.BackColor = System.Drawing.SystemColors.Control;
             this.mResetButton.Location = new System.Drawing.Point(152, 3);
             this.mResetButton.Name = "mResetButton";
-            this.mResetButton.Size = new System.Drawing.Size(45, 22);
+            this.mResetButton.Size = new System.Drawing.Size(45, 23);
             this.mResetButton.TabIndex = 4;
             this.mResetButton.Text = "Re&set";
             this.mResetButton.UseVisualStyleBackColor = false;
+            this.mResetButton.Click += new System.EventHandler(this.mResetButton_Click);
+            // 
+            // tmUpdateText
+            // 
+            this.tmUpdateText.Enabled = true;
+            this.tmUpdateText.Interval = 1500;
+            this.tmUpdateText.Tick += new System.EventHandler(this.tmUpdateText_Tick);
             // 
             // TextVUMeterPanel
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Transparent;
             this.Controls.Add(this.mResetButton);
             this.Controls.Add(this.mRightBox);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblRight);
             this.Controls.Add(this.mLeftBox);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblLeft);
             this.Name = "TextVUMeterPanel";
-            this.Size = new System.Drawing.Size(220, 41);
+            this.Size = new System.Drawing.Size(220, 44);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -98,11 +107,12 @@ namespace Obi.UserControls
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblLeft;
         private System.Windows.Forms.TextBox mLeftBox;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblRight;
         private System.Windows.Forms.TextBox mRightBox;
         private System.Windows.Forms.Button mResetButton;
+        private System.Windows.Forms.Timer tmUpdateText;
 
     }
 }
