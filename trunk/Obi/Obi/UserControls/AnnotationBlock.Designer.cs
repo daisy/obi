1508b233936ba46b922fb6cdfc9d423d3d4946d3
julienq <julienq@f04d5631-4067-4348-862e-39f3aacb62c1>
2006-12-07 09:18:51 +0000

    Playlist slowly improving. Annotation block looks better now?!?!?

--- trunk/Obi/Obi/UserControls/AnnotationBlock.Designer.cs
+++ trunk/Obi/Obi/UserControls/AnnotationBlock.Designer.cs
@@ -41,7 +41,7 @@ namespace Obi.UserControls
             this.mRenameBox.Location = new System.Drawing.Point(3, 3);
             this.mRenameBox.Margin = new System.Windows.Forms.Padding(0);
             this.mRenameBox.Name = "mRenameBox";
-            this.mRenameBox.Size = new System.Drawing.Size(66, 13);
+            this.mRenameBox.Size = new System.Drawing.Size(66, 12);
             this.mRenameBox.TabIndex = 3;
             this.mRenameBox.TabStop = false;
             this.mRenameBox.Visible = false;
@@ -55,7 +55,7 @@ namespace Obi.UserControls
             this.mLabel.Location = new System.Drawing.Point(3, 3);
             this.mLabel.Margin = new System.Windows.Forms.Padding(3);
             this.mLabel.Name = "mLabel";
-            this.mLabel.Size = new System.Drawing.Size(63, 13);
+            this.mLabel.Size = new System.Drawing.Size(66, 12);
             this.mLabel.TabIndex = 4;
             this.mLabel.Text = "(annotation)";
             this.mLabel.Click += new System.EventHandler(this.AnnotationBlock_Click);
@@ -67,7 +67,7 @@ namespace Obi.UserControls
             // 
             // AnnotationBlock
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.PowderBlue;
@@ -75,7 +75,8 @@ namespace Obi.UserControls
             this.Controls.Add(this.mRenameBox);
             this.Margin = new System.Windows.Forms.Padding(0, 0, 1, 0);
             this.Name = "AnnotationBlock";
-            this.Size = new System.Drawing.Size(72, 20);
+            this.Size = new System.Drawing.Size(72, 18);
+            this.Load += new System.EventHandler(this.AnnotationBlock_Load);
             this.Click += new System.EventHandler(this.AnnotationBlock_Click);
             this.ResumeLayout(false);
             this.PerformLayout();
