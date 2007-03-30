39954e1e44965cd939ff5eb31e3c00fe13bec9be
avneeshsingh <avneeshsingh@f04d5631-4067-4348-862e-39f3aacb62c1>
2007-03-30 09:01:42 +0000

    Fix to post split display bugg i.e. new audio was appended after last phrase in section instead of current phrase: currently resynchronising done, would be better to insert after split.

--- trunk/Obi/Obi/UserControls/StripManagerPanel.Blocks.cs
+++ trunk/Obi/Obi/UserControls/StripManagerPanel.Blocks.cs
@@ -228,9 +228,13 @@ namespace Obi.UserControls
                 NodeSelection originalSelection = mProjectPanel.CurrentSelection;
                 if (dialog.ShowDialog() == DialogResult.OK)
                 {
-                    if ( dialog.ResultAsset != null ) 
-                    mProjectPanel.CurrentSelection =
-                        new NodeSelection(mProjectPanel.Project.Split(phrase, dialog.ResultAsset), this);
+                    if (dialog.ResultAsset != null)
+                    {
+                        PhraseNode SplitPhraseNode = mProjectPanel.Project.Split(phrase, dialog.ResultAsset ) ;
+                        SynchronizeWithCoreTree(SplitPhraseNode.ParentSection);
+                        mProjectPanel.CurrentSelection =
+                            new NodeSelection(SplitPhraseNode , this);
+                    }
                 }
                 else
                 {
@@ -261,6 +265,7 @@ namespace Obi.UserControls
                 mProjectPanel.TransportBar.Enabled = true;
                 if (split != null)
                 {
+                    SynchronizeWithCoreTree(split.ParentSection);
                     mProjectPanel.CurrentSelection = new NodeSelection(split, this);
                     mProjectPanel.TransportBar.Play();
                 }
