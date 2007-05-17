13f6891f8d82ebca88b44bfb15d5eae49c90c8f5
julienq <julienq@f04d5631-4067-4348-862e-39f3aacb62c1>
2007-05-17 08:49:49 +0000

    Adding forgotten file (oops)

---
+++ trunk/Obi/Obi/Commands/Node/MarkSectionHeading.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Obi.Commands.Node
+{
+    public class MarkSectionHeading : Command
+    {
+        PhraseNode mNewHeading;  // new section heading
+        PhraseNode mOldHeading;  // previous section heading
+
+        public MarkSectionHeading(PhraseNode newHeading, PhraseNode oldHeading)
+        {
+            mNewHeading = newHeading;
+            mOldHeading = oldHeading;
+        }
+
+        public override string Label
+        {
+            get
+            {
+                return Localizer.Message(mNewHeading == null ?
+                    "unmark_section_heading_command_label" :
+                    "mark_section_heading_command_label");
+            }
+        }
+
+        public override void Do()
+        {
+            if (mNewHeading == null)
+            {
+                mOldHeading.Project.UnmakePhraseHeading(mOldHeading.ParentSection);
+            }
+            else
+            {
+                mNewHeading.Project.MakePhraseHeading(mNewHeading);
+            }
+        }
+
+        public override void Undo()
+        {
+            if (mOldHeading == null)
+            {
+                mNewHeading.Project.UnmakePhraseHeading(mNewHeading.ParentSection);
+            }
+            else
+            {
+                mOldHeading.Project.MakePhraseHeading(mOldHeading);
+            }
+        }
+    }
+}
