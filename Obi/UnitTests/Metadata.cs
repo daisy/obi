f80e423100f1c3123a66a5ee330aff95fe6c435a
Julien Quint <722235+julienq@users.noreply.github.com>
2007-06-08 03:36:48 +0000

    Adding unit tests (at last.) Although only one so far.

---
+++ Obi/UnitTests/Metadata.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Obi;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class Metadata
+    {
+        [Test]
+        public void XukVersion()
+        {
+            UserProfile profile = new UserProfile();
+            profile.Culture = new System.Globalization.CultureInfo("en");
+            profile.Name = "Obird";
+            profile.Organization = "The Urakawa Project";
+            Project project = Project.BlankProject();
+            project.Create(System.IO.Path.GetTempFileName(), "Test project", "test_id", profile, false);
+            Assert.AreEqual(project.XukVersion, Project.CURRENT_XUK_VERSION);
+        }
+    }
+}
