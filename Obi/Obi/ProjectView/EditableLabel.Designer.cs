952e2dfec325bacf5a3a831feb8f7582ddaf7e3a
Julien Quint <722235+julienq@users.noreply.github.com>
2007-09-14 09:47:55 +0000

    Added editable lable widget, to be used with all items in the strips view.

---
+++ Obi/Obi/ProjectView/EditableLabel.Designer.cs
@@ -0,0 +1,108 @@
+namespace Obi.ProjectView
+{
+    partial class EditableLabel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mLabel = new System.Windows.Forms.Label();
+            this.mTextBox = new System.Windows.Forms.TextBox();
+            this.mOKButton = new System.Windows.Forms.Button();
+            this.mCancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // mLabel
+            // 
+            this.mLabel.AutoSize = true;
+            this.mLabel.Location = new System.Drawing.Point(3, 3);
+            this.mLabel.Margin = new System.Windows.Forms.Padding(3);
+            this.mLabel.Name = "mLabel";
+            this.mLabel.Size = new System.Drawing.Size(35, 13);
+            this.mLabel.TabIndex = 0;
+            this.mLabel.Text = "label1";
+            this.mLabel.Click += new System.EventHandler(this.mLabel_Click);
+            // 
+            // mTextBox
+            // 
+            this.mTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.mTextBox.BackColor = System.Drawing.SystemColors.Info;
+            this.mTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mTextBox.Location = new System.Drawing.Point(3, 3);
+            this.mTextBox.Name = "mTextBox";
+            this.mTextBox.Size = new System.Drawing.Size(144, 20);
+            this.mTextBox.TabIndex = 1;
+            this.mTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.mTextBox_KeyDown);
+            // 
+            // mOKButton
+            // 
+            this.mOKButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mOKButton.Location = new System.Drawing.Point(3, 29);
+            this.mOKButton.Name = "mOKButton";
+            this.mOKButton.Size = new System.Drawing.Size(35, 23);
+            this.mOKButton.TabIndex = 2;
+            this.mOKButton.Text = "&OK";
+            this.mOKButton.UseVisualStyleBackColor = true;
+            this.mOKButton.Click += new System.EventHandler(this.mOKButton_Click);
+            // 
+            // mCancelButton
+            // 
+            this.mCancelButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mCancelButton.Location = new System.Drawing.Point(44, 29);
+            this.mCancelButton.Name = "mCancelButton";
+            this.mCancelButton.Size = new System.Drawing.Size(50, 23);
+            this.mCancelButton.TabIndex = 3;
+            this.mCancelButton.Text = "&Cancel";
+            this.mCancelButton.UseVisualStyleBackColor = true;
+            this.mCancelButton.Click += new System.EventHandler(this.mCancelButton_Click);
+            // 
+            // EditableLabel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Thistle;
+            this.Controls.Add(this.mCancelButton);
+            this.Controls.Add(this.mOKButton);
+            this.Controls.Add(this.mTextBox);
+            this.Controls.Add(this.mLabel);
+            this.Margin = new System.Windows.Forms.Padding(0);
+            this.Name = "EditableLabel";
+            this.Size = new System.Drawing.Size(150, 55);
+            this.Load += new System.EventHandler(this.EditableLabel_Load);
+            this.Click += new System.EventHandler(this.EditableLabel_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label mLabel;
+        private System.Windows.Forms.TextBox mTextBox;
+        private System.Windows.Forms.Button mOKButton;
+        private System.Windows.Forms.Button mCancelButton;
+    }
+}
---
+++ Obi/Obi/ProjectView/Strip.Designer.cs
@@ -0,0 +1,48 @@
+namespace Obi.ProjectView
+{
+    partial class Strip
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Strip
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Margin = new System.Windows.Forms.Padding(0);
+            this.Name = "Strip";
+            this.Padding = new System.Windows.Forms.Padding(3);
+            this.Size = new System.Drawing.Size(244, 150);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+    }
+}
