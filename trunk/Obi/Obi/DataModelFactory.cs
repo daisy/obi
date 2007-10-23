2e364671a510bcd3cddb37576556e97d8da59d23
julienq <julienq@f04d5631-4067-4348-862e-39f3aacb62c1>
2007-10-23 08:37:24 +0000

    Updated DataModelFactory to follow Ole's suggestions.

--- trunk/Obi/Obi/DataModelFactory.cs
+++ trunk/Obi/Obi/DataModelFactory.cs
@@ -27,55 +27,50 @@ namespace Obi
             }
         }
 
+        // TODO: what about a custom data manager?
+
         public override urakawa.Presentation createPresentation()
         {
             return createPresentation(typeof(Obi.Presentation).Name, NS);
         }
 
-        // TODO: what about a custom data manager?
-
         public override urakawa.Presentation createPresentation(string localName, string namespaceUri)
         {
-            if (namespaceUri != NS || localName != typeof(Obi.Presentation).Name)
-            {
-                throw new Exception(String.Format("Cannot create presentation for QName `{0}:{1}'",
-                    namespaceUri, localName));
-            }
-            Obi.Presentation presentation = new Obi.Presentation();
-            presentation.setTreeNodeFactory(new ObiNodeFactory());
-            presentation.setPropertyFactory(new ObiPropertyFactory());
-            presentation.setUndoRedoManager(new Commands.UndoRedoManager());
-            return presentation;
+            return namespaceUri == NS && localName == typeof(Obi.Presentation).Name ?
+                new Obi.Presentation() : base.createPresentation(localName, namespaceUri);
+        }
+
+        public override urakawa.property.PropertyFactory createPropertyFactory()
+        {
+            return createPropertyFactory(typeof(ObiPropertyFactory).Name, NS);
         }
 
         public override urakawa.property.PropertyFactory createPropertyFactory(string localName, string namespaceUri)
         {
-            if (namespaceUri != NS && localName != typeof(ObiPropertyFactory).Name)
-            {
-                throw new Exception(String.Format("Cannot create property factory for QName `{0}:{1}'",
-                    namespaceUri, localName));
-            }
-            return new ObiPropertyFactory();
+            return namespaceUri == NS && localName == typeof(ObiPropertyFactory).Name ?
+                new ObiPropertyFactory() : base.createPropertyFactory(localName, namespaceUri);
+        }
+
+        public override urakawa.core.TreeNodeFactory createTreeNodeFactory()
+        {
+            return createTreeNodeFactory(typeof(ObiNodeFactory).Name, NS);
         }
 
         public override urakawa.core.TreeNodeFactory createTreeNodeFactory(string localName, string namespaceUri)
         {
-            if (namespaceUri != NS && localName != typeof(ObiNodeFactory).Name)
-            {
-                throw new Exception(String.Format("Cannot create tree node factory for QName `{0}:{1}'",
-                    namespaceUri, localName));
-            }
-            return new ObiNodeFactory();
+            return namespaceUri == NS && localName == typeof(ObiNodeFactory).Name ?
+                new ObiNodeFactory() : base.createTreeNodeFactory(localName, namespaceUri);
+        }
+
+        public override urakawa.undo.UndoRedoManager createUndoRedoManager()
+        {
+            return createUndoRedoManager(typeof(Commands.UndoRedoManager).Name, NS);
         }
 
         public override urakawa.undo.UndoRedoManager createUndoRedoManager(string localName, string namespaceUri)
         {
-            if (namespaceUri != NS && localName != typeof(Commands.UndoRedoManager).Name)
-            {
-                throw new Exception(String.Format("Cannot create undo/redo manager for QName `{0}:{1}'",
-                    namespaceUri, localName));
-            }
-            return new Commands.UndoRedoManager();
+            return namespaceUri == NS && localName == typeof(Commands.UndoRedoManager).Name ?
+                new Commands.UndoRedoManager() : base.createUndoRedoManager(localName, namespaceUri);
         }
     }
 }
