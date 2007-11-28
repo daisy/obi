d4a0fbc4a5b2d7f844143f123314588d569aba38
julienq <julienq@f04d5631-4067-4348-862e-39f3aacb62c1>
2007-11-28 08:35:18 +0000

    Merge blocks.

---
+++ trunk/Obi/Obi/Commands/Node/MergeAudio.cs
@@ -0,0 +1,39 @@
+namespace Obi.Commands.Node
+{
+    class MergeAudio: Command
+    {
+        private PhraseNode mNode;
+        private PhraseNode mNextNode;
+        private urakawa.media.timing.Time mSplitPoint;
+
+        public MergeAudio(ProjectView.ProjectView view): base(view)
+        {
+            mNode = view.SelectedBlockNode;
+            mNextNode = mNode.ParentAs<ObiNode>().PhraseChild(mNode.Index + 1);
+            mSplitPoint = new urakawa.media.timing.Time(mNode.Audio.getDuration().getTimeDeltaAsMillisecondFloat());
+        }
+
+        public override string getShortDescription() { return Localizer.Message("merge_block_with_next"); }
+
+        /// <summary>
+        /// Merge the audio of a node with the next, and remove next.
+        /// </summary>
+        public static void Merge(PhraseNode node, PhraseNode next)
+        {
+            next.Detach();
+            node.MergeAudioWith(next.Audio);
+        }
+
+        public override void execute()
+        {
+            base.execute();
+            Merge(mNode, mNextNode);
+        }
+
+        public override void unExecute()
+        {
+            SplitAudio.Split(mNode, mSplitPoint);
+            base.unExecute();
+        }
+    }
+}
