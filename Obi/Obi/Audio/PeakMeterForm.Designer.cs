c35a1ab11d2d86a9ae5ff414f4ff7a5e3da65f17
oleha <oleha@f04d5631-4067-4348-862e-39f3aacb62c1>
2007-12-04 09:54:10 +0000

    Added minimal size to GraphicalPeakMeter (100;40) to avoid exceptions when making the containing for very small

--- Obi/Obi/Audio/PeakMeterForm.Designer.cs
+++ Obi/Obi/Audio/PeakMeterForm.Designer.cs
@@ -28,30 +28,31 @@ namespace Obi.Audio
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.mGraphicalPeakMeter = new Obi.UserControls.GraphicalPeakMeter();
-			this.SuspendLayout();
-			// 
-			// mGraphicalPeakMeter
-			// 
-			this.mGraphicalPeakMeter.BarPaddingToHeightRatio = 0.075F;
-			this.mGraphicalPeakMeter.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.mGraphicalPeakMeter.FontToHeightRatio = 0.075F;
-			this.mGraphicalPeakMeter.FontToWidthRatio = 0.03F;
-			this.mGraphicalPeakMeter.Location = new System.Drawing.Point(0, 0);
-			this.mGraphicalPeakMeter.Name = "mGraphicalPeakMeter";
-			this.mGraphicalPeakMeter.Size = new System.Drawing.Size(449, 102);
-			this.mGraphicalPeakMeter.SourceVuMeter = null;
-			this.mGraphicalPeakMeter.TabIndex = 0;
-			// 
-			// PeakMeterForm
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(449, 102);
-			this.Controls.Add(this.mGraphicalPeakMeter);
-			this.Name = "PeakMeterForm";
-			this.Text = "Peak Meter";
-			this.ResumeLayout(false);
+            this.mGraphicalPeakMeter = new Obi.UserControls.GraphicalPeakMeter();
+            this.SuspendLayout();
+            // 
+            // mGraphicalPeakMeter
+            // 
+            this.mGraphicalPeakMeter.BarPaddingToHeightRatio = 0.075F;
+            this.mGraphicalPeakMeter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mGraphicalPeakMeter.FontToHeightRatio = 0.075F;
+            this.mGraphicalPeakMeter.FontToWidthRatio = 0.03F;
+            this.mGraphicalPeakMeter.Location = new System.Drawing.Point(0, 0);
+            this.mGraphicalPeakMeter.MinimumSize = new System.Drawing.Size(100, 40);
+            this.mGraphicalPeakMeter.Name = "mGraphicalPeakMeter";
+            this.mGraphicalPeakMeter.Size = new System.Drawing.Size(307, 119);
+            this.mGraphicalPeakMeter.SourceVuMeter = null;
+            this.mGraphicalPeakMeter.TabIndex = 0;
+            // 
+            // PeakMeterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 119);
+            this.Controls.Add(this.mGraphicalPeakMeter);
+            this.Name = "PeakMeterForm";
+            this.Text = "Peak Meter";
+            this.ResumeLayout(false);
 
 		}
 
