88cb4f761c68cdb5d85ea6735d06edb22acdee70
julienq <julienq@f04d5631-4067-4348-862e-39f3aacb62c1>
2008-03-08 16:12:59 +0000

    Made 0.10.4 release.

--- trunk/Obi/Obi/Properties/AssemblyInfo.cs
+++ trunk/Obi/Obi/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.10.3.*")]
+[assembly: AssemblyVersion("0.10.4.*")]
 
 // There is a warning about this but it is harmless (1607)
 [assembly: AssemblyInformationalVersion("1.0 alpha")]
