7b966040634389b1849bc2ab72f75821318f22db
avneeshsingh <avneeshsingh@f04d5631-4067-4348-862e-39f3aacb62c1>
2008-03-13 12:56:16 +0000

    Fix to bugg: Keyboard focus is lost after deleting a section or phrase.

--- trunk/Obi/Obi/Commands/Node/Delete.cs
+++ trunk/Obi/Obi/Commands/Node/Delete.cs
@@ -25,8 +25,10 @@ namespace Obi.Commands.Node
 
         public override void execute()
         {
+            NodeSelection AfterDeleteSelection = PostDeleteSelection();
             mNode.Detach();
-            if (UpdateSelection) View.Selection = null;
+            //if (UpdateSelection) View.Selection = null;
+            if (UpdateSelection) View.Selection = AfterDeleteSelection;
         }
 
         public override void unExecute()
@@ -34,5 +36,40 @@ namespace Obi.Commands.Node
             mParent.Insert(mNode, mIndex);
             base.unExecute();
         }
+
+        // Avn: function to determine selection after delete.
+        private NodeSelection PostDeleteSelection()
+        {
+            ObiNode AfterDeleteSelectionNode = null;
+            if (mNode is SectionNode) // if selected node is section select appropriate section after delete
+            {
+                SectionNode node = (SectionNode)mNode;
+                AfterDeleteSelectionNode = node.NextSection;
+
+                if (AfterDeleteSelectionNode == null)
+                    AfterDeleteSelectionNode = node.PrecedingSection; // bugg in proceeding section property, sometimes triggeres out of range index exception
+            }
+            else // else selected node is empty node or phrase node so after delete select appropriate  empty/phrase node or parent section if there are no children left.
+            {
+                EmptyNode ENode = (EmptyNode)mNode;
+                SectionNode Parent = ENode.ParentAs<SectionNode>();
+                int PhraseIndex = ENode.Index;
+                if (Parent.PhraseChildCount >= PhraseIndex + 2) // atleast one node is  there after deleted node so select it
+                    AfterDeleteSelectionNode = Parent.PhraseChild(PhraseIndex + 1);
+                else if (Parent.PhraseChildCount > 1) // no node after deleted node but there are some nodes before deleted node so select previous phrase
+                {
+                    AfterDeleteSelectionNode = Parent.PhraseChild(PhraseIndex - 1);
+                }
+                else // there is no phrase in section, move selection to parent section
+                {
+                    AfterDeleteSelectionNode = Parent;
+                }
+            }
+            if (AfterDeleteSelectionNode != null)
+                return new NodeSelection(AfterDeleteSelectionNode, View.Selection.Control);
+            else
+                return null;
+        }
+
     }
 }
