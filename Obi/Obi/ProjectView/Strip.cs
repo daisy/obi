b7e25a9ad8c074cb03d265ebd6d975fb7f02a5ff
Avneesh Singh <13777742+avneeshsingh@users.noreply.github.com>
2008-05-22 08:14:17 +0000

    fix to bugg 1967718: Screenreader speak too much info in section in contents view

--- Obi/Obi/ProjectView/Strip.cs
+++ Obi/Obi/ProjectView/Strip.cs
@@ -411,7 +411,16 @@ namespace Obi.ProjectView
         }
 
         // Set verbose accessible name for the strip 
-        private void SetAccessibleName() { mLabel.AccessibleName = mNode.ToString(); }
+        private void SetAccessibleName() 
+        {
+                        mLabel.AccessibleName = string.Concat ( mNode.Used ? "" : Localizer.Message("unused"),
+                mNode.Label,
+                mNode.Duration== 0.0 ? Localizer.Message("empty") : string.Format(Localizer.Message("time_in_seconds"), mNode.Duration / 1000.0),
+                string.Format(Localizer.Message("section_level_to_string"), mNode.IsRooted ? mNode.Level: 0),
+                mNode.PhraseChildCount == 0 ? "" :
+                    mNode.PhraseChildCount== 1 ? Localizer.Message("section_one_phrase_to_string") :
+                        string.Format(Localizer.Message("section_phrases_to_string"), mNode.PhraseChildCount));
+                }
 
         // Toggle selection when clicking
         private void Strip_Click(object sender, EventArgs e)
