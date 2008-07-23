828586254b26eed1d7416e1f12c9bfa0c69c1e74
Julien Quint <722235+julienq@users.noreply.github.com>
2008-07-23 22:45:41 +0000

    Metadata view resizing on its way... more complicated than I think it should be :(

--- Obi/Obi/ProjectView/MetadataView.Designer.cs
+++ Obi/Obi/ProjectView/MetadataView.Designer.cs
@@ -34,8 +34,8 @@ namespace Obi.ProjectView
             this.mContentTextbox = new System.Windows.Forms.TextBox();
             this.mUpdateButton = new System.Windows.Forms.Button();
             this.mNameTextbox = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.mNameLabel = new System.Windows.Forms.Label();
+            this.mContentLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // mMetadataListView
@@ -50,11 +50,11 @@ namespace Obi.ProjectView
             this.mNameColumn,
             this.mContentColumn});
             this.mMetadataListView.FullRowSelect = true;
-            this.mMetadataListView.Location = new System.Drawing.Point(4, 4);
+            this.mMetadataListView.Location = new System.Drawing.Point(0, 0);
             this.mMetadataListView.Margin = new System.Windows.Forms.Padding(4);
             this.mMetadataListView.MultiSelect = false;
             this.mMetadataListView.Name = "mMetadataListView";
-            this.mMetadataListView.Size = new System.Drawing.Size(296, 237);
+            this.mMetadataListView.Size = new System.Drawing.Size(311, 321);
             this.mMetadataListView.TabIndex = 1;
             this.mMetadataListView.UseCompatibleStateImageBehavior = false;
             this.mMetadataListView.View = System.Windows.Forms.View.Details;
@@ -77,65 +77,68 @@ namespace Obi.ProjectView
             this.mContentTextbox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.mContentTextbox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.mContentTextbox.Location = new System.Drawing.Point(75, 280);
-            this.mContentTextbox.Margin = new System.Windows.Forms.Padding(4);
+            this.mContentTextbox.Location = new System.Drawing.Point(64, 359);
+            this.mContentTextbox.Margin = new System.Windows.Forms.Padding(4, 4, 0, 4);
             this.mContentTextbox.Name = "mContentTextbox";
-            this.mContentTextbox.Size = new System.Drawing.Size(225, 22);
+            this.mContentTextbox.Size = new System.Drawing.Size(247, 22);
             this.mContentTextbox.TabIndex = 4;
             this.mContentTextbox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.mContentTextbox_KeyDown);
             // 
             // mUpdateButton
             // 
             this.mUpdateButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.mUpdateButton.AutoSize = true;
+            this.mUpdateButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.mUpdateButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.mUpdateButton.Location = new System.Drawing.Point(201, 312);
-            this.mUpdateButton.Margin = new System.Windows.Forms.Padding(4);
+            this.mUpdateButton.Location = new System.Drawing.Point(246, 389);
+            this.mUpdateButton.Margin = new System.Windows.Forms.Padding(4, 4, 0, 0);
             this.mUpdateButton.Name = "mUpdateButton";
-            this.mUpdateButton.Size = new System.Drawing.Size(100, 31);
+            this.mUpdateButton.Size = new System.Drawing.Size(65, 28);
             this.mUpdateButton.TabIndex = 5;
             this.mUpdateButton.Text = "&Update";
             this.mUpdateButton.UseVisualStyleBackColor = true;
             this.mUpdateButton.Click += new System.EventHandler(this.mCommitButton_Click);
+            this.mUpdateButton.SizeChanged += new System.EventHandler(this.mUpdateButton_SizeChanged);
             // 
             // mNameTextbox
             // 
             this.mNameTextbox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.mNameTextbox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.mNameTextbox.Location = new System.Drawing.Point(75, 248);
-            this.mNameTextbox.Margin = new System.Windows.Forms.Padding(4);
+            this.mNameTextbox.Location = new System.Drawing.Point(64, 329);
+            this.mNameTextbox.Margin = new System.Windows.Forms.Padding(4, 4, 0, 4);
             this.mNameTextbox.Name = "mNameTextbox";
-            this.mNameTextbox.Size = new System.Drawing.Size(225, 22);
+            this.mNameTextbox.Size = new System.Drawing.Size(247, 22);
             this.mNameTextbox.TabIndex = 3;
             // 
-            // label1
+            // mNameLabel
             // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(19, 250);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(48, 16);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "Name:";
+            this.mNameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.mNameLabel.AutoSize = true;
+            this.mNameLabel.Location = new System.Drawing.Point(8, 331);
+            this.mNameLabel.Margin = new System.Windows.Forms.Padding(0, 0, 4, 0);
+            this.mNameLabel.Name = "mNameLabel";
+            this.mNameLabel.Size = new System.Drawing.Size(48, 16);
+            this.mNameLabel.TabIndex = 7;
+            this.mNameLabel.Text = "&Name:";
             // 
-            // label2
+            // mContentLabel
             // 
-            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(11, 282);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 16);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "Content:";
+            this.mContentLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.mContentLabel.AutoSize = true;
+            this.mContentLabel.Location = new System.Drawing.Point(0, 361);
+            this.mContentLabel.Margin = new System.Windows.Forms.Padding(0, 0, 4, 0);
+            this.mContentLabel.Name = "mContentLabel";
+            this.mContentLabel.Size = new System.Drawing.Size(56, 16);
+            this.mContentLabel.TabIndex = 8;
+            this.mContentLabel.Text = "&Content:";
             // 
             // MetadataView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.Controls.Add(this.mContentLabel);
+            this.Controls.Add(this.mNameLabel);
             this.Controls.Add(this.mMetadataListView);
             this.Controls.Add(this.mNameTextbox);
             this.Controls.Add(this.mUpdateButton);
@@ -143,7 +146,7 @@ namespace Obi.ProjectView
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(0);
             this.Name = "MetadataView";
-            this.Size = new System.Drawing.Size(305, 347);
+            this.Size = new System.Drawing.Size(311, 417);
             this.VisibleChanged += new System.EventHandler(this.MetadataView_VisibleChanged);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -158,8 +161,8 @@ namespace Obi.ProjectView
         private System.Windows.Forms.TextBox mNameTextbox;
         private System.Windows.Forms.ColumnHeader mNameColumn;
         private System.Windows.Forms.ColumnHeader mContentColumn;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label mNameLabel;
+        private System.Windows.Forms.Label mContentLabel;
 
     }
 }
