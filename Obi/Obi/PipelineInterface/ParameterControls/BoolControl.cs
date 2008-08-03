cabf7e352a967a18305070e4188dfd73b080c85c
Avneesh Singh <13777742+avneeshsingh@users.noreply.github.com>
2008-08-03 07:42:25 +0000

    Updated Pipeline GUI controls

---
+++ Obi/Obi/PipelineInterface/ParameterControls/BoolControl.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Obi.PipelineInterface.ParameterControls
+{
+    public partial class BoolControl : BaseUserControl
+    {
+        public BoolControl()
+        {
+            InitializeComponent();
+                    }
+
+
+        public override void UpdateScriptParameterValue ()
+        {
+                     }
+
+            }
+}
