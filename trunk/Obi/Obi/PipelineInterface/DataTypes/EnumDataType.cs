a9f604aa3d24ca276076f4bd13fcfe485e1f2148
avneesh-singh <avneesh-singh@f04d5631-4067-4348-862e-39f3aacb62c1>
2008-08-03 07:43:51 +0000

    Updating Pipeline  datatypes for scripts

---
+++ trunk/Obi/Obi/PipelineInterface/DataTypes/EnumDataType.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections ;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.Windows.Forms;
+
+namespace Obi.PipelineInterface.DataTypes
+{
+    public class EnumDataType:IDataTypes
+    {
+        private List<string>  m_ValueList ;
+        private List<string> m_NiceNameList;
+        private int m_SelectedIndex;
+        public ScriptParameter m_Parameter ;
+
+        public EnumDataType (ScriptParameter p ,  XmlNode node )
+        {
+            m_Parameter = p;
+            m_ValueList = new List<string>();
+            m_NiceNameList = new List<string>();
+            m_SelectedIndex = -1;
+            PopulateListFromNode(node);
+
+            // select list node as default value if default  exists
+            if (p.ParameterValue != null && p.ParameterValue !="" 
+                && m_ValueList.Contains(p.ParameterValue ))
+                m_SelectedIndex = m_ValueList.BinarySearch(p.ParameterValue) ;
+        }
+
+        private void PopulateListFromNode(XmlNode DatatypeNode )
+        {
+            XmlNode EnumNode = DatatypeNode.FirstChild;
+                        XmlNodeList nodeList = EnumNode.SelectNodes("item");
+            
+            foreach (XmlNode n in nodeList)
+            {
+                                                                                                                                                                    m_ValueList.Add(n.Attributes.GetNamedItem("value").Value);
+                m_NiceNameList.Add (n.Attributes.GetNamedItem("nicename").Value );
+                            }
+        }
+
+
+        public List<string> GetValues { get { return m_ValueList; } }
+        public List<string> GetNiceNames { get { return m_NiceNameList; } }
+
+        public int SelectedIndex
+        {
+            get { return m_SelectedIndex;  }
+            set 
+            {
+                if (value >= 0 && value < m_ValueList.Count )
+                                    SetSelectedIndexAndUpdateScript( value  ) ;
+                                    else throw new System.Exception ("Index out of range") ;
+            }
+        }
+
+        public string SelectedItemValue
+        {
+            get { return m_ValueList[m_SelectedIndex]; }
+            set
+            {
+                if (value != null && m_ValueList.Contains(value))
+                    SetSelectedIndexAndUpdateScript( m_ValueList.BinarySearch(value) );
+                else throw new System.Exception("Not able to select item");
+            }
+        }
+
+
+        public string SelectedItemNiceName
+        {
+            get { return m_NiceNameList[m_SelectedIndex]; }
+            set
+            {
+                if (value != null && m_NiceNameList.Contains(value))
+                    SetSelectedIndexAndUpdateScript( m_NiceNameList.BinarySearch(value) ) ;
+                else throw new System.Exception("Not able to select item");
+            }
+        }
+
+        private bool SetSelectedIndexAndUpdateScript ( int Index)
+        {
+            if (Index > 0 && Index < m_ValueList.Count)
+            {
+                m_SelectedIndex = Index;
+                                m_Parameter.ParameterValue = SelectedItemValue;
+                return true;
+            }
+            else
+                return false;
+                    }
+        
+
+
+
+    }
+}
---
+++ trunk/Obi/Obi/PipelineInterface/DataTypes/IDataTypes.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Obi.PipelineInterface.DataTypes
+{
+    public interface IDataTypes
+    {
+
+         //T DataType <T>  Value ()
+        //{
+            //get;
+            //set;
+        //}
+
+    }
+}
