b33b1f9f21ef6a3a38e7e2627fe062f07c20fdc5
avneesh-singh <avneesh-singh@f04d5631-4067-4348-862e-39f3aacb62c1>
2008-08-20 11:21:40 +0000

    updated hindi resource files

--- trunk/Obi/Obi/Dialogs/PhraseProperties.Designer.cs
+++ trunk/Obi/Obi/Dialogs/PhraseProperties.Designer.cs
@@ -49,161 +49,106 @@ namespace Obi.Dialogs
             // 
             // m_lblParentSection
             // 
-            this.m_lblParentSection.AccessibleDescription = null;
-            this.m_lblParentSection.AccessibleName = null;
             resources.ApplyResources(this.m_lblParentSection, "m_lblParentSection");
-            this.m_lblParentSection.Font = null;
             this.m_lblParentSection.Name = "m_lblParentSection";
             // 
             // m_txtParentSection
             // 
-            this.m_txtParentSection.AccessibleDescription = null;
             resources.ApplyResources(this.m_txtParentSection, "m_txtParentSection");
-            this.m_txtParentSection.BackgroundImage = null;
             this.m_txtParentSection.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.m_txtParentSection.Font = null;
             this.m_txtParentSection.Name = "m_txtParentSection";
             this.m_txtParentSection.ReadOnly = true;
             // 
             // m_lblLocationInsideSection
             // 
-            this.m_lblLocationInsideSection.AccessibleDescription = null;
-            this.m_lblLocationInsideSection.AccessibleName = null;
             resources.ApplyResources(this.m_lblLocationInsideSection, "m_lblLocationInsideSection");
-            this.m_lblLocationInsideSection.Font = null;
             this.m_lblLocationInsideSection.Name = "m_lblLocationInsideSection";
             // 
             // m_txtLocationInsideSection
             // 
-            this.m_txtLocationInsideSection.AccessibleDescription = null;
             resources.ApplyResources(this.m_txtLocationInsideSection, "m_txtLocationInsideSection");
-            this.m_txtLocationInsideSection.BackgroundImage = null;
             this.m_txtLocationInsideSection.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.m_txtLocationInsideSection.Font = null;
             this.m_txtLocationInsideSection.Name = "m_txtLocationInsideSection";
             this.m_txtLocationInsideSection.ReadOnly = true;
             // 
             // m_lbParentsList
             // 
-            this.m_lbParentsList.AccessibleDescription = null;
             resources.ApplyResources(this.m_lbParentsList, "m_lbParentsList");
-            this.m_lbParentsList.BackgroundImage = null;
             this.m_lbParentsList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.m_lbParentsList.Font = null;
             this.m_lbParentsList.FormattingEnabled = true;
             this.m_lbParentsList.Name = "m_lbParentsList";
             // 
             // m_lblPhraseRole
             // 
-            this.m_lblPhraseRole.AccessibleDescription = null;
-            this.m_lblPhraseRole.AccessibleName = null;
             resources.ApplyResources(this.m_lblPhraseRole, "m_lblPhraseRole");
-            this.m_lblPhraseRole.Font = null;
             this.m_lblPhraseRole.Name = "m_lblPhraseRole";
             // 
             // m_comboPhraseRole
             // 
-            this.m_comboPhraseRole.AccessibleDescription = null;
             resources.ApplyResources(this.m_comboPhraseRole, "m_comboPhraseRole");
-            this.m_comboPhraseRole.BackgroundImage = null;
-            this.m_comboPhraseRole.Font = null;
             this.m_comboPhraseRole.FormattingEnabled = true;
             this.m_comboPhraseRole.Name = "m_comboPhraseRole";
             this.m_comboPhraseRole.SelectionChangeCommitted += new System.EventHandler(this.m_comboPhraseRole_SelectionChangeCommitted);
             // 
             // m_lblCustomClassName
             // 
-            this.m_lblCustomClassName.AccessibleDescription = null;
-            this.m_lblCustomClassName.AccessibleName = null;
             resources.ApplyResources(this.m_lblCustomClassName, "m_lblCustomClassName");
-            this.m_lblCustomClassName.Font = null;
             this.m_lblCustomClassName.Name = "m_lblCustomClassName";
             // 
             // m_txtCustomClassName
             // 
-            this.m_txtCustomClassName.AccessibleDescription = null;
             resources.ApplyResources(this.m_txtCustomClassName, "m_txtCustomClassName");
-            this.m_txtCustomClassName.BackgroundImage = null;
             this.m_txtCustomClassName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.m_txtCustomClassName.Font = null;
             this.m_txtCustomClassName.Name = "m_txtCustomClassName";
             // 
             // m_chkUsed
             // 
-            this.m_chkUsed.AccessibleDescription = null;
-            this.m_chkUsed.AccessibleName = null;
             resources.ApplyResources(this.m_chkUsed, "m_chkUsed");
-            this.m_chkUsed.BackgroundImage = null;
-            this.m_chkUsed.Font = null;
             this.m_chkUsed.Name = "m_chkUsed";
             this.m_chkUsed.UseVisualStyleBackColor = true;
             // 
             // m_chkToDo
             // 
-            this.m_chkToDo.AccessibleDescription = null;
-            this.m_chkToDo.AccessibleName = null;
             resources.ApplyResources(this.m_chkToDo, "m_chkToDo");
-            this.m_chkToDo.BackgroundImage = null;
-            this.m_chkToDo.Font = null;
             this.m_chkToDo.Name = "m_chkToDo";
             this.m_chkToDo.UseVisualStyleBackColor = true;
             // 
             // m_btnOk
             // 
-            this.m_btnOk.AccessibleDescription = null;
-            this.m_btnOk.AccessibleName = null;
             resources.ApplyResources(this.m_btnOk, "m_btnOk");
-            this.m_btnOk.BackgroundImage = null;
             this.m_btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.m_btnOk.Font = null;
             this.m_btnOk.Name = "m_btnOk";
             this.m_btnOk.UseVisualStyleBackColor = true;
             // 
             // m_btnCancel
             // 
-            this.m_btnCancel.AccessibleDescription = null;
-            this.m_btnCancel.AccessibleName = null;
             resources.ApplyResources(this.m_btnCancel, "m_btnCancel");
-            this.m_btnCancel.BackgroundImage = null;
             this.m_btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.m_btnCancel.Font = null;
             this.m_btnCancel.Name = "m_btnCancel";
             this.m_btnCancel.UseVisualStyleBackColor = true;
             // 
             // m_lblTimeLength
             // 
-            this.m_lblTimeLength.AccessibleDescription = null;
-            this.m_lblTimeLength.AccessibleName = null;
             resources.ApplyResources(this.m_lblTimeLength, "m_lblTimeLength");
-            this.m_lblTimeLength.Font = null;
             this.m_lblTimeLength.Name = "m_lblTimeLength";
             // 
             // m_txtTimeLength
             // 
-            this.m_txtTimeLength.AccessibleDescription = null;
             resources.ApplyResources(this.m_txtTimeLength, "m_txtTimeLength");
-            this.m_txtTimeLength.BackgroundImage = null;
             this.m_txtTimeLength.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.m_txtTimeLength.Font = null;
             this.m_txtTimeLength.Name = "m_txtTimeLength";
             this.m_txtTimeLength.ReadOnly = true;
             // 
             // label1
             // 
-            this.label1.AccessibleDescription = null;
-            this.label1.AccessibleName = null;
             resources.ApplyResources(this.label1, "label1");
-            this.label1.Font = null;
             this.label1.Name = "label1";
             // 
             // PhraseProperties
             // 
             this.AcceptButton = this.m_btnOk;
-            this.AccessibleDescription = null;
-            this.AccessibleName = null;
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = null;
             this.CancelButton = this.m_btnCancel;
             this.Controls.Add(this.label1);
             this.Controls.Add(this.m_txtTimeLength);
@@ -221,7 +166,6 @@ namespace Obi.Dialogs
             this.Controls.Add(this.m_txtParentSection);
             this.Controls.Add(this.m_lblParentSection);
             this.Controls.Add(this.m_lbParentsList);
-            this.Icon = null;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "PhraseProperties";
