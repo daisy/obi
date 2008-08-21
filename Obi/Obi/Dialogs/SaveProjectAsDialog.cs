1e85adfd3724ad5e758b0d14e260abda75f5c7bd
Avneesh Singh <13777742+avneeshsingh@users.noreply.github.com>
2008-08-21 13:17:05 +0000

    save project as dialog created

---
+++ Obi/Obi/Dialogs/SaveProjectAsDialog.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Obi.Dialogs
+{
+    public partial class SaveProjectAsDialog : Form
+    {
+        private string m_OriginalProjectPath;
+        private string m_NewProjectDirectoryPath;
+
+        public SaveProjectAsDialog()
+        {
+            InitializeComponent();
+                    }
+
+        public SaveProjectAsDialog( string ProjectDirectoryPath):this ()
+        {
+            DirectoryInfo DirInfo = new DirectoryInfo(ProjectDirectoryPath);
+                        m_txtProjectDirectoryName.Text = DirInfo.Name;
+            m_txtParentDirectory.Text =DirInfo.Parent.FullName ;
+            m_OriginalProjectPath = ProjectDirectoryPath;
+        }
+        
+
+        public string NewProjectDirectoryPath
+        {
+            get { return m_NewProjectDirectoryPath; }
+                    }
+
+        private void m_btnBrowseParentDirectory_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog () == DialogResult.OK)
+            {
+                m_txtParentDirectory.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void m_btnOk_Click(object sender, EventArgs e)
+        {
+            string DirPath = Path.Combine(m_txtParentDirectory.Text, m_txtProjectDirectoryName.Text);
+
+            if (new DirectoryInfo(m_OriginalProjectPath).FullName == new DirectoryInfo(DirPath).FullName)
+            {
+                MessageBox.Show("Cannot save in the same directory. Please choose some other location");
+                                return;
+            }
+
+            try
+            {
+                Directory.CreateDirectory(DirPath);
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                DialogResult = DialogResult.None;
+                return;
+            }
+            m_NewProjectDirectoryPath = DirPath;
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void m_btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
