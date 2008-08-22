dd867d95498fae600cab95bce0d31c683766811c
Julien Quint <722235+julienq@users.noreply.github.com>
2008-08-22 09:36:45 +0000

    Split preserving roles and attributes.

--- Obi/Obi/Commands/Node/SplitAudio.cs
+++ Obi/Obi/Commands/Node/SplitAudio.cs
@@ -10,16 +10,18 @@ namespace Obi.Commands.Node
         private Time mSplitTime;        // split point (begin/cursor)
 
 
-        /// <summary>
-        /// Create a split command to split the selected node at the given position.
-        /// </summary>
-        public SplitAudio(ProjectView.ProjectView view, PhraseNode node, double time): base(view)
+        // Create a split command to split the selected node at the given position.
+        // Use only through GetSplitCommand.
+        private SplitAudio(ProjectView.ProjectView view, PhraseNode node, double time): base(view)
         {
             mNode = node;
             mNodeAfter = view.Presentation.CreatePhraseNode();
             mSplitTime = new Time(time);
             Label = Localizer.Message("split_phrase");
         }
+
+        public PhraseNode Node { get { return mNode; } }
+        public PhraseNode NodeAfter { get { return mNodeAfter; } }
 
         /// <summary>
         /// Create a split command for the current selection.
@@ -36,14 +38,31 @@ namespace Obi.Commands.Node
                 {
                     urakawa.undo.CompositeCommand command =
                         view.Presentation.CreateCompositeCommand(Localizer.Message("split_phrase"));
-                    if (end > 0.0) command.append(new SplitAudio(view, phrase, end));
-                    if (begin > 0.0) command.append(new SplitAudio(view, phrase, begin));
+                    if (end > 0.0) AppendSplitCommandWithProperties(view, command, phrase, end, false);
+                    if (begin > 0.0) AppendSplitCommandWithProperties(view, command, phrase, begin,
+                        view.Selection is AudioSelection && !((AudioSelection)view.Selection).AudioRange.HasCursor);
                     if (command.getCount() > 0) return command;
                 }
             }
             return null;
         }
 
+        // Create a split command preserving used/TODO status, and optionally transferring the role to the next node
+        private static void AppendSplitCommandWithProperties(ProjectView.ProjectView view, urakawa.undo.CompositeCommand command,
+            PhraseNode phrase, double time, bool transferRole)
+        {
+            SplitAudio split = new SplitAudio(view, phrase, time);
+            if (split.Node.TODO) command.append(new Commands.Node.ToggleNodeTo_Do(view, split.NodeAfter));
+            if (!split.Node.Used) command.append(new Commands.Node.ToggleNodeUsed(view, split.NodeAfter));
+            command.append(split);
+            if (transferRole && phrase.NodeKind != EmptyNode.Kind.Plain)
+            {
+                command.append(new Commands.Node.ChangeCustomType(view, phrase, EmptyNode.Kind.Plain));
+                command.append(new Commands.Node.ChangeCustomType(view, split.NodeAfter, phrase.NodeKind, phrase.CustomClass));
+            }
+        }
+
+        // Perform a split given a time and a node with no audio, optionally updating the selection in the view afterward.
         public static void Split(ProjectView.ProjectView view, PhraseNode node, PhraseNode nodeAfter, Time splitTime,
             bool updateSelection)
         {
