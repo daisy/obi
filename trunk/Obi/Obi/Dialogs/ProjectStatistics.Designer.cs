c7347ce50568f0e6e2a22cbb32ac7a26b22d0f38
avneesh-singh <avneesh-singh@f04d5631-4067-4348-862e-39f3aacb62c1>
2008-09-03 08:44:55 +0000

    minor update to project statistics dialog

--- trunk/Obi/Obi/Dialogs/ProjectStatistics.Designer.cs
+++ trunk/Obi/Obi/Dialogs/ProjectStatistics.Designer.cs
@@ -28,159 +28,118 @@ namespace Obi.Dialogs
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProjectStatistics));
-            this.m_lblDuration = new System.Windows.Forms.Label();
-            this.m_txtDuration = new System.Windows.Forms.TextBox();
-            this.m_lblSectionCount = new System.Windows.Forms.Label();
-            this.m_txtSectionsCount = new System.Windows.Forms.TextBox();
-            this.m_lblPhraseCount = new System.Windows.Forms.Label();
-            this.m_txtPhraseCount = new System.Windows.Forms.TextBox();
-            this.m_lblPageCount = new System.Windows.Forms.Label();
-            this.m_txtPageCount = new System.Windows.Forms.TextBox();
-            this.m_btnOk = new System.Windows.Forms.Button();
-            this.m_lblTitle = new System.Windows.Forms.Label();
-            this.m_txtTitle = new System.Windows.Forms.TextBox();
-            this.mCancelButton = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // m_lblDuration
-            // 
-            this.m_lblDuration.AccessibleDescription = null;
-            this.m_lblDuration.AccessibleName = null;
-            resources.ApplyResources(this.m_lblDuration, "m_lblDuration");
-            this.m_lblDuration.Font = null;
-            this.m_lblDuration.Name = "m_lblDuration";
-            // 
-            // m_txtDuration
-            // 
-            this.m_txtDuration.AccessibleDescription = null;
-            resources.ApplyResources(this.m_txtDuration, "m_txtDuration");
-            this.m_txtDuration.BackgroundImage = null;
-            this.m_txtDuration.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.m_txtDuration.Font = null;
-            this.m_txtDuration.Name = "m_txtDuration";
-            this.m_txtDuration.ReadOnly = true;
-            // 
-            // m_lblSectionCount
-            // 
-            this.m_lblSectionCount.AccessibleDescription = null;
-            this.m_lblSectionCount.AccessibleName = null;
-            resources.ApplyResources(this.m_lblSectionCount, "m_lblSectionCount");
-            this.m_lblSectionCount.Font = null;
-            this.m_lblSectionCount.Name = "m_lblSectionCount";
-            // 
-            // m_txtSectionsCount
-            // 
-            this.m_txtSectionsCount.AccessibleDescription = null;
-            resources.ApplyResources(this.m_txtSectionsCount, "m_txtSectionsCount");
-            this.m_txtSectionsCount.BackgroundImage = null;
-            this.m_txtSectionsCount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.m_txtSectionsCount.Font = null;
-            this.m_txtSectionsCount.Name = "m_txtSectionsCount";
-            this.m_txtSectionsCount.ReadOnly = true;
-            // 
-            // m_lblPhraseCount
-            // 
-            this.m_lblPhraseCount.AccessibleDescription = null;
-            this.m_lblPhraseCount.AccessibleName = null;
-            resources.ApplyResources(this.m_lblPhraseCount, "m_lblPhraseCount");
-            this.m_lblPhraseCount.Font = null;
-            this.m_lblPhraseCount.Name = "m_lblPhraseCount";
-            // 
-            // m_txtPhraseCount
-            // 
-            this.m_txtPhraseCount.AccessibleDescription = null;
-            resources.ApplyResources(this.m_txtPhraseCount, "m_txtPhraseCount");
-            this.m_txtPhraseCount.BackgroundImage = null;
-            this.m_txtPhraseCount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.m_txtPhraseCount.Font = null;
-            this.m_txtPhraseCount.Name = "m_txtPhraseCount";
-            this.m_txtPhraseCount.ReadOnly = true;
-            // 
-            // m_lblPageCount
-            // 
-            this.m_lblPageCount.AccessibleDescription = null;
-            this.m_lblPageCount.AccessibleName = null;
-            resources.ApplyResources(this.m_lblPageCount, "m_lblPageCount");
-            this.m_lblPageCount.Font = null;
-            this.m_lblPageCount.Name = "m_lblPageCount";
-            // 
-            // m_txtPageCount
-            // 
-            this.m_txtPageCount.AccessibleDescription = null;
-            resources.ApplyResources(this.m_txtPageCount, "m_txtPageCount");
-            this.m_txtPageCount.BackgroundImage = null;
-            this.m_txtPageCount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.m_txtPageCount.Font = null;
-            this.m_txtPageCount.Name = "m_txtPageCount";
-            this.m_txtPageCount.ReadOnly = true;
-            // 
-            // m_btnOk
-            // 
-            this.m_btnOk.AccessibleDescription = null;
-            this.m_btnOk.AccessibleName = null;
-            resources.ApplyResources(this.m_btnOk, "m_btnOk");
-            this.m_btnOk.BackgroundImage = null;
-            this.m_btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.m_btnOk.Font = null;
-            this.m_btnOk.Name = "m_btnOk";
-            this.m_btnOk.UseVisualStyleBackColor = true;
-            // 
-            // m_lblTitle
-            // 
-            this.m_lblTitle.AccessibleDescription = null;
-            this.m_lblTitle.AccessibleName = null;
-            resources.ApplyResources(this.m_lblTitle, "m_lblTitle");
-            this.m_lblTitle.Name = "m_lblTitle";
-            // 
-            // m_txtTitle
-            // 
-            this.m_txtTitle.AccessibleDescription = null;
-            resources.ApplyResources(this.m_txtTitle, "m_txtTitle");
-            this.m_txtTitle.BackgroundImage = null;
-            this.m_txtTitle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.m_txtTitle.Font = null;
-            this.m_txtTitle.Name = "m_txtTitle";
-            // 
-            // mCancelButton
-            // 
-            this.mCancelButton.AccessibleDescription = null;
-            this.mCancelButton.AccessibleName = null;
-            resources.ApplyResources(this.mCancelButton, "mCancelButton");
-            this.mCancelButton.BackgroundImage = null;
-            this.mCancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.mCancelButton.Font = null;
-            this.mCancelButton.Name = "mCancelButton";
-            this.mCancelButton.UseVisualStyleBackColor = true;
-            // 
-            // ProjectStatistics
-            // 
-            this.AcceptButton = this.m_btnOk;
-            this.AccessibleDescription = null;
-            this.AccessibleName = null;
-            resources.ApplyResources(this, "$this");
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = null;
-            this.CancelButton = this.mCancelButton;
-            this.Controls.Add(this.mCancelButton);
-            this.Controls.Add(this.m_txtTitle);
-            this.Controls.Add(this.m_lblTitle);
-            this.Controls.Add(this.m_btnOk);
-            this.Controls.Add(this.m_txtPageCount);
-            this.Controls.Add(this.m_lblPageCount);
-            this.Controls.Add(this.m_txtPhraseCount);
-            this.Controls.Add(this.m_lblPhraseCount);
-            this.Controls.Add(this.m_txtSectionsCount);
-            this.Controls.Add(this.m_lblSectionCount);
-            this.Controls.Add(this.m_txtDuration);
-            this.Controls.Add(this.m_lblDuration);
-            this.Icon = null;
-            this.Name = "ProjectStatistics";
-            this.ShowIcon = false;
-            this.ShowInTaskbar = false;
-            this.Load += new System.EventHandler(this.ProjectStatistics_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+        System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager ( typeof ( ProjectStatistics ) );
+        this.m_lblDuration = new System.Windows.Forms.Label ();
+        this.m_txtDuration = new System.Windows.Forms.TextBox ();
+        this.m_lblSectionCount = new System.Windows.Forms.Label ();
+        this.m_txtSectionsCount = new System.Windows.Forms.TextBox ();
+        this.m_lblPhraseCount = new System.Windows.Forms.Label ();
+        this.m_txtPhraseCount = new System.Windows.Forms.TextBox ();
+        this.m_lblPageCount = new System.Windows.Forms.Label ();
+        this.m_txtPageCount = new System.Windows.Forms.TextBox ();
+        this.m_btnOk = new System.Windows.Forms.Button ();
+        this.m_lblTitle = new System.Windows.Forms.Label ();
+        this.m_txtTitle = new System.Windows.Forms.TextBox ();
+        this.mCancelButton = new System.Windows.Forms.Button ();
+        this.SuspendLayout ();
+        // 
+        // m_lblDuration
+        // 
+        resources.ApplyResources ( this.m_lblDuration, "m_lblDuration" );
+        this.m_lblDuration.Name = "m_lblDuration";
+        // 
+        // m_txtDuration
+        // 
+        resources.ApplyResources ( this.m_txtDuration, "m_txtDuration" );
+        this.m_txtDuration.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+        this.m_txtDuration.Name = "m_txtDuration";
+        this.m_txtDuration.ReadOnly = true;
+        // 
+        // m_lblSectionCount
+        // 
+        resources.ApplyResources ( this.m_lblSectionCount, "m_lblSectionCount" );
+        this.m_lblSectionCount.Name = "m_lblSectionCount";
+        // 
+        // m_txtSectionsCount
+        // 
+        resources.ApplyResources ( this.m_txtSectionsCount, "m_txtSectionsCount" );
+        this.m_txtSectionsCount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+        this.m_txtSectionsCount.Name = "m_txtSectionsCount";
+        this.m_txtSectionsCount.ReadOnly = true;
+        // 
+        // m_lblPhraseCount
+        // 
+        resources.ApplyResources ( this.m_lblPhraseCount, "m_lblPhraseCount" );
+        this.m_lblPhraseCount.Name = "m_lblPhraseCount";
+        // 
+        // m_txtPhraseCount
+        // 
+        resources.ApplyResources ( this.m_txtPhraseCount, "m_txtPhraseCount" );
+        this.m_txtPhraseCount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+        this.m_txtPhraseCount.Name = "m_txtPhraseCount";
+        this.m_txtPhraseCount.ReadOnly = true;
+        // 
+        // m_lblPageCount
+        // 
+        resources.ApplyResources ( this.m_lblPageCount, "m_lblPageCount" );
+        this.m_lblPageCount.Name = "m_lblPageCount";
+        // 
+        // m_txtPageCount
+        // 
+        resources.ApplyResources ( this.m_txtPageCount, "m_txtPageCount" );
+        this.m_txtPageCount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+        this.m_txtPageCount.Name = "m_txtPageCount";
+        this.m_txtPageCount.ReadOnly = true;
+        // 
+        // m_btnOk
+        // 
+        resources.ApplyResources ( this.m_btnOk, "m_btnOk" );
+        this.m_btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+        this.m_btnOk.Name = "m_btnOk";
+        this.m_btnOk.UseVisualStyleBackColor = true;
+        // 
+        // m_lblTitle
+        // 
+        resources.ApplyResources ( this.m_lblTitle, "m_lblTitle" );
+        this.m_lblTitle.Name = "m_lblTitle";
+        // 
+        // m_txtTitle
+        // 
+        resources.ApplyResources ( this.m_txtTitle, "m_txtTitle" );
+        this.m_txtTitle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+        this.m_txtTitle.Name = "m_txtTitle";
+        // 
+        // mCancelButton
+        // 
+        resources.ApplyResources ( this.mCancelButton, "mCancelButton" );
+        this.mCancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+        this.mCancelButton.Name = "mCancelButton";
+        this.mCancelButton.UseVisualStyleBackColor = true;
+        // 
+        // ProjectStatistics
+        // 
+        this.AcceptButton = this.m_btnOk;
+        resources.ApplyResources ( this, "$this" );
+        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        this.CancelButton = this.mCancelButton;
+        this.Controls.Add ( this.mCancelButton );
+        this.Controls.Add ( this.m_txtTitle );
+        this.Controls.Add ( this.m_lblTitle );
+        this.Controls.Add ( this.m_btnOk );
+        this.Controls.Add ( this.m_txtPageCount );
+        this.Controls.Add ( this.m_lblPageCount );
+        this.Controls.Add ( this.m_txtPhraseCount );
+        this.Controls.Add ( this.m_lblPhraseCount );
+        this.Controls.Add ( this.m_txtSectionsCount );
+        this.Controls.Add ( this.m_lblSectionCount );
+        this.Controls.Add ( this.m_txtDuration );
+        this.Controls.Add ( this.m_lblDuration );
+        this.Name = "ProjectStatistics";
+        this.ShowIcon = false;
+        this.ShowInTaskbar = false;
+        this.Load += new System.EventHandler ( this.ProjectStatistics_Load );
+        this.ResumeLayout ( false );
+        this.PerformLayout ();
 
         }
 
