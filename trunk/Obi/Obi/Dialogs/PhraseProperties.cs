c9bccb762d9f136922f3fec7a1dff9a8ee4602a7
avneesh-singh <avneesh-singh@f04d5631-4067-4348-862e-39f3aacb62c1>
2008-09-17 18:41:48 +0000

    Phrase properties dialog provides combobox for custom class roles selection

--- trunk/Obi/Obi/Dialogs/PhraseProperties.cs
+++ trunk/Obi/Obi/Dialogs/PhraseProperties.cs
@@ -9,26 +9,33 @@ using System.Windows.Forms;
 namespace Obi.Dialogs
 {
     public partial class PhraseProperties : Form
-    {
+        {
         private EmptyNode mNode;                // the node to show the information for.
         private ProjectView.ProjectView mView;  // the current view.
-
+        private bool m_IsNewCustomClass;
 
         /// <summary>
         /// Create the dialog to be shown by ShowDialog() for the given view.
         /// </summary>
-        public PhraseProperties(ProjectView.ProjectView view)
-        {
-            InitializeComponent();
+        public PhraseProperties ( ProjectView.ProjectView view )
+            {
+            InitializeComponent ();
             mView = view;
-            mNode = view.SelectedNodeAs<EmptyNode>();
-        }
+            mNode = view.SelectedNodeAs<EmptyNode> ();
+            m_IsNewCustomClass = false;
+            }
 
-        
+
         /// <summary>
         /// Get the custom class value input by the user. Should be used only when role is set to custom.
         /// </summary>
-        public string CustomClass { get { return m_txtCustomClassName.Text; } }
+        public string CustomClass
+            {
+            get
+                {
+                return m_comboCustomClassName.Text;
+                }
+            }
 
         /// <summary>
         /// Get the node under inspection.
@@ -52,40 +59,46 @@ namespace Obi.Dialogs
 
 
         // Fill out the fields when the form loads.
-        private void PhraseProperties_Load(object sender, EventArgs e)
-        {
-            m_txtParentSection.Text = mNode.AncestorAs<SectionNode>().Label;
-            m_txtLocationInsideSection.Text = string.Format(Localizer.Message("node_position"),
-                mNode.Index + 1, mNode.ParentAs<ObiNode>().PhraseChildCount);
-            for (SectionNode parent = mNode.AncestorAs<SectionNode>(); parent != null; parent = parent.ParentAs<SectionNode>())
+        private void PhraseProperties_Load ( object sender, EventArgs e )
             {
-                m_lbParentsList.Items.Insert(0, string.Format(Localizer.Message("section_level"),
-                    parent.Label, parent.Level));
-            }
-            m_txtTimeLength.Text = Program.FormatDuration_Long(mNode.Duration);
-            m_comboPhraseRole.Items.Add(EmptyNode.LOCALIZED_HEADING);
-            m_comboPhraseRole.Items.Add(EmptyNode.LOCALIZED_PAGE);
-            m_comboPhraseRole.Items.Add(EmptyNode.LOCALIZED_PLAIN);
-            m_comboPhraseRole.Items.Add(EmptyNode.LOCALIZED_SILENCE);
-            m_comboPhraseRole.Items.Add(EmptyNode.LOCALIZED_CUSTOM);
-            m_comboPhraseRole.SelectedItem = EmptyNode.LocalizedRoleFor(mNode.NodeKind);
-            m_txtCustomClassName.Text = mNode.NodeKind == EmptyNode.Kind.Custom ? mNode.CustomClass : "";
+            m_txtParentSection.Text = mNode.AncestorAs<SectionNode> ().Label;
+            m_txtLocationInsideSection.Text = string.Format ( Localizer.Message ( "node_position" ),
+                mNode.Index + 1, mNode.ParentAs<ObiNode> ().PhraseChildCount );
+            for (SectionNode parent = mNode.AncestorAs<SectionNode> (); parent != null; parent = parent.ParentAs<SectionNode> ())
+                {
+                m_lbParentsList.Items.Insert ( 0, string.Format ( Localizer.Message ( "section_level" ),
+                    parent.Label, parent.Level ) );
+                }
+            m_txtTimeLength.Text = Program.FormatDuration_Long ( mNode.Duration );
+            m_comboPhraseRole.Items.Add ( EmptyNode.LOCALIZED_HEADING );
+            m_comboPhraseRole.Items.Add ( EmptyNode.LOCALIZED_PAGE );
+            m_comboPhraseRole.Items.Add ( EmptyNode.LOCALIZED_PLAIN );
+            m_comboPhraseRole.Items.Add ( EmptyNode.LOCALIZED_SILENCE );
+            m_comboPhraseRole.Items.Add ( EmptyNode.LOCALIZED_CUSTOM );
+            m_comboPhraseRole.SelectedItem = EmptyNode.LocalizedRoleFor ( mNode.NodeKind );
+
+            // load custom class combobox
+            foreach (string customType in mView.Presentation.CustomClasses) m_comboCustomClassName.Items.Add ( customType );
+
+            m_comboCustomClassName.Text = mNode.NodeKind == EmptyNode.Kind.Custom ? mNode.CustomClass : "";
             m_chkUsed.Checked = mNode.Used;
             m_chkToDo.Checked = mNode.TODO;
-            EnableCustomClassField();
-        }
+            EnableCustomClassField ();
+            }
 
         // Turn the custom class field on/off depending on the role (i.e. turned on only for custom classes.)
-        private void EnableCustomClassField()
-        {
-            m_txtCustomClassName.Enabled =
+        private void EnableCustomClassField ()
+            {
+            m_comboCustomClassName.Enabled =
                 m_comboPhraseRole.SelectedItem == EmptyNode.LOCALIZED_CUSTOM;
-        }
+            }
 
         // Update the custom class text box when the role changes.
-        private void m_comboPhraseRole_SelectionChangeCommitted(object sender, EventArgs e)
-        {
-            EnableCustomClassField();
+        private void m_comboPhraseRole_SelectionChangeCommitted ( object sender, EventArgs e )
+            {
+            EnableCustomClassField ();
+            }
+
+
         }
-    }
 }
