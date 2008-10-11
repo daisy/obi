28dd21b45ce6c3920d1ab0dc997b69559fc41674
Julien Quint <722235+julienq@users.noreply.github.com>
2008-10-11 09:38:42 +0000

    Fix for #154 (deleting custom entry from Metadata list twice crash)

--- Obi/Obi/Commands/Metadata/DeleteEntry.cs
+++ Obi/Obi/Commands/Metadata/DeleteEntry.cs
@@ -6,18 +6,21 @@ namespace Obi.Commands.Metadata
 {
     class DeleteEntry: Command
     {
-        private urakawa.metadata.Metadata mEntry;
+        private urakawa.metadata.Metadata mEntry;   // the entry to delete
+        private MetadataSelection mSelectionAfter;  // selection after the deletion
 
         public DeleteEntry(ProjectView.ProjectView view)
             : base(view)
         {
             mEntry = ((MetadataSelection)view.Selection).Item.Entry;
+            mSelectionAfter = null;
             Label = Localizer.Message("delete_metadata_entry");
         }
 
         public override void execute()
         {
             View.Presentation.DeleteMetadata(mEntry);
+            View.Selection = mSelectionAfter;
         }
 
         public override void unExecute()
