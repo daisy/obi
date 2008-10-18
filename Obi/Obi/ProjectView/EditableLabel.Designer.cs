2d29434a2bee213a7306444c7ef8541129bdff08
Julien Quint <722235+julienq@users.noreply.github.com>
2008-10-18 07:30:58 +0000

    Fixes #229 (Strip shows underscore in label instead of &)

--- Obi/Obi/ProjectView/EditableLabel.Designer.cs
+++ Obi/Obi/ProjectView/EditableLabel.Designer.cs
@@ -37,57 +37,43 @@ namespace Obi.ProjectView
             // 
             // mLabel
             // 
-            this.mLabel.AccessibleDescription = null;
-            this.mLabel.AccessibleName = null;
             resources.ApplyResources(this.mLabel, "mLabel");
             this.mLabel.Name = "mLabel";
+            this.mLabel.UseMnemonic = false;
             this.mLabel.Click += new System.EventHandler(this.mLabel_Click);
             this.mLabel.Enter += new System.EventHandler(this.mLabel_Enter);
             // 
             // mTextBox
             // 
-            this.mTextBox.AccessibleDescription = null;
-            this.mTextBox.AccessibleName = null;
             resources.ApplyResources(this.mTextBox, "mTextBox");
             this.mTextBox.BackColor = System.Drawing.SystemColors.Info;
-            this.mTextBox.BackgroundImage = null;
             this.mTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.mTextBox.Name = "mTextBox";
             this.mTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.mTextBox_KeyDown);
             // 
             // mOKButton
             // 
-            this.mOKButton.AccessibleDescription = null;
-            this.mOKButton.AccessibleName = null;
             resources.ApplyResources(this.mOKButton, "mOKButton");
-            this.mOKButton.BackgroundImage = null;
             this.mOKButton.Name = "mOKButton";
             this.mOKButton.UseVisualStyleBackColor = true;
             this.mOKButton.Click += new System.EventHandler(this.mOKButton_Click);
             // 
             // mCancelButton
             // 
-            this.mCancelButton.AccessibleDescription = null;
-            this.mCancelButton.AccessibleName = null;
             resources.ApplyResources(this.mCancelButton, "mCancelButton");
-            this.mCancelButton.BackgroundImage = null;
             this.mCancelButton.Name = "mCancelButton";
             this.mCancelButton.UseVisualStyleBackColor = true;
             this.mCancelButton.Click += new System.EventHandler(this.mCancelButton_Click);
             // 
             // EditableLabel
             // 
-            this.AccessibleDescription = null;
-            this.AccessibleName = null;
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Thistle;
-            this.BackgroundImage = null;
             this.Controls.Add(this.mCancelButton);
             this.Controls.Add(this.mOKButton);
             this.Controls.Add(this.mTextBox);
             this.Controls.Add(this.mLabel);
-            this.Font = null;
             this.Name = "EditableLabel";
             this.Tag = "";
             this.Leave += new System.EventHandler(this.EditableLabel_Leave);
