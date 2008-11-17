4a8bb9bb23c5aadb88a69651f9e098ea15fa2a81
Avneesh Singh <13777742+avneeshsingh@users.noreply.github.com>
2008-11-17 19:12:24 +0000

    fixed #314 // (Validation error due to comma in folder name

--- Obi/Obi/Program.cs
+++ Obi/Obi/Program.cs
@@ -15,8 +15,8 @@ namespace Obi
         static void Main(string[] args)
         {
             // Hardcoded language switch
-            // System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("hi-IN");
-            // System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("hi-IN");
+             //System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("hi-IN");
+             //System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("hi-IN");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(args.Length == 0 ? new ObiForm() : new ObiForm(args[0]));
@@ -96,6 +96,7 @@ namespace Obi
             string safe = Regex.Replace(title, invalid, "_");
             safe = Regex.Replace(safe, "^_", "");
             safe = Regex.Replace(safe, "_$", "");
+            safe = Regex.Replace ( safe, ",", "_" );
             return safe;
         }
     }
