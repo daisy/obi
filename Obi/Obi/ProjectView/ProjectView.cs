5c8778a763a80ea4084009eaa6f9e1e30ffa840c
chhavi-negi <chhavi-negi@f04d5631-4067-4348-862e-39f3aacb62c1>
2009-05-12 12:51:17 +0000

    made a few changes in visual presentation of Goto Page

--- Obi/Obi/ProjectView/ProjectView.cs
+++ Obi/Obi/ProjectView/ProjectView.cs
@@ -2237,7 +2237,7 @@ namespace Obi.ProjectView
                     }
                 else
                     {
-                    MessageBox.Show ( "Page do not exists" );
+                    MessageBox.Show ( "Page does not exist" );
                     }
 
                 }
