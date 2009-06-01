2df0b4996c07dea6ca869433ef1c29a797b58846
rachana-singh <rachana-singh@f04d5631-4067-4348-862e-39f3aacb62c1>
2009-06-01 17:21:31 +0000

    Removed Message boxes used for testing purpose  and few GUI enhancement.

--- Obi/utilities/MergeUtility/MergeUtilityUI/Form1.cs
+++ Obi/utilities/MergeUtility/MergeUtilityUI/Form1.cs
@@ -33,13 +33,14 @@ namespace MergeUtilityUI
             }
 
             if (m_lbOPFfiles.Items.Count >= 2) //&& (m_txtDirectoryPath.Text != null))
-            {
-                m_BtnMerge.Enabled = true;
+            {              
+               m_BtnMerge.Enabled = true;                
             }
-            if (m_lbOPFfiles.Items.Count >= 1)
+            if (m_lbOPFfiles.Items.Count == 1)
             {
-                m_StatusLabel.Text = " The OPF File have been successfully added in the ListBox ";
+                m_StatusLabel.Text = "  Please select at least two OPF Files for merging ";
             }
+            m_StatusLabel.Text = " The OPF Files selected has successfully been added in the Listbox ";
         }//m_btnAdd_Click
 
         private void m_BtnOutputDirectory_Click(object sender, EventArgs e)
@@ -89,9 +90,10 @@ namespace MergeUtilityUI
 
         private void m_BtnDelete_Click(object sender, EventArgs e)
         {
-            m_StatusLabel.Text = " Deleting the selected file from the Listbox ";
-            m_lbOPFfiles.Items.Remove(m_lbOPFfiles.SelectedItem);
-            m_txtDTBookInfo.Clear();
+                m_StatusLabel.Text = " Deleting the selected file from the Listbox ";
+                m_lbOPFfiles.Items.Remove(m_lbOPFfiles.SelectedItem);
+                m_txtDTBookInfo.Clear();
+               m_StatusLabel.Text = string.Empty;           
         }//m_BtnDelete_Click
 
         private void m_BtnReset_Click(object sender, EventArgs e)
@@ -102,6 +104,10 @@ namespace MergeUtilityUI
             m_txtDTBookInfo.Clear();
             m_BtnMerge.Enabled = false;
             m_BtnValidateInput.Enabled = false;
+            m_BtnValidateOutput.Enabled = false;
+            m_BtnValidateInput.Enabled = false;
+            m_btnUP.Enabled = false;
+            m_BtnDown.Enabled = false;
         }//m_BtnReset_Click
 
         private void m_btnUP_Click(object sender, EventArgs e)
@@ -140,7 +146,6 @@ namespace MergeUtilityUI
                         object item = m_lbOPFfiles.SelectedItem;
                         m_lbOPFfiles.Items.RemoveAt(index);
                         m_lbOPFfiles.Items.Insert(index + 1, item);
-
                     }
                 }
             }
@@ -152,13 +157,13 @@ namespace MergeUtilityUI
 
         private void m_BtnMerge_Click(object sender, EventArgs e)
         {
-            m_StatusLabel.Text = "You have selected " + m_lbOPFfiles.SelectedItems.ToString() + " from the listbox for merging. ";
+            m_StatusLabel.Text = "You have selected all the files from the listbox for merging. ";
             if (m_txtDirectoryPath.Text == "")
             {
                 MessageBox.Show("Output Directory Path cannot be empty, Please select the output Directory Path", "Select Directory", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 m_txtDirectoryPath.Focus();
             }
-            if (m_txtDirectoryPath.Text.Length > 0)
+            if (m_txtDirectoryPath.Text.Length > 0) 
             {
                 m_bgWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(m_bgWorker_DoWork);
                 m_bgWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(m_bgWorker_RunWorkerCompleted);
@@ -205,6 +210,8 @@ namespace MergeUtilityUI
             if (e.Error == null)
             {
                 m_StatusLabel.Text = "The Files has been merged and put in Selected output directory.";
+                m_BtnMerge.Enabled = false;
+                m_BtnValidateOutput.Enabled = true;
             }
             if (e.Error != null)
             {
@@ -219,9 +226,17 @@ namespace MergeUtilityUI
 
         private void m_lbOPFfiles_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (m_lbOPFfiles.SelectedIndex == 0)
+            if (m_lbOPFfiles.Items.Count > 0)
             {
-                m_BtnValidateInput.Enabled = true;
+                m_BtnDelete.Enabled = true;
+               if (m_lbOPFfiles.SelectedIndex >= 0)
+                {
+                    m_BtnValidateInput.Enabled = true;                    
+                }
+            }
+            if (m_lbOPFfiles.Items.Count == 1)
+            {
+                m_StatusLabel.Text = "  Please select at least two OPF Files for merging ";
             }
             this.m_btnUP.Enabled = this.m_lbOPFfiles.SelectedIndex > 0;
             this.m_BtnDown.Enabled = this.m_lbOPFfiles.SelectedIndex > -1 && m_lbOPFfiles.SelectedIndex < m_lbOPFfiles.Items.Count - 1;
