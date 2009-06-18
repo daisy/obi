2641a3373b3e5606e630ef1d1a2e255a82ac253e
avneesh-singh <avneesh-singh@f04d5631-4067-4348-862e-39f3aacb62c1>
2009-06-18 14:09:52 +0000

    update to pipeline interface.

--- trunk/Obi/Obi/PipelineInterface/PipelineInfo.cs
+++ trunk/Obi/Obi/PipelineInterface/PipelineInfo.cs
@@ -4,34 +4,43 @@ using System.Text;
 using System.IO;
 
 namespace Obi.PipelineInterface
-{
-    public class PipelineInfo
     {
-        private Dictionary<string, FileInfo> m_ScriptsInfo = new Dictionary<string, FileInfo>();
-
-        public PipelineInfo(string ScriptsDirectory)
+    public class PipelineInfo
         {
-            PopulateScriptsDictionary(ScriptsDirectory);
-        }
+        private Dictionary<string, FileInfo> m_ScriptsInfo = new Dictionary<string, FileInfo> ();
 
-        private void PopulateScriptsDictionary(string DirPath)
-        {
-            string[] ScriptsFilePaths = Directory.GetFiles(DirPath, "*.taskScript", SearchOption.TopDirectoryOnly);
+        public PipelineInfo ( string ScriptsDirectory )
+            {
+            PopulateScriptsDictionary ( ScriptsDirectory );
+            }
+
+        private void PopulateScriptsDictionary ( string DirPath )
+            {
+            string[] ScriptsFilePaths = Directory.GetFiles ( DirPath, "*.taskScript", SearchOption.TopDirectoryOnly );
             FileInfo ScriptFileInfo = null;
             foreach (string s in ScriptsFilePaths)
-            {
-                ScriptFileInfo = new FileInfo(s);
-                ScriptParser parser = new ScriptParser(ScriptFileInfo.FullName);
-                m_ScriptsInfo.Add(parser.NiceName, ScriptFileInfo);
+                {
+                ScriptFileInfo = new FileInfo ( s );
+                ScriptParser parser = new ScriptParser ( ScriptFileInfo.FullName );
+                if (!m_ScriptsInfo.ContainsKey ( parser.NiceName ))
+                    {
+                    m_ScriptsInfo.Add ( parser.NiceName, ScriptFileInfo );
+                    }
+                else
+                    {
+                    System.Windows.Forms.MessageBox.Show (string.Format ( Localizer.Message ( "Pipeline_DuplicateScript" ),parser.NiceName) , 
+                        Localizer.Message ( "Caption_Warning" ),
+                        System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Warning );
+                    }
+                }
             }
-        }
-    
+
         public Dictionary<string, FileInfo> ScriptsInfo
-        {
+            {
             get { return m_ScriptsInfo; }
+            }
+
+
+
         }
-
-
-
     }
-}
