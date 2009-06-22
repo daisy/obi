629953f685864fa221c3fadcdde7ee8b9eed5859
chhavi-negi <chhavi-negi@f04d5631-4067-4348-862e-39f3aacb62c1>
2009-06-22 11:45:36 +0000

    Some minor changes in the GoToPageOrPhrase form.

--- trunk/Obi/Obi/Dialogs/GoToPageOrPhrase.Designer.cs
+++ trunk/Obi/Obi/Dialogs/GoToPageOrPhrase.Designer.cs
@@ -31,11 +31,46 @@ namespace Obi.Dialogs
                 this.m_btnOk = new System.Windows.Forms.Button();
                 this.m_radPage = new System.Windows.Forms.RadioButton();
                 this.m_radPhrase = new System.Windows.Forms.RadioButton();
+                this.m_grpSearchPagePhrase = new System.Windows.Forms.GroupBox();
+                this.m_grpSearchPagePhrase.SuspendLayout();
                 this.SuspendLayout();
+                // 
+                // label1
+                // 
+                this.label1.TabIndex = 3;
+                // 
+                // mNumberBox
+                // 
+                this.mNumberBox.TabIndex = 4;
+                // 
+                // mOKButton
+                // 
+                this.mOKButton.TabIndex = 8;
+                // 
+                // mCancelButton
+                // 
+                this.mCancelButton.TabIndex = 9;
+                // 
+                // mRenumber
+                // 
+                this.mRenumber.TabIndex = 7;
+                // 
+                // mNumberOfPagesBox
+                // 
+                this.mNumberOfPagesBox.TabIndex = 6;
+                // 
+                // label2
+                // 
+                this.label2.TabIndex = 5;
                 // 
                 // mPageKindComboBox
                 // 
                 this.mPageKindComboBox.Size = new System.Drawing.Size(184, 24);
+                this.mPageKindComboBox.TabIndex = 6;
+                // 
+                // label3
+                // 
+                this.label3.TabIndex = 5;
                 // 
                 // m_btnOk
                 // 
@@ -50,10 +85,10 @@ namespace Obi.Dialogs
                 // m_radPage
                 // 
                 this.m_radPage.AutoSize = true;
-                this.m_radPage.Location = new System.Drawing.Point(0, 0);
+                this.m_radPage.Location = new System.Drawing.Point(165, 13);
                 this.m_radPage.Name = "m_radPage";
                 this.m_radPage.Size = new System.Drawing.Size(107, 20);
-                this.m_radPage.TabIndex = 9;
+                this.m_radPage.TabIndex = 1;
                 this.m_radPage.TabStop = true;
                 this.m_radPage.Text = "Page number";
                 this.m_radPage.UseVisualStyleBackColor = true;
@@ -62,28 +97,37 @@ namespace Obi.Dialogs
                 // m_radPhrase
                 // 
                 this.m_radPhrase.AutoSize = true;
-                this.m_radPhrase.Location = new System.Drawing.Point(0, 11);
+                this.m_radPhrase.Location = new System.Drawing.Point(36, 11);
                 this.m_radPhrase.Name = "m_radPhrase";
                 this.m_radPhrase.Size = new System.Drawing.Size(104, 20);
-                this.m_radPhrase.TabIndex = 10;
+                this.m_radPhrase.TabIndex = 1;
                 this.m_radPhrase.TabStop = true;
                 this.m_radPhrase.Text = "Phrase index";
                 this.m_radPhrase.UseVisualStyleBackColor = true;
                 this.m_radPhrase.CheckedChanged += new System.EventHandler(this.m_radPhrase_CheckedChanged);
+                // 
+                // m_grpSearchPagePhrase
+                // 
+                this.m_grpSearchPagePhrase.Controls.Add(this.m_radPhrase);
+                this.m_grpSearchPagePhrase.Controls.Add(this.m_radPage);
+                this.m_grpSearchPagePhrase.Location = new System.Drawing.Point(37, 12);
+                this.m_grpSearchPagePhrase.Name = "m_grpSearchPagePhrase";
+                this.m_grpSearchPagePhrase.Size = new System.Drawing.Size(298, 49);
+                this.m_grpSearchPagePhrase.TabIndex = 0;
+                this.m_grpSearchPagePhrase.TabStop = false;
+                this.m_grpSearchPagePhrase.Text = "GoTo Page or Phrase";
                 // 
                 // GoToPageOrPhrase
                 // 
                 this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
                 this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
                 this.ClientSize = new System.Drawing.Size(389, 208);
-                this.Controls.Add(this.m_radPhrase);
-                this.Controls.Add(this.m_radPage);
                 this.Controls.Add(this.m_btnOk);
+                this.Controls.Add(this.m_grpSearchPagePhrase);
                 this.Name = "GoToPageOrPhrase";
                 this.Text = "GoToPageOrPhrase";
+                this.Controls.SetChildIndex(this.m_grpSearchPagePhrase, 0);
                 this.Controls.SetChildIndex(this.m_btnOk, 0);
-                this.Controls.SetChildIndex(this.m_radPage, 0);
-                this.Controls.SetChildIndex(this.m_radPhrase, 0);
                 this.Controls.SetChildIndex(this.label1, 0);
                 this.Controls.SetChildIndex(this.mNumberBox, 0);
                 this.Controls.SetChildIndex(this.mOKButton, 0);
@@ -93,6 +137,8 @@ namespace Obi.Dialogs
                 this.Controls.SetChildIndex(this.label2, 0);
                 this.Controls.SetChildIndex(this.mPageKindComboBox, 0);
                 this.Controls.SetChildIndex(this.label3, 0);
+                this.m_grpSearchPagePhrase.ResumeLayout(false);
+                this.m_grpSearchPagePhrase.PerformLayout();
                 this.ResumeLayout(false);
                 this.PerformLayout();
 
@@ -103,5 +149,6 @@ namespace Obi.Dialogs
         private System.Windows.Forms.Button m_btnOk;
         private System.Windows.Forms.RadioButton m_radPage;
         private System.Windows.Forms.RadioButton m_radPhrase;
+        private System.Windows.Forms.GroupBox m_grpSearchPagePhrase;
         }
     }
--- trunk/Obi/Obi/Dialogs/GoToPageOrPhrase.cs
+++ trunk/Obi/Obi/Dialogs/GoToPageOrPhrase.cs
@@ -19,19 +19,27 @@ namespace Obi.Dialogs
             base.m_GoToPage = true;
             base.Text = "Go to page";
             base.label2.Visible = false;
-            base.label1.Location = new Point(50, 56);
-            base.label3.Location = new Point(50, 94);
-            base.mNumberBox.Location = new Point(150, 56);
-            base.mPageKindComboBox.Location = new Point(150, 92);
-            base.Size = new Size(370, 200);
+            base.label1.Location = new Point(50, 75);
+            base.label3.Location = new Point(50, 110);
+            base.mNumberBox.Location = new Point(150, 75);
+            base.mPageKindComboBox.Location = new Point(150, 108);
+            base.Size = new Size(370, 210);
             base.mNumberOfPagesBox.Visible = false;
             base.mRenumber.Visible = false;
             base.mPageKindComboBox.SelectedIndex = 1;
-            m_btnOk.Location = new Point(70, 130);
-            base.mCancelButton.Location = new Point(200, 130);
+            m_btnOk.Location = new Point(70, 140);
+            base.mCancelButton.Location = new Point(200, 140);
             m_radPhrase.Location = new Point(50, 20);
             m_radPage.Location = new Point(180, 20);
+            m_radPhrase.TabIndex = 1;
+            m_radPage.TabIndex = 1;
             base.mOKButton.Visible = false;
+            base.label1.TabIndex = 2;
+            base.mNumberBox.TabIndex = 3;
+            base.mPageKindComboBox.TabIndex = 4;
+            m_btnOk.TabIndex = 5;
+            base.mCancelButton.TabIndex = 6;
+
             }
 
         public override PageNumber Number
