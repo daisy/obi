f3c8038f22bb3c29524bf7e6200bb2e15de5b196
avneesh-singh <avneesh-singh@f04d5631-4067-4348-862e-39f3aacb62c1>
2009-06-30 11:10:47 +0000

    handled npt and master.smil in merger

--- trunk/Obi/utilities/MergeUtility/DTBMerger/CommonFunctions.cs
+++ trunk/Obi/utilities/MergeUtility/DTBMerger/CommonFunctions.cs
@@ -64,10 +64,16 @@ namespace DTBMerger
         /// <returns></returns>
         public static TimeSpan GetTimeSpan ( string timeString )
             {
+            
             if (timeString.EndsWith ( "ms" ))
                 {
                 double temp = Convert.ToDouble ( timeString.Replace ( "ms", "" ) );
                 return new TimeSpan ( (long)temp * 10000 );
+                }
+            else if (timeString.StartsWith ( "npt=" ))
+                {
+                double temp = double.Parse ( timeString.Replace ( "s", "" ) );
+                return new TimeSpan ( (long)temp * 10000000 );
                 }
             else
                 {
