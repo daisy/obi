28cb25153bf0b757663d0ac674cdac3e5adb7cde
Avneesh Singh <13777742+avneeshsingh@users.noreply.github.com>
2009-07-23 10:52:54 +0000

    quick fix for aligning bit dept controls in mp3 encoder form

--- Obi/Obi/PipelineInterface/ParameterControls/EnumControl.Designer.cs
+++ Obi/Obi/PipelineInterface/ParameterControls/EnumControl.Designer.cs
@@ -28,50 +28,51 @@ namespace Obi.PipelineInterface.ParameterControls
         /// </summary>
         private void InitializeComponent()
         {
-            this.mComboBox = new System.Windows.Forms.ComboBox();
-            this.mNiceNameLabel = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // mComboBox
-            // 
-            this.mComboBox.AccessibleRole = System.Windows.Forms.AccessibleRole.ComboBox;
-            this.mComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.mComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.mComboBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.mComboBox.FormattingEnabled = true;
-            this.mComboBox.Location = new System.Drawing.Point(51, 20);
-            this.mComboBox.Margin = new System.Windows.Forms.Padding(4);
-            this.mComboBox.Name = "mComboBox";
-            this.mComboBox.Size = new System.Drawing.Size(160, 24);
-            this.mComboBox.TabIndex = 1;
-            // 
-            // mNiceNameLabel
-            // 
-            this.mNiceNameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.mNiceNameLabel.AutoSize = true;
-            this.mNiceNameLabel.Location = new System.Drawing.Point(3, 23);
-            this.mNiceNameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.mNiceNameLabel.Name = "mNiceNameLabel";
-            this.mNiceNameLabel.Size = new System.Drawing.Size(39, 16);
-            this.mNiceNameLabel.TabIndex = 1;
-            this.mNiceNameLabel.Text = "Nice:";
-            // 
-            // EnumControl
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Transparent;
-            this.Controls.Add(this.mNiceNameLabel);
-            this.Controls.Add(this.mComboBox);
-            this.Margin = new System.Windows.Forms.Padding(5);
-            this.Name = "EnumControl";
-            this.Size = new System.Drawing.Size(215, 48);
-            this.Load += new System.EventHandler(this.ComboboxControl_Load);
-            this.Controls.SetChildIndex(this.mLabel, 0);
-            this.Controls.SetChildIndex(this.mComboBox, 0);
-            this.Controls.SetChildIndex(this.mNiceNameLabel, 0);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+        this.mComboBox = new System.Windows.Forms.ComboBox ();
+        this.mNiceNameLabel = new System.Windows.Forms.Label ();
+        this.SuspendLayout ();
+        // 
+        // mComboBox
+        // 
+        this.mComboBox.AccessibleRole = System.Windows.Forms.AccessibleRole.ComboBox;
+        this.mComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+        this.mComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+        this.mComboBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+        this.mComboBox.FormattingEnabled = true;
+        this.mComboBox.Location = new System.Drawing.Point ( 51, 20 );
+        this.mComboBox.Margin = new System.Windows.Forms.Padding ( 4 );
+        this.mComboBox.Name = "mComboBox";
+        this.mComboBox.Size = new System.Drawing.Size ( 160, 24 );
+        this.mComboBox.TabIndex = 1;
+        // 
+        // mNiceNameLabel
+        // 
+        this.mNiceNameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+        this.mNiceNameLabel.AutoSize = true;
+        this.mNiceNameLabel.Location = new System.Drawing.Point ( 3, 23 );
+        this.mNiceNameLabel.Margin = new System.Windows.Forms.Padding ( 4, 0, 4, 0 );
+        this.mNiceNameLabel.Name = "mNiceNameLabel";
+        this.mNiceNameLabel.Size = new System.Drawing.Size ( 39, 16 );
+        this.mNiceNameLabel.TabIndex = 1;
+        this.mNiceNameLabel.Text = "Nice:";
+        this.mNiceNameLabel.LocationChanged += new System.EventHandler ( this.mNiceNameLabel_LocationChanged );
+        // 
+        // EnumControl
+        // 
+        this.AutoScaleDimensions = new System.Drawing.SizeF ( 8F, 16F );
+        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        this.BackColor = System.Drawing.Color.Transparent;
+        this.Controls.Add ( this.mNiceNameLabel );
+        this.Controls.Add ( this.mComboBox );
+        this.Margin = new System.Windows.Forms.Padding ( 5 );
+        this.Name = "EnumControl";
+        this.Size = new System.Drawing.Size ( 215, 48 );
+        this.Load += new System.EventHandler ( this.ComboboxControl_Load );
+        this.Controls.SetChildIndex ( this.mLabel, 0 );
+        this.Controls.SetChildIndex ( this.mComboBox, 0 );
+        this.Controls.SetChildIndex ( this.mNiceNameLabel, 0 );
+        this.ResumeLayout ( false );
+        this.PerformLayout ();
 
         }
 
--- Obi/Obi/PipelineInterface/ParameterControls/EnumControl.cs
+++ Obi/Obi/PipelineInterface/ParameterControls/EnumControl.cs
@@ -7,56 +7,66 @@ using System.Text;
 using System.Windows.Forms;
 
 namespace Obi.PipelineInterface.ParameterControls
-{
-    public partial class EnumControl : BaseUserControl
     {
+    public partial class EnumControl : BaseUserControl
+        {
         private ScriptParameter m_Parameter;
         private DataTypes.EnumDataType m_EnumData;
 
-        public EnumControl()
-        {
-            InitializeComponent();
-        }
+        public EnumControl ()
+            {
+            InitializeComponent ();
+            }
 
-        public EnumControl( ScriptParameter p)
-            : this()
-        {
-            m_Parameter = p ;
-            m_EnumData = (DataTypes.EnumDataType) p.ParameterDataType ;
+        public EnumControl ( ScriptParameter p )
+            : this ()
+            {
+            m_Parameter = p;
+            m_EnumData = (DataTypes.EnumDataType)p.ParameterDataType;
 
-            mNiceNameLabel.Text =  p.NiceName;
+            mNiceNameLabel.Text = p.NiceName;
             mComboBox.AccessibleName = p.NiceName;
             base.DescriptionLabel = p.Description;
 
             DataTypes.EnumDataType EnumData = (DataTypes.EnumDataType)p.ParameterDataType;
             foreach (string s in EnumData.GetNiceNames)
-            {
-                mComboBox.Items.Add(s);
-            }
+                {
+                mComboBox.Items.Add ( s );
+                }
 
             base.Size = this.Size;
-            if ( m_EnumData.SelectedIndex >= 0 && m_EnumData.SelectedIndex < m_EnumData.GetValues.Count )
-            mComboBox.SelectedIndex = m_EnumData.SelectedIndex;
-        }
+            if (m_EnumData.SelectedIndex >= 0 && m_EnumData.SelectedIndex < m_EnumData.GetValues.Count)
+                mComboBox.SelectedIndex = m_EnumData.SelectedIndex;
+            }
 
 
-        private void ComboboxControl_Load(object sender, EventArgs e)
-        {
+        private void ComboboxControl_Load ( object sender, EventArgs e )
+            {
 
-        }
+            }
 
         public override void UpdateScriptParameterValue ()
-        {
+            {
             try
-            {
-                                m_EnumData.SelectedIndex = mComboBox.SelectedIndex;
-            }
+                {
+                m_EnumData.SelectedIndex = mComboBox.SelectedIndex;
+                }
             catch (System.Exception ex)
-            {
-                MessageBox.Show(ex.ToString());
+                {
+                MessageBox.Show ( ex.ToString () );
+                }
             }
+
+        private void mNiceNameLabel_LocationChanged ( object sender, EventArgs e )
+            {
+            // this is work around to fix miss alignment of these controls. will have to look in flow layout to provide long term fix
+            if (mNiceNameLabel.Location.X > 20)
+                {
+                mNiceNameLabel.Location = new Point ( 6, mNiceNameLabel.Location.Y );
+                mComboBox.Location = new Point ( 20 + mNiceNameLabel.Size.Width, mComboBox.Location.Y );
+                }
+            }
+
+
         }
-
-
     }
-}
