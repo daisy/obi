7e0257ec82d5d03208746b944f0ca9e3008393aa
chhavi-negi <chhavi-negi@f04d5631-4067-4348-862e-39f3aacb62c1>
2009-07-23 06:28:45 +0000

    Aligning text box in fileset renamer

--- Obi/Obi/PipelineInterface/ParameterControls/StringControl.Designer.cs
+++ Obi/Obi/PipelineInterface/ParameterControls/StringControl.Designer.cs
@@ -28,40 +28,40 @@ namespace Obi.PipelineInterface.ParameterControls
         /// </summary>
         private void InitializeComponent ()
             {
-            this.label1 = new System.Windows.Forms.Label ();
-            this.textBox1 = new System.Windows.Forms.TextBox ();
-            this.SuspendLayout ();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point ( 0, 25 );
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size ( 45, 16 );
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point ( 100, 25 );
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size ( 150, 22 );
-            this.textBox1.TabIndex = 2;
-            this.textBox1.TextChanged += new System.EventHandler ( this.textBox1_TextChanged );
-            // 
-            // StringControl
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF ( 8F, 16F );
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add ( this.textBox1 );
-            this.Controls.Add ( this.label1 );
-            this.Name = "StringControl";
-            this.Size = new System.Drawing.Size ( 215, 45 );
-            this.Controls.SetChildIndex ( this.label1, 0 );
-            this.Controls.SetChildIndex ( this.textBox1, 0 );
-            this.Controls.SetChildIndex ( this.mLabel, 0 );
-            this.ResumeLayout ( false );
-            this.PerformLayout ();
+                this.label1 = new System.Windows.Forms.Label();
+                this.textBox1 = new System.Windows.Forms.TextBox();
+                this.SuspendLayout();
+                // 
+                // label1
+                // 
+                this.label1.AutoSize = true;
+                this.label1.Location = new System.Drawing.Point(0, 25);
+                this.label1.Name = "label1";
+                this.label1.Size = new System.Drawing.Size(45, 16);
+                this.label1.TabIndex = 1;
+                this.label1.Text = "label1";
+                // 
+                // textBox1
+                // 
+                this.textBox1.Location = new System.Drawing.Point(138, 22);
+                this.textBox1.Name = "textBox1";
+                this.textBox1.Size = new System.Drawing.Size(150, 22);
+                this.textBox1.TabIndex = 2;
+                this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+                // 
+                // StringControl
+                // 
+                this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+                this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+                this.Controls.Add(this.label1);
+                this.Controls.Add(this.textBox1);
+                this.Name = "StringControl";
+                this.Size = new System.Drawing.Size(215, 45);
+                this.Controls.SetChildIndex(this.textBox1, 0);
+                this.Controls.SetChildIndex(this.label1, 0);
+                this.Controls.SetChildIndex(this.mLabel, 0);
+                this.ResumeLayout(false);
+                this.PerformLayout();
 
             }
 
