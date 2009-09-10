db4efe48a371546c1efa8ffc4ea644fae953fcc5
Avneesh Singh <13777742+avneeshsingh@users.noreply.github.com>
2009-09-10 19:02:10 +0000

    updating assemblies version.

--- Obi/Obi/Properties/AssemblyInfo.cs
+++ Obi/Obi/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.05.*")]
+[assembly: AssemblyVersion("1.0.08.*")]
 
 // There is a warning about this but it is harmless (1607)
-[assembly: AssemblyInformationalVersion("1.1-rc")]
+[assembly: AssemblyInformationalVersion("1.0.8")]
