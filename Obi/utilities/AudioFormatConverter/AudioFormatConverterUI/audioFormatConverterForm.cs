e1a2ffa81c062f04ea4a5c6092cc1fc11dee1988
Avneesh Singh <13777742+avneeshsingh@users.noreply.github.com>
2009-10-13 10:03:55 +0000

    adding basic files for mp3 import in Obi

---
+++ Obi/utilities/AudioFormatConverter/AudioFormatConverterUI/audioFormatConverterForm.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AudioFormatConverterUI
+    {
+    public partial class audioFormatConverterForm : Form
+        {
+        public audioFormatConverterForm ()
+            {
+            InitializeComponent ();
+            }
+        }
+    }
