be1a8f6c402d3836f2f80bfca30e85f5c2a534f6
chhavi-negi <chhavi-negi@f04d5631-4067-4348-862e-39f3aacb62c1>
2010-05-29 09:12:19 +0000

    Improved code for AudioFormatConverter

--- trunk/Obi/Obi/Audio/AudioFormatConverter.cs
+++ trunk/Obi/Obi/Audio/AudioFormatConverter.cs
@@ -9,47 +9,55 @@ namespace Obi.Audio
 {
     public class AudioFormatConverter
     {
-        public static string[] ConvertFile(string[] fileName, Presentation presentation) 
+        public static string[] ConvertFiles(string[] fileName, Presentation presentation) 
         {
-            AudioLib.WavFormatConverter audioConverter = new WavFormatConverter(true);
-            int samplingRate = (int)presentation.DataManager.getDefaultPCMFormat().getSampleRate();
-            int channels = presentation.DataManager.getDefaultPCMFormat().getNumberOfChannels();
-            int bitDepth = presentation.DataManager.getDefaultPCMFormat().getBitDepth();
+            
             int numberOfFiles = fileName.Length;
             string convertedFile = null;
             string[] listOfConvertedFiles = new string[numberOfFiles];
-            string directoryPath = ((urakawa.media.data.FileDataProviderManager)presentation.getDataProviderManager ()).getDataFileDirectoryFullPath ();
+           
             
             for (int i = 0; i < numberOfFiles; i++)
             {
-
-                try
-                {
-                    if (fileName[i].EndsWith(".wav"))
-                    {
-                        Stream wavStream = null;
-                        wavStream = File.Open(fileName[i], FileMode.Open, FileAccess.Read, FileShare.Read);
-                        urakawa.media.data.audio.PCMDataInfo newFilePCMInfo = urakawa.media.data.audio.PCMDataInfo.parseRiffWaveHeader(wavStream);
-                        if (wavStream != null) wavStream.Close();
-                        if (newFilePCMInfo.getSampleRate() == samplingRate && newFilePCMInfo.getNumberOfChannels() == channels && newFilePCMInfo.getBitDepth() == bitDepth)
-                        {
-                            convertedFile = fileName[i];
-                        }
-                        else
-                            convertedFile = audioConverter.ConvertSampleRate(fileName[i], directoryPath, channels, samplingRate, bitDepth);
-                    }
-                    else if (fileName[i].EndsWith(".mp3"))
-                    {
-                        convertedFile = audioConverter.UnCompressMp3File(fileName[i], directoryPath, channels, samplingRate, bitDepth);
-                    }
-                    listOfConvertedFiles[i] = convertedFile;
-                }
-                catch (System.Exception ex)
-                {
-                    MessageBox.Show(ex.ToString());
-                }
+                convertedFile = ConvertedFile(fileName[i], presentation);
+                listOfConvertedFiles[i] = convertedFile;
             }
             return listOfConvertedFiles;
+        }
+        public static string ConvertedFile(string filePath, Presentation pres)
+        {
+            AudioLib.WavFormatConverter audioConverter = new WavFormatConverter(true);
+            int samplingRate = (int)pres.DataManager.getDefaultPCMFormat().getSampleRate();
+            int channels = pres.DataManager.getDefaultPCMFormat().getNumberOfChannels();
+            int bitDepth = pres.DataManager.getDefaultPCMFormat().getBitDepth();
+            string directoryPath = ((urakawa.media.data.FileDataProviderManager)pres.getDataProviderManager()).getDataFileDirectoryFullPath();
+            string convertedFile = null;
+            try
+            {
+                if (filePath.EndsWith(".wav"))
+                {
+                    Stream wavStream = null;
+                    wavStream = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+                    urakawa.media.data.audio.PCMDataInfo newFilePCMInfo = urakawa.media.data.audio.PCMDataInfo.parseRiffWaveHeader(wavStream);
+                    if (wavStream != null) wavStream.Close();
+                    if (newFilePCMInfo.getSampleRate() == samplingRate && newFilePCMInfo.getNumberOfChannels() == channels && newFilePCMInfo.getBitDepth() == bitDepth)
+                    {
+                        convertedFile = filePath;
+                    }
+                    else
+                        convertedFile = audioConverter.ConvertSampleRate(filePath, directoryPath, channels, samplingRate, bitDepth);
+                }
+                else if (filePath.EndsWith(".mp3"))
+                {
+                    convertedFile = audioConverter.UnCompressMp3File(filePath, directoryPath, channels, samplingRate, bitDepth);
+                }
+              
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            return convertedFile;
         }
 
     }
