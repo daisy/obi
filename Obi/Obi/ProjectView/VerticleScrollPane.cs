27cb2a370e1ee36407a1a0ca5581068da2816ebc
Avneesh Singh <13777742+avneeshsingh@users.noreply.github.com>
2010-07-27 04:54:35 +0000

    update to scroll pane buttons

--- Obi/Obi/ProjectView/VerticleScrollPane.cs
+++ Obi/Obi/ProjectView/VerticleScrollPane.cs
@@ -7,35 +7,35 @@ using System.Text;
 using System.Windows.Forms;
 
 namespace Obi.ProjectView
-{
-    public partial class VerticleScrollPane : UserControl
     {
+    public partial class VerticleScrollPane : UserControl
+        {
 
         private ContentView m_ContentView = null;
         private bool m_CanScrollUp = true;
         private bool m_CanScrollDown = true;
 
-        public VerticleScrollPane()
-        {
-            InitializeComponent();
-        }
+        public VerticleScrollPane ()
+            {
+            InitializeComponent ();
+            }
 
         public ContentView contentView
-        {
+            {
             get { return m_ContentView; }
             set { m_ContentView = value; }
-        }
+            }
 
         public int TrackBarValueInPercentage
             {
             get { return 100 - trackBar1.Value; }
-            set 
+            set
                 {
                 if (value >= 0 && value <= 100)
                     {
                     trackBar1.Value = 100 - value;
                     }
-                    }
+                }
             }
 
         public bool CanScrollUp
@@ -45,8 +45,9 @@ namespace Obi.ProjectView
                 {
                 m_CanScrollUp = value;
                 m_BtnGoToBegining.Enabled =
-                    m_BtnLargeIncrementUp.Enabled = 
+                    m_BtnLargeIncrementUp.Enabled =
                     m_BtnSmallIncrementUp.Enabled = m_CanScrollUp;
+
                 TrackBarValueInPercentage = 0;
                 }
             }
@@ -60,84 +61,88 @@ namespace Obi.ProjectView
                 m_BtnGoToEnd.Enabled =
                 m_BtnLargeIncrementDown.Enabled =
                  m_BtnSmallIncrementDown.Enabled = m_CanScrollDown;
+
                 TrackBarValueInPercentage = 100;
                 }
             }
 
-        private void m_BtnGoToBegining_Click(object sender, EventArgs e)
-        {
-            if (m_ContentView != null)
+        private void m_BtnGoToBegining_Click ( object sender, EventArgs e )
             {
-              scrollButtons(false);
-              m_ContentView.ScrollStripsPanel_Top();
-              scrollButtons(true);
-            }
-        }
-       
-        private void m_BtnLargeIncrementUp_Click(object sender, EventArgs e)
-        {
             if (m_ContentView != null)
-            {
-                scrollButtons(false);
-                m_ContentView.ScrollUp_LargeIncrement();
-                scrollButtons(true);
+                {
+                UpdateScrollButtons ( false );
+                m_ContentView.ScrollStripsPanel_Top ();
+                UpdateScrollButtons ( true );
+                }
             }
-        }
 
-        private void m_BtnLargeIncrementDown_Click(object sender, EventArgs e)
-        {
+        private void m_BtnLargeIncrementUp_Click ( object sender, EventArgs e )
+            {
             if (m_ContentView != null)
-            {
-                scrollButtons(false);
-                m_ContentView.ScrollDown_LargeIncrement();
-                scrollButtons(true);
+                {
+                UpdateScrollButtons ( false );
+                m_ContentView.ScrollUp_LargeIncrement ();
+                UpdateScrollButtons ( true );
+                }
             }
-        }       
 
-        private void m_BtnGoToEnd_Click(object sender, EventArgs e)
-        {
+        private void m_BtnLargeIncrementDown_Click ( object sender, EventArgs e )
+            {
             if (m_ContentView != null)
-            {
-                scrollButtons(false);
-                m_ContentView.ScrollStripsPanel_Bottom();
-                scrollButtons(true);
+                {
+                UpdateScrollButtons ( false );
+                m_ContentView.ScrollDown_LargeIncrement ();
+                UpdateScrollButtons ( true );
+                }
             }
-        }
 
-        private void m_BtnSmallIncrementUp_Click(object sender, EventArgs e)
-        {
+        private void m_BtnGoToEnd_Click ( object sender, EventArgs e )
+            {
             if (m_ContentView != null)
-            {
-                scrollButtons(false);
-                m_ContentView.ScrollUp_SmallIncrement();
-                scrollButtons(true);
+                {
+                UpdateScrollButtons ( false );
+                m_ContentView.ScrollStripsPanel_Bottom ();
+                UpdateScrollButtons ( true );
+                }
             }
-        }
 
-        private void m_BtnSmallIncrementDown_Click(object sender, EventArgs e)
-        {
+        private void m_BtnSmallIncrementUp_Click ( object sender, EventArgs e )
+            {
             if (m_ContentView != null)
-            {
-                scrollButtons(false);
-                m_ContentView.ScrollDown_SmallIncrement();
-                scrollButtons(true);
+                {
+                UpdateScrollButtons ( false );
+                m_ContentView.ScrollUp_SmallIncrement ();
+                UpdateScrollButtons ( true );
+                }
             }
+
+        private void m_BtnSmallIncrementDown_Click ( object sender, EventArgs e )
+            {
+            if (m_ContentView != null)
+                {
+                UpdateScrollButtons ( false );
+                m_ContentView.ScrollDown_SmallIncrement ();
+                UpdateScrollButtons ( true );
+                }
+            }
+
+        private void trackBar1_Scroll ( object sender, EventArgs e )
+            {
+            trackBar1.Capture = false;
+            }
+
+        public void UpdateScrollButtons ( bool value )
+            {
+            m_BtnGoToBegining.Enabled = value && m_CanScrollUp;
+            m_BtnLargeIncrementUp.Enabled = value && m_CanScrollUp;
+            m_BtnSmallIncrementUp.Enabled = value && m_CanScrollUp;
+
+            m_BtnGoToEnd.Enabled = value && m_CanScrollDown;
+            m_BtnLargeIncrementDown.Enabled = value && m_CanScrollDown;
+            m_BtnSmallIncrementDown.Enabled = value && m_CanScrollDown;
+
+            //Console.WriteLine ( "down button status : " + m_BtnGoToEnd.Enabled + " Scroll up button status : " + m_BtnGoToBegining.Enabled );
+            }
+
         }
-
-        private void trackBar1_Scroll(object sender, EventArgs e)
-        {
-            trackBar1.Capture = false;            
-        }
-
-        public void scrollButtons(bool value)
-        {
-            m_BtnGoToBegining.Enabled = value;
-            m_BtnGoToEnd.Enabled = value;
-            m_BtnLargeIncrementDown.Enabled = value;
-            m_BtnLargeIncrementUp.Enabled = value ;
-            m_BtnSmallIncrementDown.Enabled = value;
-            m_BtnSmallIncrementUp.Enabled = value;
-        }      
-
     }
-}
