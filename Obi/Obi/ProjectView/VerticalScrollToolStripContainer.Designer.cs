b0514c51d9fc4b2571d24570752f9ee0ab188878
chhavi-negi <chhavi-negi@f04d5631-4067-4348-862e-39f3aacb62c1>
2010-08-14 05:59:34 +0000

    Added tool strip container panel instead of button panel.

---
+++ Obi/Obi/ProjectView/VerticalScrollToolStripContainer.Designer.cs
@@ -0,0 +1,204 @@
+namespace Obi.ProjectView
+{
+    partial class VerticalScrollToolStripContainer
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VerticalScrollToolStripContainer));
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.m_BtnGoToBegining = new System.Windows.Forms.ToolStripButton();
+            this.m_BtnLargeIncrementUp = new System.Windows.Forms.ToolStripButton();
+            this.m_BtnSmallIncrementUp = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.m_BtnSmallIncrementDown = new System.Windows.Forms.ToolStripButton();
+            this.m_BtnLargeIncrementDown = new System.Windows.Forms.ToolStripButton();
+            this.m_BtnGoToEnd = new System.Windows.Forms.ToolStripButton();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.toolStripContainer1.RightToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            this.toolStrip2.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(0, 505);
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            // 
+            // toolStripContainer1.RightToolStripPanel
+            // 
+            this.toolStripContainer1.RightToolStripPanel.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.toolStripContainer1.RightToolStripPanel.Controls.Add(this.toolStrip2);
+            this.toolStripContainer1.RightToolStripPanel.Controls.Add(this.toolStrip1);
+            this.toolStripContainer1.Size = new System.Drawing.Size(30, 530);
+            this.toolStripContainer1.TabIndex = 0;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStrip2
+            // 
+            this.toolStrip2.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip2.ImageScalingSize = new System.Drawing.Size(28, 50);
+            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.m_BtnGoToBegining,
+            this.m_BtnLargeIncrementUp,
+            this.m_BtnSmallIncrementUp});
+            this.toolStrip2.Location = new System.Drawing.Point(0, 46);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.Size = new System.Drawing.Size(37, 206);
+            this.toolStrip2.TabIndex = 1;
+            // 
+            // m_BtnGoToBegining
+            // 
+            this.m_BtnGoToBegining.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.m_BtnGoToBegining.Image = ((System.Drawing.Image)(resources.GetObject("m_BtnGoToBegining.Image")));
+            this.m_BtnGoToBegining.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.m_BtnGoToBegining.Margin = new System.Windows.Forms.Padding(4, 1, 0, 10);
+            this.m_BtnGoToBegining.Name = "m_BtnGoToBegining";
+            this.m_BtnGoToBegining.Size = new System.Drawing.Size(31, 54);
+            this.m_BtnGoToBegining.Text = "toolStripButton1";
+            this.m_BtnGoToBegining.Click += new System.EventHandler(this.m_BtnGoToBegining_Click);
+            // 
+            // m_BtnLargeIncrementUp
+            // 
+            this.m_BtnLargeIncrementUp.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.m_BtnLargeIncrementUp.Image = ((System.Drawing.Image)(resources.GetObject("m_BtnLargeIncrementUp.Image")));
+            this.m_BtnLargeIncrementUp.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.m_BtnLargeIncrementUp.Margin = new System.Windows.Forms.Padding(7, 1, 0, 10);
+            this.m_BtnLargeIncrementUp.Name = "m_BtnLargeIncrementUp";
+            this.m_BtnLargeIncrementUp.Size = new System.Drawing.Size(28, 54);
+            this.m_BtnLargeIncrementUp.Text = "toolStripButton2";
+            this.m_BtnLargeIncrementUp.Click += new System.EventHandler(this.m_BtnLargeIncrementUp_Click);
+            // 
+            // m_BtnSmallIncrementUp
+            // 
+            this.m_BtnSmallIncrementUp.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.m_BtnSmallIncrementUp.Image = ((System.Drawing.Image)(resources.GetObject("m_BtnSmallIncrementUp.Image")));
+            this.m_BtnSmallIncrementUp.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.m_BtnSmallIncrementUp.Margin = new System.Windows.Forms.Padding(7, 1, 0, 10);
+            this.m_BtnSmallIncrementUp.Name = "m_BtnSmallIncrementUp";
+            this.m_BtnSmallIncrementUp.Size = new System.Drawing.Size(28, 54);
+            this.m_BtnSmallIncrementUp.Text = "toolStripButton3";
+            this.m_BtnSmallIncrementUp.Click += new System.EventHandler(this.m_BtnSmallIncrementUp_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(28, 50);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.m_BtnSmallIncrementDown,
+            this.m_BtnLargeIncrementDown,
+            this.m_BtnGoToEnd});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 290);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(37, 206);
+            this.toolStrip1.TabIndex = 0;
+            // 
+            // m_BtnSmallIncrementDown
+            // 
+            this.m_BtnSmallIncrementDown.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.m_BtnSmallIncrementDown.Image = ((System.Drawing.Image)(resources.GetObject("m_BtnSmallIncrementDown.Image")));
+            this.m_BtnSmallIncrementDown.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.m_BtnSmallIncrementDown.Margin = new System.Windows.Forms.Padding(7, 1, 0, 10);
+            this.m_BtnSmallIncrementDown.Name = "m_BtnSmallIncrementDown";
+            this.m_BtnSmallIncrementDown.Size = new System.Drawing.Size(28, 54);
+            this.m_BtnSmallIncrementDown.Text = "toolStripButton4";
+            this.m_BtnSmallIncrementDown.Click += new System.EventHandler(this.m_BtnSmallIncrementDown_Click);
+            // 
+            // m_BtnLargeIncrementDown
+            // 
+            this.m_BtnLargeIncrementDown.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.m_BtnLargeIncrementDown.Image = ((System.Drawing.Image)(resources.GetObject("m_BtnLargeIncrementDown.Image")));
+            this.m_BtnLargeIncrementDown.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.m_BtnLargeIncrementDown.Margin = new System.Windows.Forms.Padding(7, 1, 0, 10);
+            this.m_BtnLargeIncrementDown.Name = "m_BtnLargeIncrementDown";
+            this.m_BtnLargeIncrementDown.Size = new System.Drawing.Size(28, 54);
+            this.m_BtnLargeIncrementDown.Text = "toolStripButton5";
+            this.m_BtnLargeIncrementDown.Click += new System.EventHandler(this.m_BtnLargeIncrementDown_Click);
+            // 
+            // m_BtnGoToEnd
+            // 
+            this.m_BtnGoToEnd.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.m_BtnGoToEnd.Image = ((System.Drawing.Image)(resources.GetObject("m_BtnGoToEnd.Image")));
+            this.m_BtnGoToEnd.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.m_BtnGoToEnd.Margin = new System.Windows.Forms.Padding(7, 1, 0, 10);
+            this.m_BtnGoToEnd.Name = "m_BtnGoToEnd";
+            this.m_BtnGoToEnd.Size = new System.Drawing.Size(28, 54);
+            this.m_BtnGoToEnd.Text = "toolStripButton6";
+            this.m_BtnGoToEnd.Click += new System.EventHandler(this.m_BtnGoToEnd_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.trackBar1.Location = new System.Drawing.Point(-2, 510);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 104);
+            this.trackBar1.TabIndex = 1;
+            // 
+            // VerticalScrollToolStripContainer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.toolStripContainer1);
+            this.Name = "VerticalScrollToolStripContainer";
+            this.Size = new System.Drawing.Size(30, 533);
+            this.toolStripContainer1.RightToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.RightToolStripPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.toolStrip2.ResumeLayout(false);
+            this.toolStrip2.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.ToolStripButton m_BtnSmallIncrementDown;
+        private System.Windows.Forms.ToolStripButton m_BtnLargeIncrementDown;
+        private System.Windows.Forms.ToolStripButton m_BtnGoToEnd;
+        private System.Windows.Forms.ToolStripButton m_BtnGoToBegining;
+        private System.Windows.Forms.ToolStripButton m_BtnLargeIncrementUp;
+        private System.Windows.Forms.ToolStripButton m_BtnSmallIncrementUp;
+    }
+}
---
+++ Obi/Obi/ProjectView/VerticalScrollToolStripContainer.cs
@@ -0,0 +1,148 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Obi.ProjectView
+{
+    public partial class VerticalScrollToolStripContainer : UserControl
+    {
+        private ContentView m_ContentView = null;
+        private bool m_CanScrollUp = true;
+        private bool m_CanScrollDown = true;
+
+        public VerticalScrollToolStripContainer()
+        {
+            InitializeComponent();
+            TrackBarValueInPercentage = 0;
+        }
+        public ContentView contentView
+        {
+            get { return m_ContentView; }
+            set { m_ContentView = value; }
+        }
+
+        public int TrackBarValueInPercentage
+        {
+            get { return 100 - trackBar1.Value; }
+            set
+            {
+                if (value >= 0 && value <= 100)
+                {
+                    trackBar1.Value = 100 - value;
+                }
+            }
+        }
+
+        public bool CanScrollUp
+        {
+            get { return m_CanScrollUp; }
+            set
+            {
+                m_CanScrollUp = value;
+                m_BtnGoToBegining.Enabled =
+                    m_BtnLargeIncrementUp.Enabled =
+                    m_BtnSmallIncrementUp.Enabled = m_CanScrollUp;
+
+                if (!m_CanScrollUp) TrackBarValueInPercentage = 0;
+            }
+        }
+
+        public bool CanScrollDown
+        {
+            get { return m_CanScrollDown; }
+            set
+            {
+                m_CanScrollDown = value;
+                m_BtnGoToEnd.Enabled =
+                m_BtnLargeIncrementDown.Enabled =
+                 m_BtnSmallIncrementDown.Enabled = m_CanScrollDown;
+
+                if (!m_CanScrollDown) TrackBarValueInPercentage = 100;
+            }
+        }
+
+       
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            trackBar1.Capture = false;
+        }
+
+        public void UpdateScrollButtons(bool value)
+        {
+            m_BtnGoToBegining.Enabled = value && m_CanScrollUp;
+            m_BtnLargeIncrementUp.Enabled = value && m_CanScrollUp;
+            m_BtnSmallIncrementUp.Enabled = value && m_CanScrollUp;
+
+            m_BtnGoToEnd.Enabled = value && m_CanScrollDown;
+            m_BtnLargeIncrementDown.Enabled = value && m_CanScrollDown;
+            m_BtnSmallIncrementDown.Enabled = value && m_CanScrollDown;
+
+            //Console.WriteLine ( "down button status : " + m_BtnGoToEnd.Enabled + " Scroll up button status : " + m_BtnGoToBegining.Enabled );
+        }
+
+        private void m_BtnGoToBegining_Click(object sender, EventArgs e)
+        {
+            if (m_ContentView != null)
+            {
+                UpdateScrollButtons(false);
+                m_ContentView.ScrollStripsPanel_Top();
+                UpdateScrollButtons(true);
+            }
+        }
+
+        private void m_BtnLargeIncrementUp_Click(object sender, EventArgs e)
+        {
+            if (m_ContentView != null)
+            {
+                UpdateScrollButtons(false);
+                m_ContentView.ScrollUp_LargeIncrement();
+                UpdateScrollButtons(true);
+            }
+        }
+
+        private void m_BtnSmallIncrementUp_Click(object sender, EventArgs e)
+        {
+            if (m_ContentView != null)
+            {
+                UpdateScrollButtons(false);
+                m_ContentView.ScrollUp_SmallIncrement();
+                UpdateScrollButtons(true);
+            }
+        }
+
+        private void m_BtnSmallIncrementDown_Click(object sender, EventArgs e)
+        {
+            if (m_ContentView != null)
+            {
+                UpdateScrollButtons(false);
+                m_ContentView.ScrollDown_SmallIncrement();
+                UpdateScrollButtons(true);
+            }
+        }
+
+        private void m_BtnLargeIncrementDown_Click(object sender, EventArgs e)
+        {
+            if (m_ContentView != null)
+            {
+                UpdateScrollButtons(false);
+                m_ContentView.ScrollDown_LargeIncrement();
+                UpdateScrollButtons(true);
+            }
+        }
+
+        private void m_BtnGoToEnd_Click(object sender, EventArgs e)
+        {
+            if (m_ContentView != null)
+            {
+                UpdateScrollButtons(false);
+                m_ContentView.ScrollStripsPanel_Bottom();
+                UpdateScrollButtons(true);
+            }
+        }
+    }
+}
