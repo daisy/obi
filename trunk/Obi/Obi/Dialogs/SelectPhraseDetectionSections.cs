5988059a13c798ec551c8ef8d630ed4202b2f7c6
chhavi-negi <chhavi-negi@f04d5631-4067-4348-862e-39f3aacb62c1>
2010-08-19 11:13:15 +0000

    Dialog box added for selective phrase detection in project

---
+++ trunk/Obi/Obi/Dialogs/SelectPhraseDetectionSections.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Obi.Dialogs
+{
+    public partial class SelectPhraseDetectionSections : Form
+    {
+        private int m_SectionRangeCount;
+        List<string> mOriginalSectionList = new List<string>();
+        List<string> mSelectedSectionList = new List<string>();
+        
+        public SelectPhraseDetectionSections()
+        {
+            InitializeComponent();
+            for(int i = 1; i<= 360; i++)
+            {
+                mOriginalSectionList.Add(i.ToString());
+            }
+            m_SectionRangeCount = mOriginalSectionList.Count / 100;
+            m_cb_StartRangeForNumberOfSections.Items.AddRange(new object[] { 1 });    
+            for (int i = 1; i <= m_SectionRangeCount; i++)
+            {
+                m_cb_StartRangeForNumberOfSections.Items.AddRange(new object[] { i * 100 });                
+                m_cb_EndRangeForNumberOfSections.Items.AddRange(new object[] { i * 100 });                
+            }
+            m_cb_StartRangeForNumberOfSections.Items.AddRange(new object[] { mOriginalSectionList.Count });
+            m_cb_EndRangeForNumberOfSections.Items.AddRange(new object[] { mOriginalSectionList.Count });
+        }
+
+        public List<string> SelectedSections
+        {
+            get { return mSelectedSectionList; }
+        }
+
+        private void m_btn_Display_Click(object sender, EventArgs e)
+        {
+           int startRange = int.Parse(m_cb_StartRangeForNumberOfSections.Items[m_cb_StartRangeForNumberOfSections.SelectedIndex].ToString());
+           int endRange =  int.Parse(m_cb_EndRangeForNumberOfSections.Items[m_cb_EndRangeForNumberOfSections.SelectedIndex].ToString());
+           if (startRange > endRange) { MessageBox.Show("End value is smaller than start value."); }
+           else
+           {
+               for (int i = startRange; i < endRange; i++)
+                   m_lb_listOfSelectedSectionsForPhraseDetection.Items.Add(i);
+           }
+        }
+
+        private void m_btn_RemoveFromList_Click(object sender, EventArgs e)
+        {
+            for (int i = m_lb_listOfSelectedSectionsForPhraseDetection.SelectedItems.Count -1; i > 0; i--)
+                m_lb_listOfSelectedSectionsForPhraseDetection.Items.Remove(m_lb_listOfSelectedSectionsForPhraseDetection.SelectedItem);              
+            m_lb_listOfSelectedSectionsForPhraseDetection.Items.Remove(m_lb_listOfSelectedSectionsForPhraseDetection.SelectedItem);
+        }
+
+        private void m_btn_OK_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= m_lb_listOfSelectedSectionsForPhraseDetection.Items.Count; i++ )
+                mSelectedSectionList.Add(i.ToString());              
+            Close();
+        }
+
+        private void m_btn_Cancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+         
+        
+    }
+}
