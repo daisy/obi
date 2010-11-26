b21b25e8d05a6df3dc25f5e1668a48f510013db3
avneesh-singh <avneesh-singh@f04d5631-4067-4348-862e-39f3aacb62c1>
2010-11-26 07:23:23 +0000

    phrase detection for phrase selects first detected phrase after command execution.

--- trunk/Obi/Obi/Commands/Node/SplitAudio.cs
+++ trunk/Obi/Obi/Commands/Node/SplitAudio.cs
@@ -182,8 +182,8 @@ namespace Obi.Commands.Node
                     }
                 if (node is PhraseNode &&  phrases.Count > 0 && view.Selection != null)
                     {
-                    command.append ( new UpdateSelection ( view, new NodeSelection ( node, view.Selection.Control ) ) );
-                    //command.append ( new UpdateSelection ( view, new NodeSelection ( phrases[0], view.Selection.Control ) ) );
+                    //command.append ( new UpdateSelection ( view, new NodeSelection ( node, view.Selection.Control ) ) );
+                    command.append ( new UpdateSelection ( view, new NodeSelection ( phrases[0], view.Selection.Control ) ) );//uncommenting this because unexecute for update selection can handle null unexecute now
                                         }
                 command.append ( new Commands.Node.Delete ( view, phrase, false ) );//@singleSection: moved delete command last for improve undo selection
                 }
