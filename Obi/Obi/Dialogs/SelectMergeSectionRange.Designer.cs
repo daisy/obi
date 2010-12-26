e3e737763f9965f013aebefd7088f68a0df55ac7
chhavi-negi <chhavi-negi@f04d5631-4067-4348-862e-39f3aacb62c1>
2010-12-26 12:20:06 +0000

    Select all sections in list box works in merge section. A read only text box added to show the information about the selected item.

--- Obi/Obi/Dialogs/SelectMergeSectionRange.Designer.cs
+++ Obi/Obi/Dialogs/SelectMergeSectionRange.Designer.cs
@@ -32,24 +32,29 @@ namespace Obi.Dialogs
             this.m_btn_OK = new System.Windows.Forms.Button();
             this.m_btn_Cancel = new System.Windows.Forms.Button();
             this.m_statusStripForMergeSection = new System.Windows.Forms.StatusStrip();
-            this.m_toolStripStatusLabelForMergeSection = new System.Windows.Forms.ToolStripStatusLabel();
+            this.m_StatusLabelForMergeSection = new System.Windows.Forms.ToolStripStatusLabel();
+            this.m_tb_SelectedSection = new System.Windows.Forms.TextBox();
+            this.m_btn_SelectAll = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.m_statusStripForMergeSection.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // m_lb_listofSectionsToMerge
             // 
             this.m_lb_listofSectionsToMerge.FormattingEnabled = true;
-            this.m_lb_listofSectionsToMerge.Location = new System.Drawing.Point(55, 12);
+            this.m_lb_listofSectionsToMerge.Location = new System.Drawing.Point(15, 19);
             this.m_lb_listofSectionsToMerge.Name = "m_lb_listofSectionsToMerge";
             this.m_lb_listofSectionsToMerge.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
-            this.m_lb_listofSectionsToMerge.Size = new System.Drawing.Size(120, 173);
+            this.m_lb_listofSectionsToMerge.Size = new System.Drawing.Size(174, 199);
             this.m_lb_listofSectionsToMerge.TabIndex = 0;
+            this.m_lb_listofSectionsToMerge.SelectedIndexChanged += new System.EventHandler(this.m_lb_listofSectionsToMerge_SelectedIndexChanged);
             // 
             // m_btn_OK
             // 
-            this.m_btn_OK.Location = new System.Drawing.Point(12, 205);
+            this.m_btn_OK.Location = new System.Drawing.Point(77, 292);
             this.m_btn_OK.Name = "m_btn_OK";
-            this.m_btn_OK.Size = new System.Drawing.Size(75, 23);
+            this.m_btn_OK.Size = new System.Drawing.Size(92, 23);
             this.m_btn_OK.TabIndex = 1;
             this.m_btn_OK.Text = "OK";
             this.m_btn_OK.UseVisualStyleBackColor = true;
@@ -57,9 +62,9 @@ namespace Obi.Dialogs
             // 
             // m_btn_Cancel
             // 
-            this.m_btn_Cancel.Location = new System.Drawing.Point(124, 205);
+            this.m_btn_Cancel.Location = new System.Drawing.Point(175, 292);
             this.m_btn_Cancel.Name = "m_btn_Cancel";
-            this.m_btn_Cancel.Size = new System.Drawing.Size(75, 23);
+            this.m_btn_Cancel.Size = new System.Drawing.Size(100, 23);
             this.m_btn_Cancel.TabIndex = 2;
             this.m_btn_Cancel.Text = "Cancel";
             this.m_btn_Cancel.UseVisualStyleBackColor = true;
@@ -68,31 +73,62 @@ namespace Obi.Dialogs
             // m_statusStripForMergeSection
             // 
             this.m_statusStripForMergeSection.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.m_toolStripStatusLabelForMergeSection});
-            this.m_statusStripForMergeSection.Location = new System.Drawing.Point(0, 242);
+            this.m_StatusLabelForMergeSection});
+            this.m_statusStripForMergeSection.Location = new System.Drawing.Point(0, 327);
             this.m_statusStripForMergeSection.Name = "m_statusStripForMergeSection";
-            this.m_statusStripForMergeSection.Size = new System.Drawing.Size(243, 22);
+            this.m_statusStripForMergeSection.Size = new System.Drawing.Size(357, 22);
             this.m_statusStripForMergeSection.TabIndex = 3;
             this.m_statusStripForMergeSection.Text = "MergeSectionStatusStrip";
             // 
-            // m_toolStripStatusLabelForMergeSection
+            // m_StatusLabelForMergeSection
             // 
-            this.m_toolStripStatusLabelForMergeSection.Name = "m_toolStripStatusLabelForMergeSection";
-            this.m_toolStripStatusLabelForMergeSection.Size = new System.Drawing.Size(0, 17);
+            this.m_StatusLabelForMergeSection.Name = "m_StatusLabelForMergeSection";
+            this.m_StatusLabelForMergeSection.Size = new System.Drawing.Size(0, 17);
+            // 
+            // m_tb_SelectedSection
+            // 
+            this.m_tb_SelectedSection.Location = new System.Drawing.Point(27, 248);
+            this.m_tb_SelectedSection.Name = "m_tb_SelectedSection";
+            this.m_tb_SelectedSection.ReadOnly = true;
+            this.m_tb_SelectedSection.Size = new System.Drawing.Size(174, 20);
+            this.m_tb_SelectedSection.TabIndex = 4;
+            // 
+            // m_btn_SelectAll
+            // 
+            this.m_btn_SelectAll.Location = new System.Drawing.Point(218, 246);
+            this.m_btn_SelectAll.Name = "m_btn_SelectAll";
+            this.m_btn_SelectAll.Size = new System.Drawing.Size(91, 23);
+            this.m_btn_SelectAll.TabIndex = 5;
+            this.m_btn_SelectAll.Text = "Select All";
+            this.m_btn_SelectAll.UseVisualStyleBackColor = true;
+            this.m_btn_SelectAll.Click += new System.EventHandler(this.m_btn_SelectAll_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.m_lb_listofSectionsToMerge);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(333, 230);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
             // 
             // SelectMergeSectionRange
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(243, 264);
+            this.ClientSize = new System.Drawing.Size(357, 349);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.m_btn_SelectAll);
             this.Controls.Add(this.m_statusStripForMergeSection);
+            this.Controls.Add(this.m_tb_SelectedSection);
             this.Controls.Add(this.m_btn_Cancel);
             this.Controls.Add(this.m_btn_OK);
-            this.Controls.Add(this.m_lb_listofSectionsToMerge);
             this.Name = "SelectMergeSectionRange";
             this.Text = "SelectMergeSectionRange";
             this.m_statusStripForMergeSection.ResumeLayout(false);
             this.m_statusStripForMergeSection.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -104,6 +140,9 @@ namespace Obi.Dialogs
         private System.Windows.Forms.Button m_btn_OK;
         private System.Windows.Forms.Button m_btn_Cancel;
         private System.Windows.Forms.StatusStrip m_statusStripForMergeSection;
-        private System.Windows.Forms.ToolStripStatusLabel m_toolStripStatusLabelForMergeSection;
+        private System.Windows.Forms.ToolStripStatusLabel m_StatusLabelForMergeSection;
+        private System.Windows.Forms.TextBox m_tb_SelectedSection;
+        private System.Windows.Forms.Button m_btn_SelectAll;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
--- Obi/Obi/Dialogs/SelectMergeSectionRange.cs
+++ Obi/Obi/Dialogs/SelectMergeSectionRange.cs
@@ -23,17 +23,37 @@ namespace Obi.Dialogs
         {
             m_SectionList = sectionsList;
             m_selectedIndex = selectedIndexOfSection;
-            populateListboxForSectionsToMerge();            
+            populateListboxForSectionsToMerge();
+            m_StatusLabelForMergeSection.Text = String.Format("Showing section {0} to {1}. Please select sections to merge. ", m_SectionList[0].Label, m_SectionList[m_SectionList.Count - 1].Label);
+        }
+
+        public List<SectionNode> SelectedSections
+        {
+            get { return  m_SelectedSectionList; }            
         }
 
         private void m_btn_OK_Click(object sender, EventArgs e)
         {
-            if (m_SectionList[m_selectedIndex].PhraseChildCount < 7000 && m_lb_listofSectionsToMerge.SelectedIndex != -1)
+            int totalPhraseCount = 0;
+            if (m_lb_listofSectionsToMerge.SelectedItems.Count == 0 || m_lb_listofSectionsToMerge.SelectedItems.Count == 1)
+            {
+                MessageBox.Show("Please select at least two sections to merge");
+                return;
+            }
+            else
             {
                 for (int i = m_lb_listofSectionsToMerge.SelectedIndex; i <= m_lb_listofSectionsToMerge.SelectedItems.Count; i++)
-                    m_SelectedSectionList.Add((SectionNode)m_lb_listofSectionsToMerge.Items[i]);
-                   
+                {
+                    if (totalPhraseCount < 7000 && m_lb_listofSectionsToMerge.SelectedIndex != -1)
+                    {
+                        m_SelectedSectionList.Add((SectionNode)m_lb_listofSectionsToMerge.Items[i]);
+                        totalPhraseCount = totalPhraseCount + m_SectionList[i].PhraseChildCount;
+                    }
+                    else
+                        MessageBox.Show("Total phrase count is more than 7000");
+                }
             }
+            
             DialogResult = DialogResult.OK;
             Close();
         }
@@ -50,9 +70,31 @@ namespace Obi.Dialogs
             for (int i = 0; i <= (m_SectionList.Count - 1); i++)
             {
                 if (m_SectionList[i].Level >= firstSection.Level)
-                    m_lb_listofSectionsToMerge.Items.Add(m_SectionList[i]);
+                    m_lb_listofSectionsToMerge.Items.Add("Section " + m_SectionList[i].Label + " Level " + m_SectionList[i].Level);
                 else
                     return;
+            }
+        }
+
+        private void m_lb_listofSectionsToMerge_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int totalPhraseCount = 0;
+            for (int i = m_lb_listofSectionsToMerge.SelectedIndex; i <= m_lb_listofSectionsToMerge.SelectedItems.Count; i++)
+            {
+                if (totalPhraseCount <= 7000 && m_lb_listofSectionsToMerge.SelectedIndex != -1)
+                   totalPhraseCount = totalPhraseCount + m_SectionList[i].PhraseChildCount;
+                else if(totalPhraseCount > 7000)
+                    m_StatusLabelForMergeSection.Text = String.Format("Total number of phrases is {0}.It should be less than 7000", totalPhraseCount);
+            }
+            
+            m_tb_SelectedSection.Text = m_SectionList[m_lb_listofSectionsToMerge.SelectedIndices[m_lb_listofSectionsToMerge.SelectedItems.Count- 1 ]].ToString();             
+        }
+
+        private void m_btn_SelectAll_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < m_lb_listofSectionsToMerge.Items.Count; i++ )
+            {
+                m_lb_listofSectionsToMerge.SetSelected(i, true);
             }
         }
     }
