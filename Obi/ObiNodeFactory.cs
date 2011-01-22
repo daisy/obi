95a0cee4bd6b18c4876e99ae407e9bcee024f605
Avneesh Singh <13777742+avneeshsingh@users.noreply.github.com>
2011-01-22 18:17:17 +0000

    made obinodefactory stand alone instead of deriving from treenode factory

--- Obi/ObiNodeFactory.cs
+++ Obi/ObiNodeFactory.cs
@@ -6,15 +6,22 @@ namespace Obi
     /// <summary>
     /// Node factory that can create plain core nodes, or Obi nodes.
     /// </summary>
-    public class ObiNodeFactory : TreeNodeFactory
+    public class ObiNodeFactory 
     {
+        private Presentation m_Presentation;
+
+        public ObiNodeFactory(Presentation presentation)
+        {
+            m_Presentation = presentation;
+        }
+
         /// <summary>
         /// Create a new node given a QName.
         /// </summary>
         /// <param name="localName">the local part of the qname.</param>
         /// <param name="namespaceUri">the namespace URI of the qname.</param>
         /// <returns>A new node or null if the qname corresponds to no known node.</returns>
-        public override TreeNode createNode(string localName, string namespaceUri)
+        public TreeNode createNode(string localName, string namespaceUri)
         {
             if (namespaceUri == DataModelFactory.NS)
             {
@@ -41,6 +48,6 @@ namespace Obi
         public override string getXukNamespaceUri() { return DataModelFactory.NS; }
 
         // Override getPresentation() to return an Obi-specific presentation
-        private Presentation Presentation { get { return (Presentation)getPresentation(); } }
+        private Presentation Presentation { get { return m_Presentation; } }
     }
 }
