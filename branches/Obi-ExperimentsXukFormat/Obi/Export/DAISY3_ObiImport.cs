7bf2beadc928dc39b49ab77c41f25a4d0abaa9ee
avneesh-singh <avneesh-singh@f04d5631-4067-4348-862e-39f3aacb62c1>
2011-03-14 07:20:09 +0000

    Obi imports structure from DTBook 2005. Need to resolve some problem in page import.

--- branches/Obi-ExperimentsXukFormat/Obi/Export/DAISY3_ObiImport.cs
+++ branches/Obi-ExperimentsXukFormat/Obi/Export/DAISY3_ObiImport.cs
@@ -4,9 +4,11 @@ using System.Text;
 using System.Xml;
 using System.Xml.XPath;
 using System.IO;
+using System.Diagnostics;
 
 using urakawa;
 using urakawa.core;
+using urakawa.property.xml;
 using urakawa.daisy;
 using urakawa.daisy.import;
 using urakawa.media.data.audio;
@@ -26,7 +28,7 @@ namespace Obi.Export
         {
             m_Session = session;
             XukPath = Path.Combine(m_outDirectory, "project.obi");
-            this.AudioNCXImport = true;
+            if ( System.IO.Path.GetExtension(bookfile).ToLower() == ".opf")     this.AudioNCXImport = true;
         }
         //protected override void CreateProjectFileAndDirectory()
         //{
@@ -266,5 +268,361 @@ namespace Obi.Export
             }
             return dtbBookTitle;
         }
+
+
+        protected override void parseContentDocument(XmlNode xmlNode, TreeNode parentTreeNode, string filePath)
+        {
+            if (RequestCancellation) return;
+
+            XmlNodeType xmlType = xmlNode.NodeType;
+            switch (xmlType)
+            {
+                case XmlNodeType.Attribute:
+                    {
+                        System.Diagnostics.Debug.Fail("Calling this method with an XmlAttribute should never happen !!");
+                        break;
+                    }
+                case XmlNodeType.Document:
+                    {
+                        /*
+                        XmlDocument xmlDoc = ((XmlDocument)xmlNode);
+                        XmlNodeList styleSheetNodeList = xmlDoc.SelectNodes
+                                                      ("/processing-instruction(\"xml-stylesheet\")");
+                        if (styleSheetNodeList != null && styleSheetNodeList.Count > 0)
+                        {
+                            AddStyleSheetsToXuk(styleSheetNodeList);
+                        }
+                        */
+                        XmlNode bodyElement = XmlDocumentHelper.GetFirstChildElementWithName(xmlNode, true, "body", null);
+
+                        if (bodyElement == null)
+                        {
+                            bodyElement = XmlDocumentHelper.GetFirstChildElementWithName(xmlNode, true, "book", null);
+                        }
+
+                        if (bodyElement != null)
+                        {
+                            Presentation presentation = Project.Presentations.Get(0);
+                            presentation.PropertyFactory.DefaultXmlNamespaceUri = bodyElement.NamespaceURI;
+                            /*
+                            // preserve internal DTD if it exists in dtbook 
+                            string strInternalDTD = ExtractInternalDTD(((XmlDocument)xmlNode).DocumentType);
+                            if (strInternalDTD != null)
+                            {
+                                byte[] bytesArray = System.Text.Encoding.UTF8.GetBytes(strInternalDTD);
+                                MemoryStream ms = new MemoryStream(bytesArray);
+ExternalFiles.ExternalFileData dtdEfd = presentation.ExternalFilesDataFactory.Create<ExternalFiles.DTDExternalFileData>();
+                                dtdEfd.InitializeWithData(ms, "DTBookLocalDTD.dtd", false);
+                            }
+                            */
+                            parseContentDocument(bodyElement, parentTreeNode, filePath);
+                        }
+                        //parseContentDocument(((XmlDocument)xmlNode).DocumentElement, parentTreeNode);
+                        break;
+                    }
+                case XmlNodeType.Element:
+                    {
+                        Presentation presentation = Project.Presentations.Get(0);
+
+                        TreeNode treeNode = null ;
+
+                        if (parentTreeNode == null)
+                        {
+                            treeNode = presentation.TreeNodeFactory.Create();
+                            presentation.RootNode = treeNode;
+                            parentTreeNode = presentation.RootNode;
+                        }
+                        else
+                        {
+                            treeNode = CreateAndAddTreeNodeForContentDocument(parentTreeNode, xmlNode);
+                            //parentTreeNode.AppendChild(treeNode);
+                        }
+
+                        XmlProperty xmlProp = presentation.PropertyFactory.CreateXmlProperty();
+                        treeNode.AddProperty(xmlProp);
+                        xmlProp.LocalName = xmlNode.LocalName;
+                        if (xmlNode.ParentNode != null && xmlNode.ParentNode.NodeType == XmlNodeType.Document)
+                        {
+                            presentation.PropertyFactory.DefaultXmlNamespaceUri = xmlNode.NamespaceURI;
+                        }
+
+                        if (xmlNode.NamespaceURI != presentation.PropertyFactory.DefaultXmlNamespaceUri)
+                        {
+                            xmlProp.NamespaceUri = xmlNode.NamespaceURI;
+                        }
+
+                        string updatedSRC = null;
+                        /*
+                        if (xmlNode.LocalName == "img")
+                        {
+                            XmlNode getSRC = xmlNode.Attributes.GetNamedItem("src");
+                            if (getSRC != null)
+                            {
+                                string relativePath = xmlNode.Attributes.GetNamedItem("src").Value;
+                                if (!relativePath.StartsWith("http://"))
+                                {
+                                    
+                                    string parentPath = Directory.GetParent(filePath).FullName;
+                                    string imgSourceFullpath = Path.Combine(parentPath, relativePath);
+
+                                    if (File.Exists(imgSourceFullpath))
+                                    {
+                                        updatedSRC = Path.GetFullPath(imgSourceFullpath).Replace(
+                                            Path.GetDirectoryName(m_Book_FilePath), "");
+                                        if (updatedSRC.StartsWith("" + Path.DirectorySeparatorChar))
+                                        {
+                                            updatedSRC = updatedSRC.Remove(0, 1);
+                                        }
+
+
+                                        //ChannelsProperty chProp = presentation.PropertyFactory.CreateChannelsProperty();
+                                        //treeNode.AddProperty(chProp);
+                                        ChannelsProperty chProp = treeNode.GetOrCreateChannelsProperty();
+
+                                        urakawa.media.data.image.ImageMediaData imageData =
+                                            CreateImageMediaData(presentation, Path.GetExtension(imgSourceFullpath));
+                                        imageData.InitializeImage(imgSourceFullpath, updatedSRC);
+                                        media.data.image.ManagedImageMedia managedImage =
+                                            presentation.MediaFactory.CreateManagedImageMedia();
+                                        managedImage.MediaData = imageData;
+                                        chProp.SetMedia(m_ImageChannel, managedImage);
+                                    }
+                                    else
+                                    {
+                                        ExternalImageMedia externalImage = presentation.MediaFactory.CreateExternalImageMedia();
+                                        externalImage.Src = relativePath;
+
+                                        ChannelsProperty chProp = treeNode.GetOrCreateChannelsProperty();
+                                        chProp.SetMedia(m_ImageChannel, externalImage);
+                                    }
+                                     
+                                }
+                         
+                            }
+                         
+                        }
+                        */
+
+                        /*
+                        XmlAttributeCollection attributeCol = xmlNode.Attributes;
+
+                        if (attributeCol != null)
+                        {
+                            for (int i = 0; i < attributeCol.Count; i++)
+                            {
+                                XmlNode attr = attributeCol.Item(i);
+                                if (attr.LocalName != "smilref"
+                                    && attr.LocalName != "imgref") // && attr.Name != "xmlns:xsi" && attr.Name != "xml:space"
+                                {
+                                    if (attr.Name.Contains(":"))
+                                    {
+                                        string[] splitArray = attr.Name.Split(':');
+
+                                        if (splitArray[0] == "xmlns")
+                                        {
+                                            if (xmlNode.LocalName == "book" || treeNode.Parent == null)
+                                            {
+                                                xmlProp.SetAttribute(attr.Name, attr.NamespaceURI, attr.Value);
+                                            }
+                                        }
+                                        else
+                                        {
+                                            xmlProp.SetAttribute(attr.Name, attr.NamespaceURI, attr.Value);
+                                        }
+                                    }
+                                    else if (updatedSRC != null && attr.LocalName == "src")
+                                    {
+                                        xmlProp.SetAttribute(attr.LocalName, "", updatedSRC);
+                                    }
+                                    else
+                                    {
+                                        if (attr.LocalName == "xmlns")
+                                        {
+                                            if (attr.Value != presentation.PropertyFactory.DefaultXmlNamespaceUri)
+                                            {
+                                                xmlProp.SetAttribute(attr.LocalName, "", attr.Value);
+                                            }
+                                        }
+                                        else if (string.IsNullOrEmpty(attr.NamespaceURI)
+                                            || attr.NamespaceURI == presentation.PropertyFactory.DefaultXmlNamespaceUri)
+                                        {
+                                            xmlProp.SetAttribute(attr.LocalName, "", attr.Value);
+                                        }
+                                        else
+                                        {
+                                            xmlProp.SetAttribute(attr.Name, attr.NamespaceURI, attr.Value);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        */
+                        if (xmlNode.LocalName == "h1" || xmlNode.LocalName == "h2" || xmlNode.LocalName == "h3"
+                            || xmlNode.LocalName == "h4" || xmlNode.LocalName == "h5" || xmlNode.LocalName == "h6" || xmlNode.LocalName == "HD")
+                        {
+                            ((SectionNode)parentTreeNode).Label = xmlNode.InnerText;
+                        }
+
+                        if (RequestCancellation) return;
+                        foreach (XmlNode childXmlNode in xmlNode.ChildNodes)
+                        {
+                            parseContentDocument(childXmlNode, treeNode, filePath);
+                        }
+                        break;
+                    }
+                case XmlNodeType.Whitespace:
+                case XmlNodeType.CDATA:
+                case XmlNodeType.SignificantWhitespace:
+                case XmlNodeType.Text:
+                    {
+                        /*
+                        Presentation presentation = Project.Presentations.Get(0);
+
+                        if (xmlType == XmlNodeType.Whitespace)
+                        {
+                            bool onlySpaces = true;
+                            for (int i = 0; i < xmlNode.Value.Length; i++)
+                            {
+                                if (xmlNode.Value[i] != ' ')
+                                {
+                                    onlySpaces = false;
+                                    break;
+                                }
+                            }
+                            if (!onlySpaces)
+                            {
+                                break;
+                            }
+                            //else
+                            //{
+                            //    int l = xmlNode.Value.Length;
+                            //}
+                        }
+#if DEBUG
+                        if (xmlType == XmlNodeType.CDATA)
+                        {
+                            Debugger.Break();
+                        }
+
+                        if (xmlType == XmlNodeType.SignificantWhitespace)
+                        {
+                            Debugger.Break();
+                        }
+#endif
+                        //string text = xmlNode.Value.Trim();
+                        string text = System.Text.RegularExpressions.Regex.Replace(xmlNode.Value, @"\s+", " ");
+
+                        Debug.Assert(!string.IsNullOrEmpty(text));
+
+#if DEBUG
+                        if (text.Length != xmlNode.Value.Length)
+                        {
+                            int debug = 1;
+                            //Debugger.Break();
+                        }
+
+                        if (string.IsNullOrEmpty(text))
+                        {
+                            Debugger.Break();
+                        }
+                        if (xmlType != XmlNodeType.Whitespace && text == " ")
+                        {
+                            int debug = 1;
+                            //Debugger.Break();
+                        }
+#endif
+                        if (string.IsNullOrEmpty(text))
+                        {
+                            break;
+                        }
+                        urakawa.media.TextMedia textMedia = presentation.MediaFactory.CreateTextMedia();
+                        textMedia.Text = text;
+
+                        urakawa.property.channel.ChannelsProperty cProp = presentation.PropertyFactory.CreateChannelsProperty();
+                        cProp.SetMedia(m_textChannel, textMedia);
+
+
+                        int counter = 0;
+                        foreach (XmlNode childXmlNode in xmlNode.ParentNode.ChildNodes)
+                        {
+                            XmlNodeType childXmlType = childXmlNode.NodeType;
+                            if (childXmlType == XmlNodeType.Text
+                                || childXmlType == XmlNodeType.Element
+                                || childXmlType == XmlNodeType.Whitespace
+                                || childXmlType == XmlNodeType.SignificantWhitespace
+                                || childXmlType == XmlNodeType.CDATA)
+                            {
+                                counter++;
+                            }
+                        }
+                        if (counter == 1)
+                        {
+                            parentTreeNode.AddProperty(cProp);
+                        }
+                        else
+                        {
+                            TreeNode txtWrapperNode = presentation.TreeNodeFactory.Create();
+                            txtWrapperNode.AddProperty(cProp);
+                            parentTreeNode.AppendChild(txtWrapperNode);
+                        }
+                        */
+                        break;
+                         
+                    }
+                default:
+                    {
+                        return;
+                    }
+            }
+        }
+
+        private TreeNode CreateAndAddTreeNodeForContentDocument(TreeNode parentNode, XmlNode node)
+        {
+                        TreeNode createdNode = null;
+            if (node.LocalName == "level")
+            {
+                SectionNode treeNode = m_Presentation.CreateSectionNode();
+                createdNode = treeNode;
+                if (parentNode is ObiRootNode)
+                {
+                    ((ObiNode)parentNode).AppendChild(treeNode);
+                }
+                else
+                {
+                    ((SectionNode)parentNode).AppendChild(treeNode);
+                }
+            }
+            else if (node.LocalName == "pagenum")
+            {
+                PhraseNode treeNode = m_Presentation.CreatePhraseNode();
+                createdNode = treeNode;
+                ((SectionNode)parentNode).AppendChild(treeNode);
+
+                string strKind  = node.Attributes.GetNamedItem("page").Value;
+                string pageNumberString = node.InnerText;
+
+                PageKind kind = strKind == "front" ? PageKind.Front :
+                strKind == "normal" ? PageKind.Normal :
+                PageKind.Special;
+
+                PageNumber number = null;
+            if (kind == PageKind.Special && pageNumberString != null && pageNumberString != "")
+            {
+                number = new PageNumber(pageNumberString);
+            }
+            else if (kind == PageKind.Front || kind == PageKind.Normal)
+            {
+                int pageNumber = EmptyNode.SafeParsePageNumber(pageNumberString);
+                if (pageNumber > 0)
+                {
+                    number = new PageNumber(pageNumber, kind);
+                }
+            }
+            }
+            return createdNode;
+        }
+
+
+
     }
 }
