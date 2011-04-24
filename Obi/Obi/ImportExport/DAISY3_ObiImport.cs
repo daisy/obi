94262a39eac17a11557c7122661f4c2476f6e239
Avneesh Singh <13777742+avneeshsingh@users.noreply.github.com>
2011-04-24 08:20:31 +0000

    generator should also be removed while import

--- Obi/Obi/ImportExport/DAISY3_ObiImport.cs
+++ Obi/Obi/ImportExport/DAISY3_ObiImport.cs
@@ -670,6 +670,7 @@ ExternalFiles.ExternalFileData dtdEfd = presentation.ExternalFilesDataFactory.Cr
             m_MetadataItemsToExclude.Add(Metadata.DTB_MULTIMEDIA_TYPE);
             m_MetadataItemsToExclude.Add(Metadata.OBI_DAISY2ExportPath);
             m_MetadataItemsToExclude.Add(Metadata.OBI_DAISY3ExportPath);
+            m_MetadataItemsToExclude.Add(Metadata.GENERATOR);
         }
 
 
