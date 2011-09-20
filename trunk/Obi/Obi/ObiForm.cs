3bb27d8c55124827ecca9eb990f02276844fec7d
avneesh-singh <avneesh-singh@f04d5631-4067-4348-862e-39f3aacb62c1>
2011-09-20 09:45:28 +0000

    minor

--- trunk/Obi/Obi/ObiForm.cs
+++ trunk/Obi/Obi/ObiForm.cs
@@ -1766,6 +1766,7 @@ namespace Obi
                                     mSession.Presentation, exportPath, dialog.EncodeToMP3, AudioLib.SampleRate.Hz44100, audioFileSectionLevel);
                             }
                             DAISYExport.BitRate_Mp3 = dialog.BitRate;
+                            DAISYExport.EnableExplicitGarbageCollection = Settings.OptimizeMemory;
                         Status(String.Format(Localizer.Message("ObiFormStatusMsg_ExportingProject") , exportPath));
                                                 
                         ProgressDialog progress = new ProgressDialog ( Localizer.Message ( "export_progress_dialog_title" ),
