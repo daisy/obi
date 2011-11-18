6f93d6913a10370864b586cfc1fdb1411ed075a3
chhavi-negi <chhavi-negi@f04d5631-4067-4348-862e-39f3aacb62c1>
2011-11-18 04:51:27 +0000

    Associating and deassociating of nodes works now

--- Obi/Dialogs/AssociateSpecialNode.Designer.cs
+++ Obi/Dialogs/AssociateSpecialNode.Designer.cs
@@ -32,20 +32,25 @@ namespace Obi.Dialogs
             this.m_btn_OK = new System.Windows.Forms.Button();
             this.m_btn_Cancel = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.m_btn_Associate = new System.Windows.Forms.Button();
+            this.m_btn_Deassociate = new System.Windows.Forms.Button();
+            this.m_lb_listOfAllAnchorNodes = new System.Windows.Forms.ListBox();
+            this.m_btn_ShowAll = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // m_lb_ListOfSpecialNodes
             // 
             this.m_lb_ListOfSpecialNodes.FormattingEnabled = true;
-            this.m_lb_ListOfSpecialNodes.Location = new System.Drawing.Point(12, 86);
+            this.m_lb_ListOfSpecialNodes.Location = new System.Drawing.Point(146, 38);
             this.m_lb_ListOfSpecialNodes.Name = "m_lb_ListOfSpecialNodes";
-            this.m_lb_ListOfSpecialNodes.Size = new System.Drawing.Size(268, 108);
+            this.m_lb_ListOfSpecialNodes.Size = new System.Drawing.Size(256, 147);
             this.m_lb_ListOfSpecialNodes.TabIndex = 0;
+            this.m_lb_ListOfSpecialNodes.SelectedIndexChanged += new System.EventHandler(this.m_lb_ListOfSpecialNodes_SelectedIndexChanged);
             // 
             // m_btn_OK
             // 
             this.m_btn_OK.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.m_btn_OK.Location = new System.Drawing.Point(36, 229);
+            this.m_btn_OK.Location = new System.Drawing.Point(222, 238);
             this.m_btn_OK.Name = "m_btn_OK";
             this.m_btn_OK.Size = new System.Drawing.Size(75, 23);
             this.m_btn_OK.TabIndex = 1;
@@ -55,25 +60,68 @@ namespace Obi.Dialogs
             // m_btn_Cancel
             // 
             this.m_btn_Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.m_btn_Cancel.Location = new System.Drawing.Point(163, 228);
+            this.m_btn_Cancel.Location = new System.Drawing.Point(314, 238);
             this.m_btn_Cancel.Name = "m_btn_Cancel";
-            this.m_btn_Cancel.Size = new System.Drawing.Size(75, 23);
+            this.m_btn_Cancel.Size = new System.Drawing.Size(88, 23);
             this.m_btn_Cancel.TabIndex = 2;
             this.m_btn_Cancel.Text = "Cancel";
             this.m_btn_Cancel.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(49, 40);
+            this.textBox1.Location = new System.Drawing.Point(6, 12);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 3;
+            // 
+            // m_btn_Associate
+            // 
+            this.m_btn_Associate.Location = new System.Drawing.Point(11, 238);
+            this.m_btn_Associate.Name = "m_btn_Associate";
+            this.m_btn_Associate.Size = new System.Drawing.Size(95, 23);
+            this.m_btn_Associate.TabIndex = 4;
+            this.m_btn_Associate.Text = "Associate";
+            this.m_btn_Associate.UseVisualStyleBackColor = true;
+            this.m_btn_Associate.Click += new System.EventHandler(this.m_btn_Associate_Click);
+            // 
+            // m_btn_Deassociate
+            // 
+            this.m_btn_Deassociate.Location = new System.Drawing.Point(122, 238);
+            this.m_btn_Deassociate.Name = "m_btn_Deassociate";
+            this.m_btn_Deassociate.Size = new System.Drawing.Size(88, 23);
+            this.m_btn_Deassociate.TabIndex = 5;
+            this.m_btn_Deassociate.Text = "Deasscociate";
+            this.m_btn_Deassociate.UseVisualStyleBackColor = true;
+            this.m_btn_Deassociate.Click += new System.EventHandler(this.m_btn_Deassociate_Click);
+            // 
+            // m_lb_listOfAllAnchorNodes
+            // 
+            this.m_lb_listOfAllAnchorNodes.FormattingEnabled = true;
+            this.m_lb_listOfAllAnchorNodes.Location = new System.Drawing.Point(6, 38);
+            this.m_lb_listOfAllAnchorNodes.Name = "m_lb_listOfAllAnchorNodes";
+            this.m_lb_listOfAllAnchorNodes.Size = new System.Drawing.Size(120, 147);
+            this.m_lb_listOfAllAnchorNodes.TabIndex = 6;
+            this.m_lb_listOfAllAnchorNodes.SelectedIndexChanged += new System.EventHandler(this.m_lb_listOfAllAnchorNodes_SelectedIndexChanged);
+            // 
+            // m_btn_ShowAll
+            // 
+            this.m_btn_ShowAll.Location = new System.Drawing.Point(12, 204);
+            this.m_btn_ShowAll.Name = "m_btn_ShowAll";
+            this.m_btn_ShowAll.Size = new System.Drawing.Size(75, 23);
+            this.m_btn_ShowAll.TabIndex = 7;
+            this.m_btn_ShowAll.Text = "Show all";
+            this.m_btn_ShowAll.UseVisualStyleBackColor = true;
+            this.m_btn_ShowAll.Click += new System.EventHandler(this.m_btn_ShowAll_Click);
             // 
             // AssociateSpecialNode
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.ClientSize = new System.Drawing.Size(414, 273);
+            this.Controls.Add(this.m_btn_ShowAll);
+            this.Controls.Add(this.m_lb_listOfAllAnchorNodes);
+            this.Controls.Add(this.m_btn_Deassociate);
+            this.Controls.Add(this.m_btn_Associate);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.m_btn_Cancel);
             this.Controls.Add(this.m_btn_OK);
@@ -91,5 +139,9 @@ namespace Obi.Dialogs
         private System.Windows.Forms.Button m_btn_OK;
         private System.Windows.Forms.Button m_btn_Cancel;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button m_btn_Associate;
+        private System.Windows.Forms.Button m_btn_Deassociate;
+        private System.Windows.Forms.ListBox m_lb_listOfAllAnchorNodes;
+        private System.Windows.Forms.Button m_btn_ShowAll;
     }
 }
