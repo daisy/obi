b380c502543b2b6737596f542c3b1fb7dbf4f7c9
avneesh-singh <avneesh-singh@f04d5631-4067-4348-862e-39f3aacb62c1>
2011-12-21 07:40:41 +0000

    minor

--- branches/Obi-SkippableElements/Obi/Commands/Node/AssociateAnchorNode.cs
+++ branches/Obi-SkippableElements/Obi/Commands/Node/AssociateAnchorNode.cs
@@ -9,12 +9,14 @@ namespace Obi.Commands.Node
         private EmptyNode m_Node;
         private EmptyNode m_NodeToBeAssociated;
         private EmptyNode m_PreviouslyAssociatedNode;
+        private EmptyNode.Role m_PreviouslyAssignedRoleToAnchor;
 
         public AssociateAnchorNode(ProjectView.ProjectView view, EmptyNode anchor, EmptyNode nodeToBeAssociated):base ( view )
         {
             m_Node = anchor;
             m_NodeToBeAssociated = nodeToBeAssociated;
             m_PreviouslyAssociatedNode = anchor.AssociatedNode;
+            m_PreviouslyAssignedRoleToAnchor = anchor.Role_;
         }
 
         public AssociateAnchorNode(ProjectView.ProjectView view, EmptyNode nodeToBeAssociated): 
@@ -27,13 +29,15 @@ namespace Obi.Commands.Node
         public override void Execute()
         {
             m_Node.AssociatedNode = m_NodeToBeAssociated ;
+            if (m_Node.Role_ != EmptyNode.Role.Anchor) m_Node.Role_ = EmptyNode.Role.Anchor;
             if (UpdateSelection) View.SelectedBlockNode = m_Node;
         }
 
         public override void UnExecute()
-        {
+        {   
             m_Node.AssociatedNode = m_PreviouslyAssociatedNode;
-            base.UnExecute();
+            m_Node.Role_ = m_PreviouslyAssignedRoleToAnchor;
+                        base.UnExecute();
         }
     
 
