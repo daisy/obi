cf79109f0a472a33eac7fdca8f68d6d69b9bb19c
Avneesh Singh <13777742+avneeshsingh@users.noreply.github.com>
2012-08-25 16:45:49 +0000

    update to code of live phrase detection. Right now it is not stable enough. Setting also added to switch it off.

--- Obi/Obi/ProjectView/TransportBar.cs
+++ Obi/Obi/ProjectView/TransportBar.cs
@@ -1886,9 +1886,12 @@ namespace Obi.ProjectView
         // Stop recording a phrase
         private void RecordingPhraseEnded(Obi.Events.Audio.Recorder.PhraseEventArgs e)
         {
-            PhraseNode phrase = (PhraseNode)mRecordingSection.PhraseChild(e.PhraseIndex + mRecordingInitPhraseIndex);
-            phrase.SignalAudioChanged(this, e.Audio);
-            mRecordingPhrase = null;
+            if (!mView.ObiForm.Settings.Audio_EnableLivePhraseDetection) //@onTheFly: following code should be executed if live phrase detection is disabled
+            {
+                PhraseNode phrase = (PhraseNode)mRecordingSection.PhraseChild(e.PhraseIndex + mRecordingInitPhraseIndex);
+                phrase.SignalAudioChanged(this, e.Audio);
+                mRecordingPhrase = null;
+            }
         }
 
         // Start recording a new page, set the right page number
