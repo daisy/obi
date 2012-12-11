1ab8fbcc9f421f7db1e9b025da96b2f50ed2698a
avneesh-singh <avneesh-singh@f04d5631-4067-4348-862e-39f3aacb62c1>
2012-12-11 16:14:18 +0000

    prevented creation of too small phrases during live phrase detection

--- trunk/Obi/Obi/ProjectView/TransportBar.cs
+++ trunk/Obi/Obi/ProjectView/TransportBar.cs
@@ -2890,7 +2890,8 @@ UpdateButtons();
                     if (newTimingList[j] < referenceTimeForPhrase) break;
                     //Commands.Node.SplitAudio split = new Commands.Node.SplitAudio(mView, phrase, newTimingList[j]);
                     double splitTimeInPhrase = newTimingList[j] - referenceTimeForPhrase;
-                    if ( splitTimeInPhrase <= 0 || splitTimeInPhrase>= phrase.Duration ) continue ;
+                    //avoid invalid time and also too small phrases, less than 200ms
+                    if ( splitTimeInPhrase <= 0 || splitTimeInPhrase>= (phrase.Duration - 200) ) continue ;
                     CompositeCommand split = Commands.Node.SplitAudio.GetSplitCommand(mView, phrase,splitTimeInPhrase );
                     multipleSplitCommand.ChildCommands.Insert(multipleSplitCommand.ChildCommands.Count, split);
                     newTimingList.RemoveAt(j);
