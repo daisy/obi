d319f22ee0f946a798e3c3b6b575f05d6b947f91
rohit_tandon <rohit_tandon@f04d5631-4067-4348-862e-39f3aacb62c1>
2013-04-04 08:35:35 +0000

    Peak meter behaviour modified.

--- trunk/Obi/Obi/Audio/PeakMeterForm.cs
+++ trunk/Obi/Obi/Audio/PeakMeterForm.cs
@@ -12,11 +12,16 @@ namespace Obi.Audio
 	public partial class PeakMeterForm : Form
 	{
         HelpProvider helpProvider1;
+        public bool flagFirstTimeInit = false;
+        private int tempFormHeight = 0;
 
 		public PeakMeterForm()
 		{
 			InitializeComponent();
             this.TopMost = true;
+            mGraphicalPeakMeter.Dock = DockStyle.None;
+            flagFirstTimeInit = true;
+            this.chkOnTop.Location = new Point(this.chkOnTop.Location.X, mGraphicalPeakMeter.Height);
             helpProvider1 = new HelpProvider();
             helpProvider1.HelpNamespace = Localizer.Message("CHMhelp_file_name");
             helpProvider1.SetHelpNavigator(this, HelpNavigator.Topic);
@@ -42,5 +47,63 @@ namespace Obi.Audio
 				mGraphicalPeakMeter.SourceVuMeter = value;
 			}
 		}
+        public void GraphicalPeakMeterDocPropSet()
+        {
+            mGraphicalPeakMeter.Dock = DockStyle.None;
+        }
+        public void PeakMeterInit()
+        {
+            mGraphicalPeakMeter.Size = new Size(154, 580);
+            this.chkOnTop.Location = new Point(this.chkOnTop.Location.X, mGraphicalPeakMeter.Height);
+            //this.Size=new Size(170,640);
+            //this.mGraphicalPeakMeter.Height = this.Height;
+            //this.Height = this.Height + 10;
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Peakmeter Size {0}", mGraphicalPeakMeter.Size);
+            flagFirstTimeInit = false;
+
+
+        }
+        public void GraphicaPeakMeterSizeSet(Settings set)
+        {
+            mGraphicalPeakMeter.Size = set.GraphicalPeakMeterContolSize;
+            this.chkOnTop.Location = new Point(this.chkOnTop.Location.X, mGraphicalPeakMeter.Height);
+        }
+        public void GraphicalPeakMeterSaveSettings(Settings set)
+        {
+            set.GraphicalPeakMeterContolSize = this.mGraphicalPeakMeter.Size;
+        }
+
+        private void chkOnTop_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.chkOnTop.Checked)
+            {
+                this.TopMost = true;
+            }
+            else
+            {
+                this.TopMost = false;
+            }
+        }
+
+        private void PeakMeterForm_ResizeBegin(object sender, EventArgs e)
+        {
+            tempFormHeight = this.Height;
+            Console.WriteLine("Peakmeter resize begin event called");
+        }
+
+        private void PeakMeterForm_ResizeEnd(object sender, EventArgs e)
+        {
+            if (flagFirstTimeInit == false)
+            {
+                int diff = this.Height - tempFormHeight;
+                Console.WriteLine("Difference is {0}", diff);
+                this.mGraphicalPeakMeter.Height = mGraphicalPeakMeter.Height + diff;
+                this.chkOnTop.Location = new Point(this.chkOnTop.Location.X, mGraphicalPeakMeter.Height);
+            }
+        }
 	}
 }
