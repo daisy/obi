f6444b2f7741782470b8498c5b505a7da4004a15
avneesh-singh <avneesh-singh@f04d5631-4067-4348-862e-39f3aacb62c1>
2013-04-14 11:22:54 +0000

    background worker added to upload user info

--- trunk/Obi/Obi/Dialogs/UserRegistration.cs
+++ trunk/Obi/Obi/Dialogs/UserRegistration.cs
@@ -15,7 +15,8 @@ namespace Obi.Dialogs
         public static readonly string Registered = "Registered";
         public static readonly int MaxUploadAttemptsAllowed = 10;
 
-        private Settings m_Settings;
+        private static Settings m_Settings;
+        private static BackgroundWorker m_BackgroundWorker = new BackgroundWorker();
 
         private UserRegistration()
         {
@@ -28,74 +29,90 @@ namespace Obi.Dialogs
             m_Settings = settings;
         }
 
-        public static void UploadUserInformation(string usersInfo)
+        public static void UploadUserInformation(Settings settings)
         {
-            //return;
-            string dataToUpload = "un:User Name,Email:abc@abc.com,org:Organization Name,ct:Delhi,con:India";
+            // the upload data should be in following format
+            // "un:User Name,em:abc@abc.com,og:Organization Name,ct:Delhi,cn:India";
             
-            System.IO.MemoryStream memoryStream = null;
-            System.IO.Stream  stream = null;
-            try
+            if (settings == null || m_BackgroundWorker.IsBusy) return;
+            m_Settings = settings;
+
+            m_BackgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(delegate(object sender, System.ComponentModel.DoWorkEventArgs e)
             {
-                string fileName = System.DateTime.Now.ToShortDateString() + System.DateTime.Now.ToShortTimeString() + System.DateTime.Now.ToUniversalTime().ToShortTimeString();
-                fileName = fileName.Replace(":", "") + ".txt";
-                fileName = fileName.Replace("/", "");
                 
-                byte[] byteArray = Encoding.UTF8.GetBytes(dataToUpload);
-                memoryStream = new System.IO.MemoryStream(byteArray);
-                
-                // Create FtpWebRequest object
-                string uploadPath = "ftp://ftp.drivehq.com/users-info/user1.txt";
-                FtpWebRequest ftpRequest = (System.Net.FtpWebRequest)System.Net.FtpWebRequest.Create(new Uri(uploadPath));
-                // Provide username and password
-                string userName = "obi-ftp";
-                string pass = "obi";
-                ftpRequest.Credentials = new System.Net.NetworkCredential(userName, pass);
-
-                
-                // after a command is executed, do not keep connection alive 
-                ftpRequest.KeepAlive = false;
-                ftpRequest.Timeout = 30000;
-                // Specify the command to be executed 
-                ftpRequest.Method = System.Net.WebRequestMethods.Ftp.UploadFile;
-                                ftpRequest.UseBinary = true;
-
-                ftpRequest.ContentLength = memoryStream.Length;
-                // buffer size is kept 2kb. 
-                int bufferLength = 2048;
-                byte[] buff = new byte[bufferLength];
-                
-
-                stream = ftpRequest.GetRequestStream();
-                // Read the memory stream 2kb at a time and copy to ftpStream
-                int dataLength = memoryStream.Read(buff, 0, bufferLength);
-                
-                while (dataLength != 0)
+                string dataToUpload = settings.UsersInfoToUpload;
+                System.IO.MemoryStream memoryStream = null;
+                System.IO.Stream stream = null;
+                try
                 {
-                    // Write Content from the memory stream to the FTP Upload Stream. 
-                    stream.Write(buff, 0, dataLength);
-                    dataLength = memoryStream.Read(buff, 0, bufferLength);
+                    string fileName = System.DateTime.Now.ToShortDateString() + System.DateTime.Now.ToShortTimeString() + System.DateTime.Now.ToUniversalTime().ToShortTimeString();
+                    fileName = fileName.Replace(":", "") + ".txt";
+                    fileName = fileName.Replace("/", "");
+
+                    byte[] byteArray = Encoding.UTF8.GetBytes(dataToUpload);
+                    memoryStream = new System.IO.MemoryStream(byteArray);
+
+                    // Create FtpWebRequest object
+                    string uploadPath = "ftp://ftp.drivehq.com/users-info/user1.txt";
+                    FtpWebRequest ftpRequest = (System.Net.FtpWebRequest)System.Net.FtpWebRequest.Create(new Uri(uploadPath));
+                    // Provide username and password
+                    string userName = "obi-ftp";
+                    string pass = "obi";
+                    ftpRequest.Credentials = new System.Net.NetworkCredential(userName, pass);
+
+
+                    // after a command is executed, do not keep connection alive 
+                    ftpRequest.KeepAlive = false;
+                    ftpRequest.Timeout = 30000;
+                    // Specify the command to be executed 
+                    ftpRequest.Method = System.Net.WebRequestMethods.Ftp.UploadFile;
+                    ftpRequest.UseBinary = true;
+
+                    ftpRequest.ContentLength = memoryStream.Length;
+                    // buffer size is kept 2kb. 
+                    int bufferLength = 2048;
+                    byte[] buff = new byte[bufferLength];
+
+
+                    stream = ftpRequest.GetRequestStream();
+                    // Read the memory stream 2kb at a time and copy to ftpStream
+                    int dataLength = memoryStream.Read(buff, 0, bufferLength);
+
+                    while (dataLength != 0)
+                    {
+                        // Write Content from the memory stream to the FTP Upload Stream. 
+                        stream.Write(buff, 0, dataLength);
+                        dataLength = memoryStream.Read(buff, 0, bufferLength);
+                    }
+                    // if successful then update settings for the same
+                    m_Settings.UsersInfoToUpload = Registered;
+                    m_Settings.SaveSettings();
                 }
-                
-            }
-            catch (System.Exception ex)
+                catch (System.Exception ex)
+                {
+                    m_Settings.UploadAttemptsCount = m_Settings.UploadAttemptsCount++;
+                    MessageBox.Show(ex.ToString());
+                }
+                finally
+                {
+                    if (stream != null)
+                    {
+                        stream.Close();
+                        stream.Dispose();
+                    }
+                    if (memoryStream != null)
+                    {
+                        memoryStream.Close();
+                        memoryStream.Dispose();
+                    }
+                }
+            });
+
+            m_BackgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(delegate(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
             {
-                MessageBox.Show(ex.ToString() );
-            }
-            finally
-            {   
-                if (stream != null)
-                {
-                    stream.Close();
-                    stream.Dispose();
-                }
-                if (memoryStream != null)
-                {
-                    memoryStream.Close();
-                    memoryStream.Dispose();
-                }
-            }
-            MessageBox.Show("done");
+                MessageBox.Show("done");
+            });
+            m_BackgroundWorker.RunWorkerAsync();
         }
 
         private void m_btnOk_Click(object sender, EventArgs e)
