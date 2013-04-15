65cdc5e692dc3f3a63a39a93f1f27096a1d67bd1
rachana-singh <rachana-singh@f04d5631-4067-4348-862e-39f3aacb62c1>
2013-04-15 07:27:34 +0000

    UI changes done in User Registration form.

--- trunk/Obi/Obi/Dialogs/UserRegistration.Designer.cs
+++ trunk/Obi/Obi/Dialogs/UserRegistration.Designer.cs
@@ -29,32 +29,241 @@ namespace Obi.Dialogs
         private void InitializeComponent()
         {
             this.m_btnOk = new System.Windows.Forms.Button();
+            this.m_btnRemindMeLater = new System.Windows.Forms.Button();
+            this.m_grpBoxUserInformation = new System.Windows.Forms.GroupBox();
+            this.m_grpBoxObiUse = new System.Windows.Forms.GroupBox();
+            this.m_txtBoxObiInformation = new System.Windows.Forms.TextBox();
+            this.m_lblName = new System.Windows.Forms.Label();
+            this.m_lblEmail = new System.Windows.Forms.Label();
+            this.m_lblOrgName = new System.Windows.Forms.Label();
+            this.m_lblCity = new System.Windows.Forms.Label();
+            this.m_lblCountry = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.m_lblSelectRadioButton = new System.Windows.Forms.Label();
+            this.m_rdbDaisyProduction = new System.Windows.Forms.RadioButton();
+            this.m_rdbTryingObi = new System.Windows.Forms.RadioButton();
+            this.m_grpBoxUserInformation.SuspendLayout();
+            this.m_grpBoxObiUse.SuspendLayout();
             this.SuspendLayout();
             // 
             // m_btnOk
             // 
-            this.m_btnOk.Location = new System.Drawing.Point(197, 227);
+            this.m_btnOk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.m_btnOk.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.m_btnOk.Location = new System.Drawing.Point(58, 344);
             this.m_btnOk.Name = "m_btnOk";
-            this.m_btnOk.Size = new System.Drawing.Size(75, 23);
-            this.m_btnOk.TabIndex = 0;
-            this.m_btnOk.Text = "OK";
+            this.m_btnOk.Size = new System.Drawing.Size(95, 23);
+            this.m_btnOk.TabIndex = 15;
+            this.m_btnOk.Text = "&OK";
             this.m_btnOk.UseVisualStyleBackColor = true;
             this.m_btnOk.Click += new System.EventHandler(this.m_btnOk_Click);
+            // 
+            // m_btnRemindMeLater
+            // 
+            this.m_btnRemindMeLater.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.m_btnRemindMeLater.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.m_btnRemindMeLater.Location = new System.Drawing.Point(193, 344);
+            this.m_btnRemindMeLater.Name = "m_btnRemindMeLater";
+            this.m_btnRemindMeLater.Size = new System.Drawing.Size(95, 23);
+            this.m_btnRemindMeLater.TabIndex = 16;
+            this.m_btnRemindMeLater.Text = "&Remind me later";
+            this.m_btnRemindMeLater.UseVisualStyleBackColor = true;
+            this.m_btnRemindMeLater.Click += new System.EventHandler(this.m_btnRemindMeLater_Click);
+            // 
+            // m_grpBoxUserInformation
+            // 
+            this.m_grpBoxUserInformation.Controls.Add(this.textBox5);
+            this.m_grpBoxUserInformation.Controls.Add(this.textBox4);
+            this.m_grpBoxUserInformation.Controls.Add(this.textBox3);
+            this.m_grpBoxUserInformation.Controls.Add(this.textBox2);
+            this.m_grpBoxUserInformation.Controls.Add(this.textBox1);
+            this.m_grpBoxUserInformation.Controls.Add(this.m_lblCountry);
+            this.m_grpBoxUserInformation.Controls.Add(this.m_lblCity);
+            this.m_grpBoxUserInformation.Controls.Add(this.m_lblOrgName);
+            this.m_grpBoxUserInformation.Controls.Add(this.m_lblEmail);
+            this.m_grpBoxUserInformation.Controls.Add(this.m_lblName);
+            this.m_grpBoxUserInformation.Location = new System.Drawing.Point(12, 68);
+            this.m_grpBoxUserInformation.Name = "m_grpBoxUserInformation";
+            this.m_grpBoxUserInformation.Size = new System.Drawing.Size(317, 184);
+            this.m_grpBoxUserInformation.TabIndex = 2;
+            this.m_grpBoxUserInformation.TabStop = false;
+            // 
+            // m_grpBoxObiUse
+            // 
+            this.m_grpBoxObiUse.Controls.Add(this.m_rdbTryingObi);
+            this.m_grpBoxObiUse.Controls.Add(this.m_rdbDaisyProduction);
+            this.m_grpBoxObiUse.Controls.Add(this.m_lblSelectRadioButton);
+            this.m_grpBoxObiUse.Location = new System.Drawing.Point(12, 253);
+            this.m_grpBoxObiUse.Name = "m_grpBoxObiUse";
+            this.m_grpBoxObiUse.Size = new System.Drawing.Size(317, 77);
+            this.m_grpBoxObiUse.TabIndex = 3;
+            this.m_grpBoxObiUse.TabStop = false;
+            // 
+            // m_txtBoxObiInformation
+            // 
+            this.m_txtBoxObiInformation.Location = new System.Drawing.Point(12, 7);
+            this.m_txtBoxObiInformation.Multiline = true;
+            this.m_txtBoxObiInformation.Name = "m_txtBoxObiInformation";
+            this.m_txtBoxObiInformation.ReadOnly = true;
+            this.m_txtBoxObiInformation.Size = new System.Drawing.Size(317, 66);
+            this.m_txtBoxObiInformation.TabIndex = 1;
+            this.m_txtBoxObiInformation.Text = "Obi is a free open source software.";
+            // 
+            // m_lblName
+            // 
+            this.m_lblName.AutoSize = true;
+            this.m_lblName.Location = new System.Drawing.Point(6, 16);
+            this.m_lblName.Name = "m_lblName";
+            this.m_lblName.Size = new System.Drawing.Size(38, 13);
+            this.m_lblName.TabIndex = 2;
+            this.m_lblName.Text = "&Name:";
+            // 
+            // m_lblEmail
+            // 
+            this.m_lblEmail.AutoSize = true;
+            this.m_lblEmail.Location = new System.Drawing.Point(6, 48);
+            this.m_lblEmail.Name = "m_lblEmail";
+            this.m_lblEmail.Size = new System.Drawing.Size(38, 13);
+            this.m_lblEmail.TabIndex = 4;
+            this.m_lblEmail.Text = "&Email :";
+            // 
+            // m_lblOrgName
+            // 
+            this.m_lblOrgName.AutoSize = true;
+            this.m_lblOrgName.Location = new System.Drawing.Point(6, 85);
+            this.m_lblOrgName.Name = "m_lblOrgName";
+            this.m_lblOrgName.Size = new System.Drawing.Size(103, 13);
+            this.m_lblOrgName.TabIndex = 6;
+            this.m_lblOrgName.Text = "&Organization Name :";
+            // 
+            // m_lblCity
+            // 
+            this.m_lblCity.AutoSize = true;
+            this.m_lblCity.Location = new System.Drawing.Point(6, 121);
+            this.m_lblCity.Name = "m_lblCity";
+            this.m_lblCity.Size = new System.Drawing.Size(107, 13);
+            this.m_lblCity.TabIndex = 8;
+            this.m_lblCity.Text = "&City/State/Province :";
+            // 
+            // m_lblCountry
+            // 
+            this.m_lblCountry.AutoSize = true;
+            this.m_lblCountry.Location = new System.Drawing.Point(6, 153);
+            this.m_lblCountry.Name = "m_lblCountry";
+            this.m_lblCountry.Size = new System.Drawing.Size(49, 13);
+            this.m_lblCountry.TabIndex = 10;
+            this.m_lblCountry.Text = "Countr&y :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(111, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(111, 82);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(111, 118);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(200, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(111, 150);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(200, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(111, 13);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(200, 20);
+            this.textBox5.TabIndex = 3;
+            // 
+            // m_lblSelectRadioButton
+            // 
+            this.m_lblSelectRadioButton.AutoSize = true;
+            this.m_lblSelectRadioButton.Location = new System.Drawing.Point(6, 16);
+            this.m_lblSelectRadioButton.Name = "m_lblSelectRadioButton";
+            this.m_lblSelectRadioButton.Size = new System.Drawing.Size(170, 13);
+            this.m_lblSelectRadioButton.TabIndex = 12;
+            this.m_lblSelectRadioButton.Text = "&Please select any of the following :";
+            // 
+            // m_rdbDaisyProduction
+            // 
+            this.m_rdbDaisyProduction.AutoSize = true;
+            this.m_rdbDaisyProduction.Location = new System.Drawing.Point(9, 35);
+            this.m_rdbDaisyProduction.Name = "m_rdbDaisyProduction";
+            this.m_rdbDaisyProduction.Size = new System.Drawing.Size(221, 17);
+            this.m_rdbDaisyProduction.TabIndex = 13;
+            this.m_rdbDaisyProduction.TabStop = true;
+            this.m_rdbDaisyProduction.Text = "I am using Obi for &Daisy Book Production.";
+            this.m_rdbDaisyProduction.UseVisualStyleBackColor = true;
+            // 
+            // m_rdbTryingObi
+            // 
+            this.m_rdbTryingObi.AutoSize = true;
+            this.m_rdbTryingObi.Location = new System.Drawing.Point(9, 57);
+            this.m_rdbTryingObi.Name = "m_rdbTryingObi";
+            this.m_rdbTryingObi.Size = new System.Drawing.Size(255, 17);
+            this.m_rdbTryingObi.TabIndex = 14;
+            this.m_rdbTryingObi.TabStop = true;
+            this.m_rdbTryingObi.Text = "I am just trying, &testing and play around  with Obi.";
+            this.m_rdbTryingObi.UseVisualStyleBackColor = true;
             // 
             // UserRegistration
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(341, 373);
+            this.Controls.Add(this.m_txtBoxObiInformation);
+            this.Controls.Add(this.m_grpBoxObiUse);
+            this.Controls.Add(this.m_grpBoxUserInformation);
+            this.Controls.Add(this.m_btnRemindMeLater);
             this.Controls.Add(this.m_btnOk);
             this.Name = "UserRegistration";
-            this.Text = "UserRegistration";
+            this.Text = "User Registration";
+            this.m_grpBoxUserInformation.ResumeLayout(false);
+            this.m_grpBoxUserInformation.PerformLayout();
+            this.m_grpBoxObiUse.ResumeLayout(false);
+            this.m_grpBoxObiUse.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button m_btnOk;
+        private System.Windows.Forms.Button m_btnRemindMeLater;
+        private System.Windows.Forms.GroupBox m_grpBoxUserInformation;
+        private System.Windows.Forms.GroupBox m_grpBoxObiUse;
+        private System.Windows.Forms.TextBox m_txtBoxObiInformation;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label m_lblCountry;
+        private System.Windows.Forms.Label m_lblCity;
+        private System.Windows.Forms.Label m_lblOrgName;
+        private System.Windows.Forms.Label m_lblEmail;
+        private System.Windows.Forms.Label m_lblName;
+        private System.Windows.Forms.Label m_lblSelectRadioButton;
+        private System.Windows.Forms.RadioButton m_rdbTryingObi;
+        private System.Windows.Forms.RadioButton m_rdbDaisyProduction;
     }
 }
