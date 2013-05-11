cc29f4993558863cb4a57173902cc1a8fbac2b4f
avneesh-singh <avneesh-singh@f04d5631-4067-4348-862e-39f3aacb62c1>
2013-05-11 15:58:13 +0000

    updated version number

--- trunk/Obi/Obi/Properties/AssemblyInfo.cs
+++ trunk/Obi/Obi/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.6.0.*")]
+[assembly: AssemblyVersion("2.6.1.*")]
 
 // There is a warning about this but it is harmless (1607)
-[assembly: AssemblyInformationalVersion("2.6.0")]
+[assembly: AssemblyInformationalVersion("2.6.1")]
