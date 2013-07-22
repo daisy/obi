9bfda00423ab525f4a01c661e461b05e408723c0
avneesh-singh <avneesh-singh@f04d5631-4067-4348-862e-39f3aacb62c1>
2013-07-22 04:08:19 +0000

    minor

--- branches/Obi-Post-2.5-GUIFeatures/Obi/ProjectView/Toolbar_EditAudoio.Designer.cs
+++ branches/Obi-Post-2.5-GUIFeatures/Obi/ProjectView/Toolbar_EditAudoio.Designer.cs
@@ -31,17 +31,18 @@ namespace Obi.ProjectView
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Toolbar_EditAudoio));
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.mbtnCuttoolStrip = new System.Windows.Forms.ToolStripButton();
-            this.mbtnCopytoolStrip = new System.Windows.Forms.ToolStripButton();
-            this.mbtnPastetoolStrip = new System.Windows.Forms.ToolStripButton();
-            this.mbtnSplittoolStrip = new System.Windows.Forms.ToolStripButton();
             this.mbtnDeletetoolStrip = new System.Windows.Forms.ToolStripButton();
-            this.mbtnMergetoolStrip = new System.Windows.Forms.ToolStripButton();
+            this.mbtnSplittoolStrip = new System.Windows.Forms.ToolStripButton();
+            this.mbtnPastetoolStrip = new System.Windows.Forms.ToolStripButton();
+            this.mbtnCopytoolStrip = new System.Windows.Forms.ToolStripButton();
             this.mbtnPraseDetectiontoolStrip = new System.Windows.Forms.ToolStripButton();
+            this.mbtnMergetoolStrip = new System.Windows.Forms.ToolStripButton();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // toolStrip1
             // 
+            this.toolStrip1.AccessibleName = "Edit audio";
             this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.mbtnCuttoolStrip,
@@ -57,6 +58,7 @@ namespace Obi.ProjectView
             this.toolStrip1.Name = "toolStrip1";
             this.toolStrip1.Size = new System.Drawing.Size(504, 25);
             this.toolStrip1.TabIndex = 7;
+            this.toolStrip1.TabStop = true;
             this.toolStrip1.Text = "toolStrip1";
             // 
             // mbtnCuttoolStrip
@@ -65,39 +67,9 @@ namespace Obi.ProjectView
             this.mbtnCuttoolStrip.Image = ((System.Drawing.Image)(resources.GetObject("mbtnCuttoolStrip.Image")));
             this.mbtnCuttoolStrip.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.mbtnCuttoolStrip.Name = "mbtnCuttoolStrip";
-            this.mbtnCuttoolStrip.Size = new System.Drawing.Size(30, 22);
+            this.mbtnCuttoolStrip.Size = new System.Drawing.Size(27, 22);
             this.mbtnCuttoolStrip.Text = "Cut";
             this.mbtnCuttoolStrip.Click += new System.EventHandler(this.mbtnCuttoolStrip_Click);
-            // 
-            // mbtnCopytoolStrip
-            // 
-            this.mbtnCopytoolStrip.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.mbtnCopytoolStrip.Image = ((System.Drawing.Image)(resources.GetObject("mbtnCopytoolStrip.Image")));
-            this.mbtnCopytoolStrip.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.mbtnCopytoolStrip.Name = "mbtnCopytoolStrip";
-            this.mbtnCopytoolStrip.Size = new System.Drawing.Size(39, 22);
-            this.mbtnCopytoolStrip.Text = "Copy";
-            this.mbtnCopytoolStrip.Click += new System.EventHandler(this.mbtnCopytoolStrip_Click);
-            // 
-            // mbtnPastetoolStrip
-            // 
-            this.mbtnPastetoolStrip.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.mbtnPastetoolStrip.Image = ((System.Drawing.Image)(resources.GetObject("mbtnPastetoolStrip.Image")));
-            this.mbtnPastetoolStrip.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.mbtnPastetoolStrip.Name = "mbtnPastetoolStrip";
-            this.mbtnPastetoolStrip.Size = new System.Drawing.Size(39, 22);
-            this.mbtnPastetoolStrip.Text = "Paste";
-            this.mbtnPastetoolStrip.Click += new System.EventHandler(this.mbtnPastetoolStrip_Click);
-            // 
-            // mbtnSplittoolStrip
-            // 
-            this.mbtnSplittoolStrip.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.mbtnSplittoolStrip.Image = ((System.Drawing.Image)(resources.GetObject("mbtnSplittoolStrip.Image")));
-            this.mbtnSplittoolStrip.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.mbtnSplittoolStrip.Name = "mbtnSplittoolStrip";
-            this.mbtnSplittoolStrip.Size = new System.Drawing.Size(34, 22);
-            this.mbtnSplittoolStrip.Text = "Split";
-            this.mbtnSplittoolStrip.Click += new System.EventHandler(this.mbtnSplittoolStrip_Click);
             // 
             // mbtnDeletetoolStrip
             // 
@@ -105,19 +77,39 @@ namespace Obi.ProjectView
             this.mbtnDeletetoolStrip.Image = ((System.Drawing.Image)(resources.GetObject("mbtnDeletetoolStrip.Image")));
             this.mbtnDeletetoolStrip.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.mbtnDeletetoolStrip.Name = "mbtnDeletetoolStrip";
-            this.mbtnDeletetoolStrip.Size = new System.Drawing.Size(44, 19);
+            this.mbtnDeletetoolStrip.Size = new System.Drawing.Size(42, 22);
             this.mbtnDeletetoolStrip.Text = "Delete";
             this.mbtnDeletetoolStrip.Click += new System.EventHandler(this.mbtnDeletetoolStrip_Click);
             // 
-            // mbtnMergetoolStrip
+            // mbtnSplittoolStrip
             // 
-            this.mbtnMergetoolStrip.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.mbtnMergetoolStrip.Image = ((System.Drawing.Image)(resources.GetObject("mbtnMergetoolStrip.Image")));
-            this.mbtnMergetoolStrip.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.mbtnMergetoolStrip.Name = "mbtnMergetoolStrip";
-            this.mbtnMergetoolStrip.Size = new System.Drawing.Size(138, 22);
-            this.mbtnMergetoolStrip.Text = "Merge With Next Phrase";
-            this.mbtnMergetoolStrip.Click += new System.EventHandler(this.mbtnMergetoolStrip_Click);
+            this.mbtnSplittoolStrip.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.mbtnSplittoolStrip.Image = ((System.Drawing.Image)(resources.GetObject("mbtnSplittoolStrip.Image")));
+            this.mbtnSplittoolStrip.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.mbtnSplittoolStrip.Name = "mbtnSplittoolStrip";
+            this.mbtnSplittoolStrip.Size = new System.Drawing.Size(31, 22);
+            this.mbtnSplittoolStrip.Text = "Split";
+            this.mbtnSplittoolStrip.Click += new System.EventHandler(this.mbtnSplittoolStrip_Click);
+            // 
+            // mbtnPastetoolStrip
+            // 
+            this.mbtnPastetoolStrip.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.mbtnPastetoolStrip.Image = ((System.Drawing.Image)(resources.GetObject("mbtnPastetoolStrip.Image")));
+            this.mbtnPastetoolStrip.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.mbtnPastetoolStrip.Name = "mbtnPastetoolStrip";
+            this.mbtnPastetoolStrip.Size = new System.Drawing.Size(38, 22);
+            this.mbtnPastetoolStrip.Text = "Paste";
+            this.mbtnPastetoolStrip.Click += new System.EventHandler(this.mbtnPastetoolStrip_Click);
+            // 
+            // mbtnCopytoolStrip
+            // 
+            this.mbtnCopytoolStrip.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.mbtnCopytoolStrip.Image = ((System.Drawing.Image)(resources.GetObject("mbtnCopytoolStrip.Image")));
+            this.mbtnCopytoolStrip.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.mbtnCopytoolStrip.Name = "mbtnCopytoolStrip";
+            this.mbtnCopytoolStrip.Size = new System.Drawing.Size(35, 22);
+            this.mbtnCopytoolStrip.Text = "Copy";
+            this.mbtnCopytoolStrip.Click += new System.EventHandler(this.mbtnCopytoolStrip_Click);
             // 
             // mbtnPraseDetectiontoolStrip
             // 
@@ -125,16 +117,26 @@ namespace Obi.ProjectView
             this.mbtnPraseDetectiontoolStrip.Image = ((System.Drawing.Image)(resources.GetObject("mbtnPraseDetectiontoolStrip.Image")));
             this.mbtnPraseDetectiontoolStrip.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.mbtnPraseDetectiontoolStrip.Name = "mbtnPraseDetectiontoolStrip";
-            this.mbtnPraseDetectiontoolStrip.Size = new System.Drawing.Size(100, 22);
+            this.mbtnPraseDetectiontoolStrip.Size = new System.Drawing.Size(93, 22);
             this.mbtnPraseDetectiontoolStrip.Text = "Phrase Detection";
             this.mbtnPraseDetectiontoolStrip.Click += new System.EventHandler(this.mbtnPraseDetectiontoolStrip_Click);
             // 
-            // Edit
+            // mbtnMergetoolStrip
+            // 
+            this.mbtnMergetoolStrip.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.mbtnMergetoolStrip.Image = ((System.Drawing.Image)(resources.GetObject("mbtnMergetoolStrip.Image")));
+            this.mbtnMergetoolStrip.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.mbtnMergetoolStrip.Name = "mbtnMergetoolStrip";
+            this.mbtnMergetoolStrip.Size = new System.Drawing.Size(127, 22);
+            this.mbtnMergetoolStrip.Text = "Merge With Next Phrase";
+            this.mbtnMergetoolStrip.Click += new System.EventHandler(this.mbtnMergetoolStrip_Click);
+            // 
+            // Toolbar_EditAudoio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.toolStrip1);
-            this.Name = "Edit";
+            this.Name = "Toolbar_EditAudoio";
             this.Size = new System.Drawing.Size(510, 28);
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
