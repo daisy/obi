84f79667d35e8d1c597430fdbefad5dbc7b16740
rohit_tandon <rohit_tandon@f04d5631-4067-4348-862e-39f3aacb62c1>
2013-11-28 14:14:43 +0000

    minor.

--- Obi/Obi/messages.Designer.cs
+++ Obi/Obi/messages.Designer.cs
@@ -4669,7 +4669,7 @@ namespace Obi {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Background color for empty sections in top view.
+        ///   Looks up a localized string similar to Background color for empty sections in TOC view.
         /// </summary>
         internal static string Project_BackgroundColorForEmptySection {
             get {
