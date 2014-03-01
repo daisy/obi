ac1821a97ee90ffad488a954eeeb64f619ca8818
rachana-singh <rachana-singh@f04d5631-4067-4348-862e-39f3aacb62c1>
2014-03-01 09:52:55 +0000

    Added image to the reset button.

--- trunk/Obi/Obi/UserControls/TextVUMeterPanel.Designer.cs
+++ trunk/Obi/Obi/UserControls/TextVUMeterPanel.Designer.cs
@@ -28,67 +28,67 @@ namespace Obi.UserControls
         /// </summary>
         private void InitializeComponent()
         {
-        this.components = new System.ComponentModel.Container ();
-        System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager ( typeof ( TextVUMeterPanel ) );
-        this.lblLeft = new System.Windows.Forms.Label ();
-        this.mLeftBox = new System.Windows.Forms.TextBox ();
-        this.lblRight = new System.Windows.Forms.Label ();
-        this.mRightBox = new System.Windows.Forms.TextBox ();
-        this.mResetButton = new System.Windows.Forms.Button ();
-        this.tmUpdateText = new System.Windows.Forms.Timer ( this.components );
-        this.SuspendLayout ();
-        // 
-        // lblLeft
-        // 
-        resources.ApplyResources ( this.lblLeft, "lblLeft" );
-        this.lblLeft.Name = "lblLeft";
-        // 
-        // mLeftBox
-        // 
-        resources.ApplyResources ( this.mLeftBox, "mLeftBox" );
-        this.mLeftBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-        this.mLeftBox.Name = "mLeftBox";
-        this.mLeftBox.ReadOnly = true;
-        // 
-        // lblRight
-        // 
-        resources.ApplyResources ( this.lblRight, "lblRight" );
-        this.lblRight.Name = "lblRight";
-        // 
-        // mRightBox
-        // 
-        resources.ApplyResources ( this.mRightBox, "mRightBox" );
-        this.mRightBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-        this.mRightBox.Name = "mRightBox";
-        this.mRightBox.ReadOnly = true;
-        // 
-        // mResetButton
-        // 
-        resources.ApplyResources ( this.mResetButton, "mResetButton" );
-        this.mResetButton.BackColor = System.Drawing.SystemColors.Control;
-        this.mResetButton.Name = "mResetButton";
-        this.mResetButton.UseVisualStyleBackColor = false;
-        this.mResetButton.Click += new System.EventHandler ( this.mResetButton_Click );
-        // 
-        // tmUpdateText
-        // 
-        this.tmUpdateText.Enabled = true;
-        this.tmUpdateText.Interval = 400;
-        this.tmUpdateText.Tick += new System.EventHandler ( this.tmUpdateText_Tick );
-        // 
-        // TextVUMeterPanel
-        // 
-        resources.ApplyResources ( this, "$this" );
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.BackColor = System.Drawing.Color.MistyRose;
-        this.Controls.Add ( this.mResetButton );
-        this.Controls.Add ( this.mRightBox );
-        this.Controls.Add ( this.lblRight );
-        this.Controls.Add ( this.mLeftBox );
-        this.Controls.Add ( this.lblLeft );
-        this.Name = "TextVUMeterPanel";
-        this.ResumeLayout ( false );
-        this.PerformLayout ();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TextVUMeterPanel));
+            this.lblLeft = new System.Windows.Forms.Label();
+            this.mLeftBox = new System.Windows.Forms.TextBox();
+            this.lblRight = new System.Windows.Forms.Label();
+            this.mRightBox = new System.Windows.Forms.TextBox();
+            this.mResetButton = new System.Windows.Forms.Button();
+            this.tmUpdateText = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblLeft
+            // 
+            resources.ApplyResources(this.lblLeft, "lblLeft");
+            this.lblLeft.Name = "lblLeft";
+            // 
+            // mLeftBox
+            // 
+            resources.ApplyResources(this.mLeftBox, "mLeftBox");
+            this.mLeftBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mLeftBox.Name = "mLeftBox";
+            this.mLeftBox.ReadOnly = true;
+            // 
+            // lblRight
+            // 
+            resources.ApplyResources(this.lblRight, "lblRight");
+            this.lblRight.Name = "lblRight";
+            // 
+            // mRightBox
+            // 
+            resources.ApplyResources(this.mRightBox, "mRightBox");
+            this.mRightBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mRightBox.Name = "mRightBox";
+            this.mRightBox.ReadOnly = true;
+            // 
+            // mResetButton
+            // 
+            resources.ApplyResources(this.mResetButton, "mResetButton");
+            this.mResetButton.BackColor = System.Drawing.SystemColors.Control;
+            this.mResetButton.Name = "mResetButton";
+            this.mResetButton.UseVisualStyleBackColor = false;
+            this.mResetButton.Click += new System.EventHandler(this.mResetButton_Click);
+            // 
+            // tmUpdateText
+            // 
+            this.tmUpdateText.Enabled = true;
+            this.tmUpdateText.Interval = 400;
+            this.tmUpdateText.Tick += new System.EventHandler(this.tmUpdateText_Tick);
+            // 
+            // TextVUMeterPanel
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.Controls.Add(this.mResetButton);
+            this.Controls.Add(this.mRightBox);
+            this.Controls.Add(this.lblRight);
+            this.Controls.Add(this.mLeftBox);
+            this.Controls.Add(this.lblLeft);
+            this.Name = "TextVUMeterPanel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
