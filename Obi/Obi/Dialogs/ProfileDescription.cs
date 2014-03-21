dda8dab48d8ab81fdb668e55a951d54f549dad5e
rohit_tandon <rohit_tandon@f04d5631-4067-4348-862e-39f3aacb62c1>
2014-03-21 15:12:23 +0000

    Commit includes:- 1)Profile 1 implementation. 2)Change in behavior for elapse back when in paused state.

--- Obi/Obi/Dialogs/ProfileDescription.cs
+++ Obi/Obi/Dialogs/ProfileDescription.cs
@@ -36,6 +36,10 @@ namespace Obi.Dialogs
             {
                 m_ProfileDescription_WebBrowser.DocumentText = m_ProfileDescription_WebBrowser.Document.GetElementById("Advance").InnerHtml;
             }
+            else if (m_ProfileSelectedIndex == 3)
+            {
+                m_ProfileDescription_WebBrowser.DocumentText = m_ProfileDescription_WebBrowser.Document.GetElementById("Profile1").InnerHtml;
+            }
             else if (m_ProfileSelectedIndex == 4)
             {
                 m_ProfileDescription_WebBrowser.DocumentText = m_ProfileDescription_WebBrowser.Document.GetElementById("Profile2").InnerHtml;
