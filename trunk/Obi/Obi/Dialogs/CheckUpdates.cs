3cddc3460eea481761ea5c855fbfb0dc3ecca4b0
avneesh-singh <avneesh-singh@f04d5631-4067-4348-862e-39f3aacb62c1>
2014-04-12 06:01:49 +0000

    version update detection enabled

--- trunk/Obi/Obi/Dialogs/CheckUpdates.cs
+++ trunk/Obi/Obi/Dialogs/CheckUpdates.cs
@@ -64,8 +64,8 @@ namespace Obi.Dialogs
                 
                 try
                 {
-                    //string releaseInfoContents = GetReleaseInfoFileContents();
-                    string releaseInfoContents = GetReleaseInfoFileContentsFromLocalFileForTesting();
+                    string releaseInfoContents = GetReleaseInfoFileContents();
+                    //string releaseInfoContents = GetReleaseInfoFileContentsFromLocalFileForTesting();
                     if (string.IsNullOrEmpty(releaseInfoContents))
                     {
                         m_IsNewVersionAvailable = false;
