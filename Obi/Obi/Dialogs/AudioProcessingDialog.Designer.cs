84a8e75c0d94f634d47f8d0e8a962912ca47f466
rachana-singh <rachana-singh@f04d5631-4067-4348-862e-39f3aacb62c1>
2014-07-24 14:41:26 +0000

    Audio processing form committed.

---
+++ Obi/Obi/Dialogs/AudioProcessingDialog.Designer.cs
@@ -0,0 +1,142 @@
+namespace Obi.Dialogs
+{
+    partial class AudioProcessingDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AudioProcessingDialog));
+            this.m_gpbox_Process = new System.Windows.Forms.GroupBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.m_lbl_Parameters = new System.Windows.Forms.Label();
+            this.m_cb_Process = new System.Windows.Forms.ComboBox();
+            this.m_lbl_Process = new System.Windows.Forms.Label();
+            this.m_btn_OK = new System.Windows.Forms.Button();
+            this.m_btn_Cancel = new System.Windows.Forms.Button();
+            this.m_gpbox_Process.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // m_gpbox_Process
+            // 
+            this.m_gpbox_Process.Controls.Add(this.numericUpDown1);
+            this.m_gpbox_Process.Controls.Add(this.m_lbl_Parameters);
+            this.m_gpbox_Process.Controls.Add(this.m_cb_Process);
+            this.m_gpbox_Process.Controls.Add(this.m_lbl_Process);
+            resources.ApplyResources(this.m_gpbox_Process, "m_gpbox_Process");
+            this.m_gpbox_Process.Name = "m_gpbox_Process";
+            this.m_gpbox_Process.TabStop = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 1;
+            resources.ApplyResources(this.numericUpDown1, "numericUpDown1");
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            40,
+            0,
+            0,
+            65536});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            // 
+            // m_lbl_Parameters
+            // 
+            resources.ApplyResources(this.m_lbl_Parameters, "m_lbl_Parameters");
+            this.m_lbl_Parameters.Name = "m_lbl_Parameters";
+            // 
+            // m_cb_Process
+            // 
+            this.m_cb_Process.FormattingEnabled = true;
+            this.m_cb_Process.Items.AddRange(new object[] {
+            resources.GetString("m_cb_Process.Items"),
+            resources.GetString("m_cb_Process.Items1"),
+            resources.GetString("m_cb_Process.Items2")});
+            resources.ApplyResources(this.m_cb_Process, "m_cb_Process");
+            this.m_cb_Process.Name = "m_cb_Process";
+            // 
+            // m_lbl_Process
+            // 
+            resources.ApplyResources(this.m_lbl_Process, "m_lbl_Process");
+            this.m_lbl_Process.Name = "m_lbl_Process";
+            // 
+            // m_btn_OK
+            // 
+            resources.ApplyResources(this.m_btn_OK, "m_btn_OK");
+            this.m_btn_OK.Name = "m_btn_OK";
+            this.m_btn_OK.UseVisualStyleBackColor = true;
+            this.m_btn_OK.Click += new System.EventHandler(this.m_btn_OK_Click);
+            // 
+            // m_btn_Cancel
+            // 
+            this.m_btn_Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.m_btn_Cancel, "m_btn_Cancel");
+            this.m_btn_Cancel.Name = "m_btn_Cancel";
+            this.m_btn_Cancel.UseVisualStyleBackColor = true;
+            this.m_btn_Cancel.Click += new System.EventHandler(this.m_btn_Cancel_Click);
+            // 
+            // AudioProcessingDialog
+            // 
+            this.AcceptButton = this.m_btn_OK;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.m_btn_Cancel;
+            this.Controls.Add(this.m_btn_Cancel);
+            this.Controls.Add(this.m_btn_OK);
+            this.Controls.Add(this.m_gpbox_Process);
+            this.MaximizeBox = false;
+            this.Name = "AudioProcessingDialog";
+            this.m_gpbox_Process.ResumeLayout(false);
+            this.m_gpbox_Process.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox m_gpbox_Process;
+        private System.Windows.Forms.ComboBox m_cb_Process;
+        private System.Windows.Forms.Label m_lbl_Process;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label m_lbl_Parameters;
+        private System.Windows.Forms.Button m_btn_OK;
+        private System.Windows.Forms.Button m_btn_Cancel;
+    }
+}
