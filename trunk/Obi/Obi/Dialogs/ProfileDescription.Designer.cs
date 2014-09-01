b1c09ea7955039e54d0490214775ed8674e82f36
rohit_tandon <rohit_tandon@f04d5631-4067-4348-862e-39f3aacb62c1>
2014-09-01 14:28:24 +0000

    Localizable is set to true.

--- trunk/Obi/Obi/Dialogs/ProfileDescription.Designer.cs
+++ trunk/Obi/Obi/Dialogs/ProfileDescription.Designer.cs
@@ -28,45 +28,35 @@ namespace Obi.Dialogs
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProfileDescription));
             this.m_ProfileDescription_WebBrowser = new System.Windows.Forms.WebBrowser();
             this.m_btnClose = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // m_ProfileDescription_WebBrowser
             // 
-            this.m_ProfileDescription_WebBrowser.Location = new System.Drawing.Point(0, 0);
+            resources.ApplyResources(this.m_ProfileDescription_WebBrowser, "m_ProfileDescription_WebBrowser");
             this.m_ProfileDescription_WebBrowser.MinimumSize = new System.Drawing.Size(20, 20);
             this.m_ProfileDescription_WebBrowser.Name = "m_ProfileDescription_WebBrowser";
-            this.m_ProfileDescription_WebBrowser.Size = new System.Drawing.Size(606, 517);
-            this.m_ProfileDescription_WebBrowser.TabIndex = 0;
             this.m_ProfileDescription_WebBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.m_ProfileDescription_WebBrowser_DocumentCompleted);
             // 
             // m_btnClose
             // 
-            this.m_btnClose.AccessibleName = "Close";
+            resources.ApplyResources(this.m_btnClose, "m_btnClose");
             this.m_btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.m_btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.m_btnClose.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.m_btnClose.Location = new System.Drawing.Point(510, 490);
             this.m_btnClose.Name = "m_btnClose";
-            this.m_btnClose.Size = new System.Drawing.Size(75, 23);
-            this.m_btnClose.TabIndex = 3;
-            this.m_btnClose.Text = "&Close";
             this.m_btnClose.UseVisualStyleBackColor = true;
             // 
             // ProfileDescription
             // 
-            this.AccessibleName = "Audio Profile Description";
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.m_btnClose;
-            this.ClientSize = new System.Drawing.Size(606, 517);
             this.Controls.Add(this.m_btnClose);
             this.Controls.Add(this.m_ProfileDescription_WebBrowser);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "ProfileDescription";
-            this.Text = "Audio Profile Description";
             this.ResumeLayout(false);
 
         }
