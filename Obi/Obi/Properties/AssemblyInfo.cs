634ae46e7ec3aea690a8648e81372547407c7d37
Avneesh Singh <13777742+avneeshsingh@users.noreply.github.com>
2014-12-16 05:27:51 +0000

    Updated assembly versions for 3.6 release

--- Obi/Obi/Properties/AssemblyInfo.cs
+++ Obi/Obi/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.5.0.*")]
+[assembly: AssemblyVersion("3.6.0.*")]
 
 // There is a warning about this but it is harmless (1607)
-[assembly: AssemblyInformationalVersion("3.5")]
+[assembly: AssemblyInformationalVersion("3.6")]
