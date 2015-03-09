715dadc580e7d90f2d40870d486011ef7a516972
avneesh-singh <avneesh-singh@f04d5631-4067-4348-862e-39f3aacb62c1>
2015-03-09 08:44:52 +0000

    updated assembly versions for 3.7 beta release

--- trunk/Obi/Obi/Properties/AssemblyInfo.cs
+++ trunk/Obi/Obi/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.6.0.*")]
+[assembly: AssemblyVersion("3.6.7.*")]
 
 // There is a warning about this but it is harmless (1607)
-[assembly: AssemblyInformationalVersion("3.6")]
+[assembly: AssemblyInformationalVersion("3.7 beta")]
