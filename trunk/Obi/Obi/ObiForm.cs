2792ebaafc97f849442834c07f0ea61ef4ce19fa
avneesh-singh <avneesh-singh@f04d5631-4067-4348-862e-39f3aacb62c1>
2015-11-30 05:00:05 +0000

    renamed the checkbox for ensuring cursor visibility, when preview before recording is undone.

--- trunk/Obi/Obi/ObiForm.cs
+++ trunk/Obi/Obi/ObiForm.cs
@@ -5657,6 +5657,7 @@ ref string exportDirectoryEPUB3)
                     epubCheckProcess.StartInfo.FileName = "java.exe";
                     //
                     string strMode =Path.GetExtension(epub3Export).ToLower() == ".epub"? "" : "\" -mode exp -v 3.0";
+                    strMode += " -profile default";
                     string strArguments = "-jar \"" +
                         epubCheckFullPath + "\" \"" +
                         epub3Export + strMode;
