d92624a47d617afb6e8b7ab72a4ebbd61e171f67
Avneesh Singh <13777742+avneeshsingh@users.noreply.github.com>
2016-10-28 04:39:11 +0000

    Updated assembly versions to 3.9

--- Obi/Obi/Properties/AssemblyInfo.cs
+++ Obi/Obi/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.8.8.*")]
+[assembly: AssemblyVersion("3.9.0.*")]
 
 // There is a warning about this but it is harmless (1607)
-[assembly: AssemblyInformationalVersion("3.9 beta 2")]
+[assembly: AssemblyInformationalVersion("3.9")]
