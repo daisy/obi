d93c607afe2d1f82c38265a98c5b57be3816de70
avneesh-singh <avneesh-singh@f04d5631-4067-4348-862e-39f3aacb62c1>
2017-08-29 06:32:51 +0000

    Updated assembly versions for 4.0 release

--- trunk/Obi/Obi/Properties/AssemblyInfo.cs
+++ trunk/Obi/Obi/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.9.7.*")]
+[assembly: AssemblyVersion("4.0.0.*")]
 
 // There is a warning about this but it is harmless (1607)
-[assembly: AssemblyInformationalVersion("4.0 beta")]
+[assembly: AssemblyInformationalVersion("4.0")]
