70eedd29dc9afe395ff5eb9ccdeb15d46a172c2e
rohit_tandon <rohit_tandon@f04d5631-4067-4348-862e-39f3aacb62c1>
2017-11-23 13:29:43 +0000

    Time Elapsed dialog can be localized now.

--- trunk/Obi/Obi/Dialogs/TimeElapsed.Designer.cs
+++ trunk/Obi/Obi/Dialogs/TimeElapsed.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TimeElapsed));
             this.m_lblTimeElapsed = new System.Windows.Forms.Label();
             this.m_txtBoxTotalTimeElapsed = new System.Windows.Forms.TextBox();
             this.m_btnClose = new System.Windows.Forms.Button();
@@ -35,42 +36,30 @@
             // 
             // m_lblTimeElapsed
             // 
-            this.m_lblTimeElapsed.AutoSize = true;
-            this.m_lblTimeElapsed.Location = new System.Drawing.Point(12, 53);
+            resources.ApplyResources(this.m_lblTimeElapsed, "m_lblTimeElapsed");
             this.m_lblTimeElapsed.Name = "m_lblTimeElapsed";
-            this.m_lblTimeElapsed.Size = new System.Drawing.Size(225, 13);
-            this.m_lblTimeElapsed.TabIndex = 0;
-            this.m_lblTimeElapsed.Text = "Time Elapsed between Begin and End marks :";
             // 
             // m_txtBoxTotalTimeElapsed
             // 
-            this.m_txtBoxTotalTimeElapsed.Location = new System.Drawing.Point(243, 50);
+            resources.ApplyResources(this.m_txtBoxTotalTimeElapsed, "m_txtBoxTotalTimeElapsed");
             this.m_txtBoxTotalTimeElapsed.Name = "m_txtBoxTotalTimeElapsed";
             this.m_txtBoxTotalTimeElapsed.ReadOnly = true;
-            this.m_txtBoxTotalTimeElapsed.Size = new System.Drawing.Size(129, 20);
-            this.m_txtBoxTotalTimeElapsed.TabIndex = 1;
             // 
             // m_btnClose
             // 
-            this.m_btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.m_btnClose.Location = new System.Drawing.Point(150, 108);
+            resources.ApplyResources(this.m_btnClose, "m_btnClose");
             this.m_btnClose.Name = "m_btnClose";
-            this.m_btnClose.Size = new System.Drawing.Size(75, 23);
-            this.m_btnClose.TabIndex = 2;
-            this.m_btnClose.Text = "&Close";
             this.m_btnClose.UseVisualStyleBackColor = true;
             this.m_btnClose.Click += new System.EventHandler(this.m_btnClose_Click);
             // 
             // TimeElapsed
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(376, 155);
             this.Controls.Add(this.m_btnClose);
             this.Controls.Add(this.m_txtBoxTotalTimeElapsed);
             this.Controls.Add(this.m_lblTimeElapsed);
             this.Name = "TimeElapsed";
-            this.Text = "Time Elapsed";
             this.ResumeLayout(false);
             this.PerformLayout();
 
