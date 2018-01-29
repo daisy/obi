0fc2220b78cda27c7f6f3c3e01943f74aa220f25
rohit_tandon <rohit_tandon@f04d5631-4067-4348-862e-39f3aacb62c1>
2018-01-29 12:58:29 +0000

    -> XML import in Import TOC implemented. -> Important updates in Import TOC for csv.

--- trunk/Obi/Obi/ImportExport/ImportTOC.cs
+++ trunk/Obi/Obi/ImportExport/ImportTOC.cs
@@ -1,25 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+using urakawa.daisy;
 
 namespace Obi.ImportExport
 {
     /// <summary>
-    /// imports the CSV file
+    /// imports the CSV,xml files
     /// </summary>
     public class ImportTOC
     {
-        private ObiPresentation m_Presentation;
-        private List<int> m_LevelsList;
-        private List<string> m_SectionsNames;
+        private List<int> m_LevelsList  = new List<int>();
+        private List<string> m_SectionsNames = new List<string>();
 
         public ImportTOC()
         {
         }
 
-        public void ImportFromCSVFile(string CSVFullPath, ObiPresentation presentation)
+        public void ImportFromCSVFile(string CSVFullPath)
         {
-            m_Presentation = presentation;
             List<int> levelsList = new List<int>();
             List<string> sectionNames = new List<string>();
             ReadListsFromCSVFile(levelsList, sectionNames, CSVFullPath);
@@ -28,15 +29,42 @@ namespace Obi.ImportExport
 
         }
 
+        public void ImportFromXMLFile(string filePath)
+        {
+            try
+            {
+                XmlDocument dtbookFileDoc = urakawa.xuk.XmlReaderWriterHelper.ParseXmlDocument(filePath, false, false);
 
-        public List<int> LevelsListOfImportedCSV
+                if (dtbookFileDoc != null && dtbookFileDoc.DocumentType != null &&
+                    dtbookFileDoc.DocumentType.ParentNode != null)
+                {
+                    XmlNode xmlNode = dtbookFileDoc.DocumentType.ParentNode;
+                    parseContentDocument(filePath, xmlNode);
+                }
+
+            }
+            catch (IOException e)
+            {
+                MessageBox.Show(Localizer.Message("FileInUse"), Localizer.Message("Caption_Error"),
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            catch (Exception e)
+            {
+                return;
+            }
+
+        }
+
+
+        public List<int> LevelsListOfImportedTocList
         {
             get
             {
                 return m_LevelsList;
             }
         }
-        public List<string> SectionNamesOfImportedCSV
+        public List<string> SectionNamesOfImportedTocList
         {
             get
             {
@@ -46,7 +74,21 @@ namespace Obi.ImportExport
 
         private void ReadListsFromCSVFile(List<int> levelsList, List<string> sectionNamesList, string CSVFullPath)
         {
-            string[] linesInFiles = File.ReadAllLines(CSVFullPath);
+            string[] linesInFiles;
+            try
+            {
+
+                linesInFiles = File.ReadAllLines(CSVFullPath);
+            }
+            catch (IOException e)
+            {
+                MessageBox.Show(Localizer.Message("FileInUse"), Localizer.Message("Caption_Error"), MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            catch (Exception e)
+            {
+                return;
+            }
 
             string tempString = "";
 
@@ -97,8 +139,82 @@ namespace Obi.ImportExport
                 }
 
             }
-            Console.WriteLine("lists loaded");
         }
+        private void parseContentDocument(string filePath, XmlNode xmlNode)
+        {
+
+            XmlNodeType xmlType = xmlNode.NodeType;
+            switch (xmlType)
+            {
+                case XmlNodeType.Attribute:
+                    {
+                        System.Diagnostics.Debug.Fail("Calling this method with an XmlAttribute should never happen !!");
+                        break;
+                    }
+                case XmlNodeType.Document:
+                    {
+
+                        XmlNode bodyElement = XmlDocumentHelper.GetFirstChildElementOrSelfWithName(xmlNode, true, "body", null);
+
+                        if (bodyElement == null)
+                        {
+                            bodyElement = XmlDocumentHelper.GetFirstChildElementOrSelfWithName(xmlNode, true, "book", null);
+                        }
+
+                        if (bodyElement != null)
+                        {
+
+                            parseContentDocument(filePath, bodyElement);
+                        }
+                        break;
+                    }
+                case XmlNodeType.Element:
+                    {
+
+                        if (xmlNode.LocalName.StartsWith("level") || xmlNode.LocalName == "doctitle")
+                        {
+                            string tempLevel = xmlNode.LocalName.Replace("level", string.Empty);
+                            int n;
+                            bool isNumeric = int.TryParse(tempLevel, out n);
+
+                            if (isNumeric)
+                            {
+                                m_LevelsList.Add(n);
+                            }
+                        }
+
+                        if (xmlNode.LocalName == "h1" || xmlNode.LocalName == "h2" || xmlNode.LocalName == "h3"
+                        || xmlNode.LocalName == "h4" || xmlNode.LocalName == "h5" || xmlNode.LocalName == "h6" || xmlNode.LocalName == "HD")
+                        {
+                            string tempSectionName = xmlNode.InnerText.Replace("\n", "").Replace("\r", "").Replace("\t", "");
+                            m_SectionsNames.Add(tempSectionName);
+
+
+                        }
+
+
+                        foreach (XmlNode childXmlNode in xmlNode.ChildNodes)
+                        {
+                            parseContentDocument(filePath, childXmlNode);
+                        }
+                        break;
+                    }
+                case XmlNodeType.Whitespace:
+                case XmlNodeType.CDATA:
+                case XmlNodeType.SignificantWhitespace:
+                case XmlNodeType.Text:
+                    {
+
+                        break;
+
+                    }
+                default:
+                    {
+                        return;
+                    }
+            }
+        }
+
 
     }
 }
