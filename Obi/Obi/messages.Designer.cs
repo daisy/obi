0641622cf5026c357fa8dfbd626d8c9d5e4d882b
Rohit Tandon <rssbrohit@gmail.com>
2020-10-03 14:04:51 +0530

    Revert "Revert "-> Help menu Online recourses/Issue tracker URL updated. ""
    This reverts commit ea685edafe7065d4cba9f05353383b095dd82249.

--- Obi/Obi/messages.Designer.cs
+++ Obi/Obi/messages.Designer.cs
@@ -5110,7 +5110,7 @@ namespace Obi {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to http://daisy.trac.cvsdude.com/obi.
+        ///   Looks up a localized string similar to http://daisy.trac.cloudforge.com/obi.
         /// </summary>
         internal static string Obi_Wiki_Url {
             get {
