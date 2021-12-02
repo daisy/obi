34d0a61f65dedf1159dda7fbe9c2b729c565216a
Rohit Tandon <rssbrohit@gmail.com>
2021-12-02 22:15:46 +0530

    French Dialog localization update for Obi 4.7

--- Obi/Obi/ImportExport/ImportMetadata.cs
+++ Obi/Obi/ImportExport/ImportMetadata.cs
@@ -59,7 +59,36 @@ namespace Obi.ImportExport
                 if (cellsInLineArray.Length >= 2 && !string.IsNullOrWhiteSpace(cellsInLineArray[0]) && !string.IsNullOrWhiteSpace(cellsInLineArray[1]))
                 {
                     if (projectView == null)
-                        presentation.SetSingleMetadataItem(cellsInLineArray[0], cellsInLineArray[1]);
+                    {
+                        if (Metadata.DAISY3MetadataNames.Contains(cellsInLineArray[0], StringComparer.OrdinalIgnoreCase))
+                        {
+                            foreach (string metadataName in Metadata.DAISY3MetadataNames)
+                            {
+                                if (metadataName.IndexOf(cellsInLineArray[0], StringComparison.OrdinalIgnoreCase) >= 0)
+                                {
+                                    cellsInLineArray[0] = metadataName;
+                                }
+                            }
+                            cellsInLineArray[0] = Metadata.DAISY3MetadataNames.ElementAt(Metadata.DAISY3MetadataNames.IndexOf(cellsInLineArray[0]));
+                        }
+                        if (cellsInLineArray[0].ToLower() == "dtb:produceddate" || cellsInLineArray[0].ToLower() == "dtb:revisiondate" || cellsInLineArray[0].ToLower() == "dc:date" || cellsInLineArray[0].ToLower() == "dtb:sourcedate")
+                        {
+                            string dateFormat = GetDate(cellsInLineArray[0], cellsInLineArray[1], entryHavingWrongDateFormat);
+                            cellsInLineArray[1] = dateFormat;
+                        }
+                        if (cellsInLineArray[0].ToLower() == "custom")
+                        {
+                            if (!entryHavingWrongDateFormat.Contains(cellsInLineArray[0], StringComparer.OrdinalIgnoreCase))
+                            {
+                                m_metadataFieldsNotImported += "\n\n" + "-> " + cellsInLineArray[0] + " " + Localizer.Message("Metadata_NameCannotBeCustom");
+                                entryHavingWrongDateFormat.Add(cellsInLineArray[0]);
+                            }
+                        }
+                        else
+                        {
+                            presentation.SetSingleMetadataItem(cellsInLineArray[0], cellsInLineArray[1]);
+                        }
+                    }
                     else if (projectView != null)
                     {
                         if (projectView.CanAddMetadataEntry())
@@ -88,7 +117,7 @@ namespace Obi.ImportExport
                             }
                             if (!entryModified)
                             {
-                                if(Metadata.DAISY3MetadataNames.Contains(cellsInLineArray[0],StringComparer.OrdinalIgnoreCase))
+                                if (Metadata.DAISY3MetadataNames.Contains(cellsInLineArray[0], StringComparer.OrdinalIgnoreCase))
                                 {
                                     foreach (string metadataName in Metadata.DAISY3MetadataNames)
                                     {
@@ -97,7 +126,7 @@ namespace Obi.ImportExport
                                             cellsInLineArray[0] = metadataName;
                                         }
                                     }
-                                    cellsInLineArray[0]  = Metadata.DAISY3MetadataNames.ElementAt(Metadata.DAISY3MetadataNames.IndexOf(cellsInLineArray[0]));
+                                    cellsInLineArray[0] = Metadata.DAISY3MetadataNames.ElementAt(Metadata.DAISY3MetadataNames.IndexOf(cellsInLineArray[0]));
                                 }
                                 if (cellsInLineArray[0].ToLower() == "custom")
                                 {
@@ -113,7 +142,7 @@ namespace Obi.ImportExport
                                     command.ChildCommands.Insert(command.ChildCommands.Count, cmd);
                                     if (cellsInLineArray[0] == "dtb:producedDate" || cellsInLineArray[0] == "dtb:revisionDate" || cellsInLineArray[0] == "dc:Date" || cellsInLineArray[0] == "dtb:sourceDate")
                                     {
-                                        cellsInLineArray[1] = GetDate(cellsInLineArray[0], cellsInLineArray[1],entryHavingWrongDateFormat);
+                                        cellsInLineArray[1] = GetDate(cellsInLineArray[0], cellsInLineArray[1], entryHavingWrongDateFormat);
                                     }
                                     command.ChildCommands.Insert(command.ChildCommands.Count, new Commands.Metadata.ModifyContent(projectView, cmd.Entry, cellsInLineArray[1]));
                                 }
