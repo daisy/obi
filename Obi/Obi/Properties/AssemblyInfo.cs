ebe230258432520a7542ea6095d1b95b6c1df513
Avneesh Singh <avneesh.sg@gmail.com>
2021-12-09 22:20:08 +0530

    Updated assembly versions for Obi 4.7 release

--- Obi/Obi/Properties/AssemblyInfo.cs
+++ Obi/Obi/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.6.7.*")]
+[assembly: AssemblyVersion("4.7.0.*")]
 
 // There is a warning about this but it is harmless (1607)
-[assembly: AssemblyInformationalVersion("4.7 beta")]
+[assembly: AssemblyInformationalVersion("4.7")]
