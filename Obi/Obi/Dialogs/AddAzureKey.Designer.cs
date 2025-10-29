4aaacbb5211bf1134a1e6f3be6da8b7ffd72a9c0
Rohit Tandon <rssbrohit@gmail.com>
2025-10-29 12:07:59 +0530

    Obi Arabic localization updated

--- Obi/Obi/Dialogs/AddAzureKey.Designer.cs
+++ Obi/Obi/Dialogs/AddAzureKey.Designer.cs
@@ -28,89 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.m_AddBtn = new System.Windows.Forms.Button();
-            this.m_RegionLbl = new System.Windows.Forms.Label();
-            this.m_KeyLbl = new System.Windows.Forms.Label();
-            this.m_ResgionTB = new System.Windows.Forms.TextBox();
-            this.m_KeyTB = new System.Windows.Forms.TextBox();
-            this.m_CancelBtn = new System.Windows.Forms.Button();
-            this.SuspendLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddAzureKey));
+            m_AddBtn = new System.Windows.Forms.Button();
+            m_RegionLbl = new System.Windows.Forms.Label();
+            m_KeyLbl = new System.Windows.Forms.Label();
+            m_ResgionTB = new System.Windows.Forms.TextBox();
+            m_KeyTB = new System.Windows.Forms.TextBox();
+            m_CancelBtn = new System.Windows.Forms.Button();
+            SuspendLayout();
             // 
             // m_AddBtn
             // 
-            this.m_AddBtn.AccessibleName = "Add Key";
-            this.m_AddBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.m_AddBtn.Location = new System.Drawing.Point(282, 270);
-            this.m_AddBtn.Name = "m_AddBtn";
-            this.m_AddBtn.Size = new System.Drawing.Size(135, 40);
-            this.m_AddBtn.TabIndex = 9;
-            this.m_AddBtn.Text = "&Add Key";
-            this.m_AddBtn.UseVisualStyleBackColor = true;
-            this.m_AddBtn.Click += new System.EventHandler(this.m_AddBtn_Click);
+            resources.ApplyResources(m_AddBtn, "m_AddBtn");
+            m_AddBtn.Name = "m_AddBtn";
+            m_AddBtn.UseVisualStyleBackColor = true;
+            m_AddBtn.Click += m_AddBtn_Click;
             // 
             // m_RegionLbl
             // 
-            this.m_RegionLbl.AutoSize = true;
-            this.m_RegionLbl.Location = new System.Drawing.Point(33, 175);
-            this.m_RegionLbl.Name = "m_RegionLbl";
-            this.m_RegionLbl.Size = new System.Drawing.Size(66, 20);
-            this.m_RegionLbl.TabIndex = 8;
-            this.m_RegionLbl.Text = "Region:";
+            resources.ApplyResources(m_RegionLbl, "m_RegionLbl");
+            m_RegionLbl.Name = "m_RegionLbl";
             // 
             // m_KeyLbl
             // 
-            this.m_KeyLbl.AutoSize = true;
-            this.m_KeyLbl.Location = new System.Drawing.Point(37, 94);
-            this.m_KeyLbl.Name = "m_KeyLbl";
-            this.m_KeyLbl.Size = new System.Drawing.Size(91, 20);
-            this.m_KeyLbl.TabIndex = 7;
-            this.m_KeyLbl.Text = "Azure Key:";
+            resources.ApplyResources(m_KeyLbl, "m_KeyLbl");
+            m_KeyLbl.Name = "m_KeyLbl";
             // 
             // m_ResgionTB
             // 
-            this.m_ResgionTB.AccessibleName = "Region";
-            this.m_ResgionTB.Location = new System.Drawing.Point(149, 172);
-            this.m_ResgionTB.Name = "m_ResgionTB";
-            this.m_ResgionTB.Size = new System.Drawing.Size(201, 26);
-            this.m_ResgionTB.TabIndex = 6;
+            resources.ApplyResources(m_ResgionTB, "m_ResgionTB");
+            m_ResgionTB.Name = "m_ResgionTB";
             // 
             // m_KeyTB
             // 
-            this.m_KeyTB.AccessibleName = "Azure Key";
-            this.m_KeyTB.Location = new System.Drawing.Point(149, 91);
-            this.m_KeyTB.Name = "m_KeyTB";
-            this.m_KeyTB.Size = new System.Drawing.Size(720, 26);
-            this.m_KeyTB.TabIndex = 5;
+            resources.ApplyResources(m_KeyTB, "m_KeyTB");
+            m_KeyTB.Name = "m_KeyTB";
             // 
             // m_CancelBtn
             // 
-            this.m_CancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.m_CancelBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.m_CancelBtn.Location = new System.Drawing.Point(466, 270);
-            this.m_CancelBtn.Name = "m_CancelBtn";
-            this.m_CancelBtn.Size = new System.Drawing.Size(125, 40);
-            this.m_CancelBtn.TabIndex = 10;
-            this.m_CancelBtn.Text = "&Cancel";
-            this.m_CancelBtn.UseVisualStyleBackColor = true;
+            m_CancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(m_CancelBtn, "m_CancelBtn");
+            m_CancelBtn.Name = "m_CancelBtn";
+            m_CancelBtn.UseVisualStyleBackColor = true;
             // 
             // AddAzureKey
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(881, 361);
-            this.Controls.Add(this.m_CancelBtn);
-            this.Controls.Add(this.m_AddBtn);
-            this.Controls.Add(this.m_RegionLbl);
-            this.Controls.Add(this.m_KeyLbl);
-            this.Controls.Add(this.m_ResgionTB);
-            this.Controls.Add(this.m_KeyTB);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "AddAzureKey";
-            this.Text = "Add Azure Key";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            Controls.Add(m_CancelBtn);
+            Controls.Add(m_AddBtn);
+            Controls.Add(m_RegionLbl);
+            Controls.Add(m_KeyLbl);
+            Controls.Add(m_ResgionTB);
+            Controls.Add(m_KeyTB);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "AddAzureKey";
+            ResumeLayout(false);
+            PerformLayout();
 
         }
 
--- Obi/Obi/Dialogs/AddAzureVoice.Designer.cs
+++ Obi/Obi/Dialogs/AddAzureVoice.Designer.cs
@@ -28,155 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.m_AddAzureVoiceListBox = new System.Windows.Forms.ListBox();
-            this.m_AddVoiceBtn = new System.Windows.Forms.Button();
-            this.m_GenderCb = new System.Windows.Forms.ComboBox();
-            this.m_GenderLbl = new System.Windows.Forms.Label();
-            this.m_LanguageLbl = new System.Windows.Forms.Label();
-            this.m_LanguageCb = new System.Windows.Forms.ComboBox();
-            this.m_DialectLbl = new System.Windows.Forms.Label();
-            this.m_DialectCb = new System.Windows.Forms.ComboBox();
-            this.m_VoiceLbl = new System.Windows.Forms.Label();
-            this.m_VoiceCb = new System.Windows.Forms.ComboBox();
-            this.m_CancelBtn = new System.Windows.Forms.Button();
-            this.SuspendLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddAzureVoice));
+            m_AddAzureVoiceListBox = new System.Windows.Forms.ListBox();
+            m_AddVoiceBtn = new System.Windows.Forms.Button();
+            m_GenderCb = new System.Windows.Forms.ComboBox();
+            m_GenderLbl = new System.Windows.Forms.Label();
+            m_LanguageLbl = new System.Windows.Forms.Label();
+            m_LanguageCb = new System.Windows.Forms.ComboBox();
+            m_DialectLbl = new System.Windows.Forms.Label();
+            m_DialectCb = new System.Windows.Forms.ComboBox();
+            m_VoiceLbl = new System.Windows.Forms.Label();
+            m_VoiceCb = new System.Windows.Forms.ComboBox();
+            m_CancelBtn = new System.Windows.Forms.Button();
+            SuspendLayout();
             // 
             // m_AddAzureVoiceListBox
             // 
-            this.m_AddAzureVoiceListBox.AccessibleName = "Voices available in Azure";
-            this.m_AddAzureVoiceListBox.FormattingEnabled = true;
-            this.m_AddAzureVoiceListBox.ItemHeight = 20;
-            this.m_AddAzureVoiceListBox.Location = new System.Drawing.Point(49, 49);
-            this.m_AddAzureVoiceListBox.Name = "m_AddAzureVoiceListBox";
-            this.m_AddAzureVoiceListBox.Size = new System.Drawing.Size(483, 504);
-            this.m_AddAzureVoiceListBox.TabIndex = 0;
+            resources.ApplyResources(m_AddAzureVoiceListBox, "m_AddAzureVoiceListBox");
+            m_AddAzureVoiceListBox.FormattingEnabled = true;
+            m_AddAzureVoiceListBox.Name = "m_AddAzureVoiceListBox";
             // 
             // m_AddVoiceBtn
             // 
-            this.m_AddVoiceBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.m_AddVoiceBtn.Location = new System.Drawing.Point(174, 599);
-            this.m_AddVoiceBtn.Name = "m_AddVoiceBtn";
-            this.m_AddVoiceBtn.Size = new System.Drawing.Size(106, 29);
-            this.m_AddVoiceBtn.TabIndex = 1;
-            this.m_AddVoiceBtn.Text = "&Add Voice";
-            this.m_AddVoiceBtn.UseVisualStyleBackColor = true;
-            this.m_AddVoiceBtn.Click += new System.EventHandler(this.m_AddVoiceBtn_Click);
+            resources.ApplyResources(m_AddVoiceBtn, "m_AddVoiceBtn");
+            m_AddVoiceBtn.Name = "m_AddVoiceBtn";
+            m_AddVoiceBtn.UseVisualStyleBackColor = true;
+            m_AddVoiceBtn.Click += m_AddVoiceBtn_Click;
             // 
             // m_GenderCb
             // 
-            this.m_GenderCb.AccessibleName = "Select Gender";
-            this.m_GenderCb.FormattingEnabled = true;
-            this.m_GenderCb.Items.AddRange(new object[] {
-            "All",
-            "Male",
-            "Female"});
-            this.m_GenderCb.Location = new System.Drawing.Point(558, 102);
-            this.m_GenderCb.Name = "m_GenderCb";
-            this.m_GenderCb.Size = new System.Drawing.Size(196, 28);
-            this.m_GenderCb.TabIndex = 3;
-            this.m_GenderCb.SelectedIndexChanged += new System.EventHandler(this.m_GenderCb_SelectedIndexChanged);
+            resources.ApplyResources(m_GenderCb, "m_GenderCb");
+            m_GenderCb.FormattingEnabled = true;
+            m_GenderCb.Items.AddRange(new object[] { resources.GetString("m_GenderCb.Items"), resources.GetString("m_GenderCb.Items1"), resources.GetString("m_GenderCb.Items2") });
+            m_GenderCb.Name = "m_GenderCb";
+            m_GenderCb.SelectedIndexChanged += m_GenderCb_SelectedIndexChanged;
             // 
             // m_GenderLbl
             // 
-            this.m_GenderLbl.AutoSize = true;
-            this.m_GenderLbl.Location = new System.Drawing.Point(558, 55);
-            this.m_GenderLbl.Name = "m_GenderLbl";
-            this.m_GenderLbl.Size = new System.Drawing.Size(64, 20);
-            this.m_GenderLbl.TabIndex = 2;
-            this.m_GenderLbl.Text = "Gender";
+            resources.ApplyResources(m_GenderLbl, "m_GenderLbl");
+            m_GenderLbl.Name = "m_GenderLbl";
             // 
             // m_LanguageLbl
             // 
-            this.m_LanguageLbl.AutoSize = true;
-            this.m_LanguageLbl.Location = new System.Drawing.Point(558, 184);
-            this.m_LanguageLbl.Name = "m_LanguageLbl";
-            this.m_LanguageLbl.Size = new System.Drawing.Size(82, 20);
-            this.m_LanguageLbl.TabIndex = 4;
-            this.m_LanguageLbl.Text = "Language";
+            resources.ApplyResources(m_LanguageLbl, "m_LanguageLbl");
+            m_LanguageLbl.Name = "m_LanguageLbl";
             // 
             // m_LanguageCb
             // 
-            this.m_LanguageCb.AccessibleName = "Language";
-            this.m_LanguageCb.FormattingEnabled = true;
-            this.m_LanguageCb.Location = new System.Drawing.Point(558, 239);
-            this.m_LanguageCb.Name = "m_LanguageCb";
-            this.m_LanguageCb.Size = new System.Drawing.Size(196, 28);
-            this.m_LanguageCb.TabIndex = 5;
-            this.m_LanguageCb.SelectedIndexChanged += new System.EventHandler(this.m_LanguageCb_SelectedIndexChanged);
+            resources.ApplyResources(m_LanguageCb, "m_LanguageCb");
+            m_LanguageCb.FormattingEnabled = true;
+            m_LanguageCb.Name = "m_LanguageCb";
+            m_LanguageCb.SelectedIndexChanged += m_LanguageCb_SelectedIndexChanged;
             // 
             // m_DialectLbl
             // 
-            this.m_DialectLbl.AutoSize = true;
-            this.m_DialectLbl.Location = new System.Drawing.Point(558, 339);
-            this.m_DialectLbl.Name = "m_DialectLbl";
-            this.m_DialectLbl.Size = new System.Drawing.Size(62, 20);
-            this.m_DialectLbl.TabIndex = 6;
-            this.m_DialectLbl.Text = "Dialect";
+            resources.ApplyResources(m_DialectLbl, "m_DialectLbl");
+            m_DialectLbl.Name = "m_DialectLbl";
             // 
             // m_DialectCb
             // 
-            this.m_DialectCb.AccessibleName = "Dialect";
-            this.m_DialectCb.FormattingEnabled = true;
-            this.m_DialectCb.Location = new System.Drawing.Point(558, 394);
-            this.m_DialectCb.Name = "m_DialectCb";
-            this.m_DialectCb.Size = new System.Drawing.Size(196, 28);
-            this.m_DialectCb.TabIndex = 7;
-            this.m_DialectCb.SelectedIndexChanged += new System.EventHandler(this.m_DialectCb_SelectedIndexChanged);
+            resources.ApplyResources(m_DialectCb, "m_DialectCb");
+            m_DialectCb.FormattingEnabled = true;
+            m_DialectCb.Name = "m_DialectCb";
+            m_DialectCb.SelectedIndexChanged += m_DialectCb_SelectedIndexChanged;
             // 
             // m_VoiceLbl
             // 
-            this.m_VoiceLbl.AutoSize = true;
-            this.m_VoiceLbl.Location = new System.Drawing.Point(558, 473);
-            this.m_VoiceLbl.Name = "m_VoiceLbl";
-            this.m_VoiceLbl.Size = new System.Drawing.Size(51, 20);
-            this.m_VoiceLbl.TabIndex = 8;
-            this.m_VoiceLbl.Text = "Voice";
+            resources.ApplyResources(m_VoiceLbl, "m_VoiceLbl");
+            m_VoiceLbl.Name = "m_VoiceLbl";
             // 
             // m_VoiceCb
             // 
-            this.m_VoiceCb.AccessibleName = "Voice";
-            this.m_VoiceCb.FormattingEnabled = true;
-            this.m_VoiceCb.Location = new System.Drawing.Point(558, 525);
-            this.m_VoiceCb.Name = "m_VoiceCb";
-            this.m_VoiceCb.Size = new System.Drawing.Size(196, 28);
-            this.m_VoiceCb.TabIndex = 9;
-            this.m_VoiceCb.SelectedIndexChanged += new System.EventHandler(this.m_VoiceCb_SelectedIndexChanged);
+            resources.ApplyResources(m_VoiceCb, "m_VoiceCb");
+            m_VoiceCb.FormattingEnabled = true;
+            m_VoiceCb.Name = "m_VoiceCb";
+            m_VoiceCb.SelectedIndexChanged += m_VoiceCb_SelectedIndexChanged;
             // 
             // m_CancelBtn
             // 
-            this.m_CancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.m_CancelBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.m_CancelBtn.Location = new System.Drawing.Point(340, 599);
-            this.m_CancelBtn.Name = "m_CancelBtn";
-            this.m_CancelBtn.Size = new System.Drawing.Size(106, 29);
-            this.m_CancelBtn.TabIndex = 10;
-            this.m_CancelBtn.Text = "&Cancel";
-            this.m_CancelBtn.UseVisualStyleBackColor = true;
+            m_CancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(m_CancelBtn, "m_CancelBtn");
+            m_CancelBtn.Name = "m_CancelBtn";
+            m_CancelBtn.UseVisualStyleBackColor = true;
             // 
             // AddAzureVoice
             // 
-            this.AccessibleName = "Add Azure voice";
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(776, 659);
-            this.Controls.Add(this.m_CancelBtn);
-            this.Controls.Add(this.m_VoiceCb);
-            this.Controls.Add(this.m_VoiceLbl);
-            this.Controls.Add(this.m_DialectCb);
-            this.Controls.Add(this.m_DialectLbl);
-            this.Controls.Add(this.m_LanguageCb);
-            this.Controls.Add(this.m_LanguageLbl);
-            this.Controls.Add(this.m_GenderLbl);
-            this.Controls.Add(this.m_GenderCb);
-            this.Controls.Add(this.m_AddVoiceBtn);
-            this.Controls.Add(this.m_AddAzureVoiceListBox);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "AddAzureVoice";
-            this.Text = "Add Azure Voice";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            Controls.Add(m_CancelBtn);
+            Controls.Add(m_VoiceCb);
+            Controls.Add(m_VoiceLbl);
+            Controls.Add(m_DialectCb);
+            Controls.Add(m_DialectLbl);
+            Controls.Add(m_LanguageCb);
+            Controls.Add(m_LanguageLbl);
+            Controls.Add(m_GenderLbl);
+            Controls.Add(m_GenderCb);
+            Controls.Add(m_AddVoiceBtn);
+            Controls.Add(m_AddAzureVoiceListBox);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "AddAzureVoice";
+            ResumeLayout(false);
+            PerformLayout();
 
         }
 
