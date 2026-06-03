d4bc96c42f978f6940f34b501d8acdf5a79ea342
Rohit Tandon <rssbrohit@gmail.com>
2026-06-03 13:31:07 +0530

    minor

--- Obi/Obi/Dialogs/CreateProjectFromAudio.Designer.cs
+++ Obi/Obi/Dialogs/CreateProjectFromAudio.Designer.cs
@@ -42,17 +42,18 @@
             // 
             // progressBar
             // 
+            progressBar.AccessibleName = "Progress Bar";
             progressBar.Location = new System.Drawing.Point(37, 281);
             progressBar.Name = "progressBar";
             progressBar.Size = new System.Drawing.Size(997, 29);
-            progressBar.TabIndex = 3;
+            progressBar.TabIndex = 6;
             // 
             // m_btnStart
             // 
             m_btnStart.Location = new System.Drawing.Point(261, 341);
             m_btnStart.Name = "m_btnStart";
             m_btnStart.Size = new System.Drawing.Size(126, 40);
-            m_btnStart.TabIndex = 5;
+            m_btnStart.TabIndex = 7;
             m_btnStart.Text = "&Start";
             m_btnStart.UseVisualStyleBackColor = true;
             m_btnStart.Click += m_btnStart_Click;
@@ -62,7 +63,7 @@
             m_btnCancel.Location = new System.Drawing.Point(472, 341);
             m_btnCancel.Name = "m_btnCancel";
             m_btnCancel.Size = new System.Drawing.Size(110, 40);
-            m_btnCancel.TabIndex = 6;
+            m_btnCancel.TabIndex = 8;
             m_btnCancel.Text = "&Cancel";
             m_btnCancel.UseVisualStyleBackColor = true;
             m_btnCancel.Click += m_btnCancel_Click;
@@ -74,14 +75,14 @@
             txtLog.Name = "txtLog";
             txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
             txtLog.Size = new System.Drawing.Size(900, 400);
-            txtLog.TabIndex = 8;
+            txtLog.TabIndex = 10;
             // 
             // m_btnClose
             // 
             m_btnClose.Location = new System.Drawing.Point(696, 341);
             m_btnClose.Name = "m_btnClose";
             m_btnClose.Size = new System.Drawing.Size(110, 40);
-            m_btnClose.TabIndex = 7;
+            m_btnClose.TabIndex = 9;
             m_btnClose.Text = "C&lose";
             m_btnClose.UseVisualStyleBackColor = true;
             m_btnClose.Click += m_btnClose_Click;
@@ -95,7 +96,7 @@
             lstAudioFiles.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             lstAudioFiles.Name = "lstAudioFiles";
             lstAudioFiles.Size = new System.Drawing.Size(718, 224);
-            lstAudioFiles.TabIndex = 9;
+            lstAudioFiles.TabIndex = 1;
             lstAudioFiles.SelectedIndexChanged += lstAudioFiles_SelectedIndexChanged;
             // 
             // m_btnRemove
@@ -106,7 +107,7 @@
             m_btnRemove.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             m_btnRemove.Name = "m_btnRemove";
             m_btnRemove.Size = new System.Drawing.Size(99, 29);
-            m_btnRemove.TabIndex = 13;
+            m_btnRemove.TabIndex = 5;
             m_btnRemove.Text = "&Remove";
             m_btnRemove.UseVisualStyleBackColor = true;
             m_btnRemove.Click += m_btnRemove_Click;
@@ -119,7 +120,7 @@
             m_btnAdd.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             m_btnAdd.Name = "m_btnAdd";
             m_btnAdd.Size = new System.Drawing.Size(102, 29);
-            m_btnAdd.TabIndex = 12;
+            m_btnAdd.TabIndex = 4;
             m_btnAdd.Text = "&Add";
             m_btnAdd.UseVisualStyleBackColor = true;
             m_btnAdd.Click += m_btnAddAudio_Click;
@@ -132,7 +133,7 @@
             m_btnMoveUp.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             m_btnMoveUp.Name = "m_btnMoveUp";
             m_btnMoveUp.Size = new System.Drawing.Size(102, 32);
-            m_btnMoveUp.TabIndex = 10;
+            m_btnMoveUp.TabIndex = 2;
             m_btnMoveUp.Text = "Move &UP";
             m_btnMoveUp.UseVisualStyleBackColor = true;
             m_btnMoveUp.Click += m_btnMoveUp_Click;
@@ -145,7 +146,7 @@
             m_btnMoveDown.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             m_btnMoveDown.Name = "m_btnMoveDown";
             m_btnMoveDown.Size = new System.Drawing.Size(102, 29);
-            m_btnMoveDown.TabIndex = 11;
+            m_btnMoveDown.TabIndex = 3;
             m_btnMoveDown.Text = "Move &Down";
             m_btnMoveDown.UseVisualStyleBackColor = true;
             m_btnMoveDown.Click += m_btnMoveDown_Click;
