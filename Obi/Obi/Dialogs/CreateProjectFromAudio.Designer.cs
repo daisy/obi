9001641b975a0e547137162afdf1c9fdc6d6a9d5
Rohit Tandon <rssbrohit@gmail.com>
2026-06-04 12:50:58 +0530

    minor

--- Obi/Obi/Dialogs/CreateProjectFromAudio.Designer.cs
+++ Obi/Obi/Dialogs/CreateProjectFromAudio.Designer.cs
@@ -51,17 +51,17 @@
             // progressBar
             // 
             progressBar.AccessibleName = "Progress Bar";
-            progressBar.Location = new System.Drawing.Point(19, 400);
+            progressBar.Location = new System.Drawing.Point(21, 295);
             progressBar.Name = "progressBar";
-            progressBar.Size = new System.Drawing.Size(911, 29);
+            progressBar.Size = new System.Drawing.Size(911, 37);
             progressBar.TabIndex = 10;
             // 
             // m_btnStart
             // 
             m_btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            m_btnStart.Location = new System.Drawing.Point(119, 22);
+            m_btnStart.Location = new System.Drawing.Point(151, 21);
             m_btnStart.Name = "m_btnStart";
-            m_btnStart.Size = new System.Drawing.Size(126, 35);
+            m_btnStart.Size = new System.Drawing.Size(158, 35);
             m_btnStart.TabIndex = 12;
             m_btnStart.Text = "&Start";
             m_btnStart.UseVisualStyleBackColor = true;
@@ -70,9 +70,9 @@
             // m_btnCancel
             // 
             m_btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            m_btnCancel.Location = new System.Drawing.Point(440, 22);
+            m_btnCancel.Location = new System.Drawing.Point(448, 21);
             m_btnCancel.Name = "m_btnCancel";
-            m_btnCancel.Size = new System.Drawing.Size(110, 35);
+            m_btnCancel.Size = new System.Drawing.Size(168, 35);
             m_btnCancel.TabIndex = 13;
             m_btnCancel.Text = "&Cancel";
             m_btnCancel.UseVisualStyleBackColor = true;
@@ -80,18 +80,18 @@
             // 
             // txtLog
             // 
-            txtLog.Location = new System.Drawing.Point(19, 525);
+            txtLog.Location = new System.Drawing.Point(21, 413);
             txtLog.Multiline = true;
             txtLog.Name = "txtLog";
             txtLog.ReadOnly = true;
             txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            txtLog.Size = new System.Drawing.Size(911, 308);
+            txtLog.Size = new System.Drawing.Size(911, 209);
             txtLog.TabIndex = 14;
             // 
             // m_btnClose
             // 
             m_btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            m_btnClose.Location = new System.Drawing.Point(386, 851);
+            m_btnClose.Location = new System.Drawing.Point(382, 629);
             m_btnClose.Name = "m_btnClose";
             m_btnClose.Size = new System.Drawing.Size(110, 35);
             m_btnClose.TabIndex = 15;
@@ -104,10 +104,10 @@
             lstAudioFiles.AccessibleName = "Audio files list box.";
             lstAudioFiles.FormattingEnabled = true;
             lstAudioFiles.HorizontalScrollbar = true;
-            lstAudioFiles.Location = new System.Drawing.Point(45, 27);
+            lstAudioFiles.Location = new System.Drawing.Point(18, 27);
             lstAudioFiles.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             lstAudioFiles.Name = "lstAudioFiles";
-            lstAudioFiles.Size = new System.Drawing.Size(718, 224);
+            lstAudioFiles.Size = new System.Drawing.Size(745, 144);
             lstAudioFiles.TabIndex = 2;
             lstAudioFiles.SelectedIndexChanged += lstAudioFiles_SelectedIndexChanged;
             // 
@@ -115,10 +115,10 @@
             // 
             m_btnRemove.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             m_btnRemove.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            m_btnRemove.Location = new System.Drawing.Point(791, 204);
+            m_btnRemove.Location = new System.Drawing.Point(791, 151);
             m_btnRemove.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             m_btnRemove.Name = "m_btnRemove";
-            m_btnRemove.Size = new System.Drawing.Size(99, 29);
+            m_btnRemove.Size = new System.Drawing.Size(99, 35);
             m_btnRemove.TabIndex = 6;
             m_btnRemove.Text = "&Remove";
             m_btnRemove.UseVisualStyleBackColor = true;
@@ -128,10 +128,10 @@
             // 
             m_btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             m_btnAdd.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            m_btnAdd.Location = new System.Drawing.Point(791, 141);
+            m_btnAdd.Location = new System.Drawing.Point(791, 107);
             m_btnAdd.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             m_btnAdd.Name = "m_btnAdd";
-            m_btnAdd.Size = new System.Drawing.Size(102, 29);
+            m_btnAdd.Size = new System.Drawing.Size(99, 35);
             m_btnAdd.TabIndex = 5;
             m_btnAdd.Text = "&Add";
             m_btnAdd.UseVisualStyleBackColor = true;
@@ -141,10 +141,10 @@
             // 
             m_btnMoveUp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             m_btnMoveUp.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            m_btnMoveUp.Location = new System.Drawing.Point(791, 27);
+            m_btnMoveUp.Location = new System.Drawing.Point(791, 20);
             m_btnMoveUp.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             m_btnMoveUp.Name = "m_btnMoveUp";
-            m_btnMoveUp.Size = new System.Drawing.Size(102, 32);
+            m_btnMoveUp.Size = new System.Drawing.Size(99, 35);
             m_btnMoveUp.TabIndex = 3;
             m_btnMoveUp.Text = "Move &UP";
             m_btnMoveUp.UseVisualStyleBackColor = true;
@@ -154,10 +154,10 @@
             // 
             m_btnMoveDown.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             m_btnMoveDown.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            m_btnMoveDown.Location = new System.Drawing.Point(791, 83);
+            m_btnMoveDown.Location = new System.Drawing.Point(791, 64);
             m_btnMoveDown.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             m_btnMoveDown.Name = "m_btnMoveDown";
-            m_btnMoveDown.Size = new System.Drawing.Size(102, 29);
+            m_btnMoveDown.Size = new System.Drawing.Size(99, 35);
             m_btnMoveDown.TabIndex = 4;
             m_btnMoveDown.Text = "Move &Down";
             m_btnMoveDown.UseVisualStyleBackColor = true;
@@ -173,7 +173,7 @@
             m_grpAddFiles.Controls.Add(m_btnMoveDown);
             m_grpAddFiles.Location = new System.Drawing.Point(19, 12);
             m_grpAddFiles.Name = "m_grpAddFiles";
-            m_grpAddFiles.Size = new System.Drawing.Size(911, 366);
+            m_grpAddFiles.Size = new System.Drawing.Size(911, 272);
             m_grpAddFiles.TabIndex = 1;
             m_grpAddFiles.TabStop = false;
             // 
@@ -182,9 +182,9 @@
             m_grpArrangeAudioFiles.AccessibleName = "";
             m_grpArrangeAudioFiles.Controls.Add(m_btnDesendingOrder);
             m_grpArrangeAudioFiles.Controls.Add(m_btnAscendingOrder);
-            m_grpArrangeAudioFiles.Location = new System.Drawing.Point(82, 272);
+            m_grpArrangeAudioFiles.Location = new System.Drawing.Point(18, 179);
             m_grpArrangeAudioFiles.Name = "m_grpArrangeAudioFiles";
-            m_grpArrangeAudioFiles.Size = new System.Drawing.Size(667, 74);
+            m_grpArrangeAudioFiles.Size = new System.Drawing.Size(745, 75);
             m_grpArrangeAudioFiles.TabIndex = 7;
             m_grpArrangeAudioFiles.TabStop = false;
             // 
@@ -195,7 +195,7 @@
             m_btnDesendingOrder.Location = new System.Drawing.Point(403, 27);
             m_btnDesendingOrder.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             m_btnDesendingOrder.Name = "m_btnDesendingOrder";
-            m_btnDesendingOrder.Size = new System.Drawing.Size(168, 32);
+            m_btnDesendingOrder.Size = new System.Drawing.Size(168, 35);
             m_btnDesendingOrder.TabIndex = 9;
             m_btnDesendingOrder.Text = "D&escending Order";
             m_btnDesendingOrder.UseVisualStyleBackColor = true;
@@ -208,7 +208,7 @@
             m_btnAscendingOrder.Location = new System.Drawing.Point(106, 27);
             m_btnAscendingOrder.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             m_btnAscendingOrder.Name = "m_btnAscendingOrder";
-            m_btnAscendingOrder.Size = new System.Drawing.Size(158, 32);
+            m_btnAscendingOrder.Size = new System.Drawing.Size(158, 35);
             m_btnAscendingOrder.TabIndex = 8;
             m_btnAscendingOrder.Text = "Asce&nding Order";
             m_btnAscendingOrder.UseVisualStyleBackColor = true;
@@ -218,9 +218,9 @@
             // 
             m_grpStartProcess.Controls.Add(m_btnStart);
             m_grpStartProcess.Controls.Add(m_btnCancel);
-            m_grpStartProcess.Location = new System.Drawing.Point(101, 448);
+            m_grpStartProcess.Location = new System.Drawing.Point(21, 337);
             m_grpStartProcess.Name = "m_grpStartProcess";
-            m_grpStartProcess.Size = new System.Drawing.Size(667, 71);
+            m_grpStartProcess.Size = new System.Drawing.Size(906, 71);
             m_grpStartProcess.TabIndex = 11;
             m_grpStartProcess.TabStop = false;
             // 
@@ -228,7 +228,7 @@
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(939, 910);
+            ClientSize = new System.Drawing.Size(960, 668);
             Controls.Add(m_grpStartProcess);
             Controls.Add(m_grpAddFiles);
             Controls.Add(m_btnClose);
