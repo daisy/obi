e57bc7d6a1454b48974ec1758b89abd4edd9cfd4
Rohit Tandon <rssbrohit@gmail.com>
2026-07-11 11:54:40 +0530

    In the continuation with last commit.

---
+++ Obi/Obi/Models/WhisperModelItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obi.Models
+{
+    public class WhisperModelItem
+    {
+        public WhisperModel Model { get; set; }
+
+        public string DisplayName { get; set; } = "";
+    }
+}
