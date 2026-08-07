f8e138cec958c63c72b1898a247ea724f160fa60
Rohit Tandon <rssbrohit@gmail.com>
2026-08-07 16:55:23 +0530

    Metadata View width increased in accordance with TOC View width.

--- Obi/Obi/ProjectView/MetadataView.Designer.cs
+++ Obi/Obi/ProjectView/MetadataView.Designer.cs
@@ -28,134 +28,130 @@ namespace Obi.ProjectView
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MetadataView));
-            this.mMetadataListView = new System.Windows.Forms.ListView();
-            this.mNameColumn = new System.Windows.Forms.ColumnHeader();
-            this.mContentColumn = new System.Windows.Forms.ColumnHeader();
-            this.mContentTextbox = new System.Windows.Forms.TextBox();
-            this.mUpdateButton = new System.Windows.Forms.Button();
-            this.mNameTextbox = new System.Windows.Forms.TextBox();
-            this.mNameLabel = new System.Windows.Forms.Label();
-            this.mContentLabel = new System.Windows.Forms.Label();
-            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
-            this.m_BtnContextMenu = new System.Windows.Forms.Button();
-            this.mMetadataContextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.SetDefaultMetadataStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.SetDefaultMetadataOverwriteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.SaveAsDefaultMetadataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.mMetadataContextMenuStrip.SuspendLayout();
-            this.SuspendLayout();
+            mMetadataListView = new System.Windows.Forms.ListView();
+            mNameColumn = new System.Windows.Forms.ColumnHeader();
+            mContentColumn = new System.Windows.Forms.ColumnHeader();
+            mContentTextbox = new System.Windows.Forms.TextBox();
+            mUpdateButton = new System.Windows.Forms.Button();
+            mNameTextbox = new System.Windows.Forms.TextBox();
+            mNameLabel = new System.Windows.Forms.Label();
+            mContentLabel = new System.Windows.Forms.Label();
+            toolTip1 = new System.Windows.Forms.ToolTip(components);
+            m_BtnContextMenu = new System.Windows.Forms.Button();
+            mMetadataContextMenuStrip = new System.Windows.Forms.ContextMenuStrip(components);
+            SetDefaultMetadataStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            SetDefaultMetadataOverwriteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            SaveAsDefaultMetadataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            mMetadataContextMenuStrip.SuspendLayout();
+            SuspendLayout();
             // 
             // mMetadataListView
             // 
-            resources.ApplyResources(this.mMetadataListView, "mMetadataListView");
-            this.mMetadataListView.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.mMetadataListView.CheckBoxes = true;
-            this.mMetadataListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.mNameColumn,
-            this.mContentColumn});
-            this.mMetadataListView.FullRowSelect = true;
-            this.mMetadataListView.MultiSelect = false;
-            this.mMetadataListView.Name = "mMetadataListView";
-            this.mMetadataListView.ShowItemToolTips = true;
-            this.mMetadataListView.UseCompatibleStateImageBehavior = false;
-            this.mMetadataListView.View = System.Windows.Forms.View.Details;
-            this.mMetadataListView.ItemChecked += new System.Windows.Forms.ItemCheckedEventHandler(this.mMetadataListView_ItemChecked);
-            this.mMetadataListView.ItemMouseHover += new System.Windows.Forms.ListViewItemMouseHoverEventHandler(this.mMetadataListView_ItemMouseHover);
-            this.mMetadataListView.SelectedIndexChanged += new System.EventHandler(this.mMetadataListView_SelectedIndexChanged);
-            this.mMetadataListView.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.mMetadataListView_ItemCheck);
+            resources.ApplyResources(mMetadataListView, "mMetadataListView");
+            mMetadataListView.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            mMetadataListView.CheckBoxes = true;
+            mMetadataListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] { mNameColumn, mContentColumn });
+            mMetadataListView.FullRowSelect = true;
+            mMetadataListView.MultiSelect = false;
+            mMetadataListView.Name = "mMetadataListView";
+            mMetadataListView.ShowItemToolTips = true;
+            mMetadataListView.UseCompatibleStateImageBehavior = false;
+            mMetadataListView.View = System.Windows.Forms.View.Details;
+            mMetadataListView.ItemCheck += mMetadataListView_ItemCheck;
+            mMetadataListView.ItemChecked += mMetadataListView_ItemChecked;
+            mMetadataListView.ItemMouseHover += mMetadataListView_ItemMouseHover;
+            mMetadataListView.SelectedIndexChanged += mMetadataListView_SelectedIndexChanged;
             // 
             // mNameColumn
             // 
-            resources.ApplyResources(this.mNameColumn, "mNameColumn");
+            resources.ApplyResources(mNameColumn, "mNameColumn");
             // 
             // mContentColumn
             // 
-            resources.ApplyResources(this.mContentColumn, "mContentColumn");
+            resources.ApplyResources(mContentColumn, "mContentColumn");
             // 
             // mContentTextbox
             // 
-            resources.ApplyResources(this.mContentTextbox, "mContentTextbox");
-            this.mContentTextbox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.mContentTextbox.Name = "mContentTextbox";
-            this.mContentTextbox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.mContentTextbox_KeyDown);
-            this.mContentTextbox.Leave += new System.EventHandler(this.mContentTextbox_Leave);
+            resources.ApplyResources(mContentTextbox, "mContentTextbox");
+            mContentTextbox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            mContentTextbox.Name = "mContentTextbox";
+            mContentTextbox.KeyDown += mContentTextbox_KeyDown;
+            mContentTextbox.Leave += mContentTextbox_Leave;
             // 
             // mUpdateButton
             // 
-            resources.ApplyResources(this.mUpdateButton, "mUpdateButton");
-            this.mUpdateButton.Name = "mUpdateButton";
-            this.mUpdateButton.UseVisualStyleBackColor = true;
-            this.mUpdateButton.Click += new System.EventHandler(this.mCommitButton_Click);
+            resources.ApplyResources(mUpdateButton, "mUpdateButton");
+            mUpdateButton.Name = "mUpdateButton";
+            mUpdateButton.UseVisualStyleBackColor = true;
+            mUpdateButton.Click += mCommitButton_Click;
             // 
             // mNameTextbox
             // 
-            resources.ApplyResources(this.mNameTextbox, "mNameTextbox");
-            this.mNameTextbox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.mNameTextbox.Name = "mNameTextbox";
-            this.mNameTextbox.Leave += new System.EventHandler(this.mNameTextbox_Leave);
+            resources.ApplyResources(mNameTextbox, "mNameTextbox");
+            mNameTextbox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            mNameTextbox.Name = "mNameTextbox";
+            mNameTextbox.Leave += mNameTextbox_Leave;
             // 
             // mNameLabel
             // 
-            resources.ApplyResources(this.mNameLabel, "mNameLabel");
-            this.mNameLabel.Name = "mNameLabel";
+            resources.ApplyResources(mNameLabel, "mNameLabel");
+            mNameLabel.Name = "mNameLabel";
             // 
             // mContentLabel
             // 
-            resources.ApplyResources(this.mContentLabel, "mContentLabel");
-            this.mContentLabel.Name = "mContentLabel";
+            resources.ApplyResources(mContentLabel, "mContentLabel");
+            mContentLabel.Name = "mContentLabel";
             // 
             // m_BtnContextMenu
             // 
-            resources.ApplyResources(this.m_BtnContextMenu, "m_BtnContextMenu");
-            this.m_BtnContextMenu.Name = "m_BtnContextMenu";
-            this.m_BtnContextMenu.UseVisualStyleBackColor = true;
-            this.m_BtnContextMenu.Click += new System.EventHandler(this.m_BtnContextMenu_Click);
+            resources.ApplyResources(m_BtnContextMenu, "m_BtnContextMenu");
+            m_BtnContextMenu.Name = "m_BtnContextMenu";
+            m_BtnContextMenu.UseVisualStyleBackColor = true;
+            m_BtnContextMenu.Click += m_BtnContextMenu_Click;
             // 
             // mMetadataContextMenuStrip
             // 
-            this.mMetadataContextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.SetDefaultMetadataStripMenuItem,
-            this.SetDefaultMetadataOverwriteToolStripMenuItem,
-            this.SaveAsDefaultMetadataToolStripMenuItem});
-            this.mMetadataContextMenuStrip.Name = "mMetadataContextMenuStrip";
-            resources.ApplyResources(this.mMetadataContextMenuStrip, "mMetadataContextMenuStrip");
+            mMetadataContextMenuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            mMetadataContextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { SetDefaultMetadataStripMenuItem, SetDefaultMetadataOverwriteToolStripMenuItem, SaveAsDefaultMetadataToolStripMenuItem });
+            mMetadataContextMenuStrip.Name = "mMetadataContextMenuStrip";
+            resources.ApplyResources(mMetadataContextMenuStrip, "mMetadataContextMenuStrip");
             // 
             // SetDefaultMetadataStripMenuItem
             // 
-            this.SetDefaultMetadataStripMenuItem.Name = "SetDefaultMetadataStripMenuItem";
-            resources.ApplyResources(this.SetDefaultMetadataStripMenuItem, "SetDefaultMetadataStripMenuItem");
-            this.SetDefaultMetadataStripMenuItem.Click += new System.EventHandler(this.SetDefaultMetadataStripMenuItem_Click);
+            SetDefaultMetadataStripMenuItem.Name = "SetDefaultMetadataStripMenuItem";
+            resources.ApplyResources(SetDefaultMetadataStripMenuItem, "SetDefaultMetadataStripMenuItem");
+            SetDefaultMetadataStripMenuItem.Click += SetDefaultMetadataStripMenuItem_Click;
             // 
             // SetDefaultMetadataOverwriteToolStripMenuItem
             // 
-            this.SetDefaultMetadataOverwriteToolStripMenuItem.Name = "SetDefaultMetadataOverwriteToolStripMenuItem";
-            resources.ApplyResources(this.SetDefaultMetadataOverwriteToolStripMenuItem, "SetDefaultMetadataOverwriteToolStripMenuItem");
-            this.SetDefaultMetadataOverwriteToolStripMenuItem.Click += new System.EventHandler(this.SetDefaultMetadataOverwriteToolStripMenuItem_Click);
+            SetDefaultMetadataOverwriteToolStripMenuItem.Name = "SetDefaultMetadataOverwriteToolStripMenuItem";
+            resources.ApplyResources(SetDefaultMetadataOverwriteToolStripMenuItem, "SetDefaultMetadataOverwriteToolStripMenuItem");
+            SetDefaultMetadataOverwriteToolStripMenuItem.Click += SetDefaultMetadataOverwriteToolStripMenuItem_Click;
             // 
             // SaveAsDefaultMetadataToolStripMenuItem
             // 
-            this.SaveAsDefaultMetadataToolStripMenuItem.Name = "SaveAsDefaultMetadataToolStripMenuItem";
-            resources.ApplyResources(this.SaveAsDefaultMetadataToolStripMenuItem, "SaveAsDefaultMetadataToolStripMenuItem");
-            this.SaveAsDefaultMetadataToolStripMenuItem.Click += new System.EventHandler(this.SaveAsDefaultMetadataToolStripMenuItem_Click);
+            SaveAsDefaultMetadataToolStripMenuItem.Name = "SaveAsDefaultMetadataToolStripMenuItem";
+            resources.ApplyResources(SaveAsDefaultMetadataToolStripMenuItem, "SaveAsDefaultMetadataToolStripMenuItem");
+            SaveAsDefaultMetadataToolStripMenuItem.Click += SaveAsDefaultMetadataToolStripMenuItem_Click;
             // 
             // MetadataView
             // 
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.Controls.Add(this.m_BtnContextMenu);
-            this.Controls.Add(this.mContentLabel);
-            this.Controls.Add(this.mNameLabel);
-            this.Controls.Add(this.mMetadataListView);
-            this.Controls.Add(this.mNameTextbox);
-            this.Controls.Add(this.mUpdateButton);
-            this.Controls.Add(this.mContentTextbox);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            Controls.Add(m_BtnContextMenu);
+            Controls.Add(mContentLabel);
+            Controls.Add(mNameLabel);
+            Controls.Add(mMetadataListView);
+            Controls.Add(mNameTextbox);
+            Controls.Add(mUpdateButton);
+            Controls.Add(mContentTextbox);
             resources.ApplyResources(this, "$this");
-            this.Name = "MetadataView";
-            this.VisibleChanged += new System.EventHandler(this.MetadataView_VisibleChanged);
-            this.mMetadataContextMenuStrip.ResumeLayout(false);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            Name = "MetadataView";
+            VisibleChanged += MetadataView_VisibleChanged;
+            mMetadataContextMenuStrip.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
 
         }
 
