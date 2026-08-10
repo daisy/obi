fa8f8b23b175b220abdb0cb3c9213f663901b85e
Rohit Tandon <rssbrohit@gmail.com>
2026-08-10 13:48:03 +0530

    Localizable set true.

--- Obi/Obi/Dialogs/CreateProjectFromAudio.Designer.cs
+++ Obi/Obi/Dialogs/CreateProjectFromAudio.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CreateProjectFromAudio));
             progressBar = new System.Windows.Forms.ProgressBar();
             m_btnStart = new System.Windows.Forms.Button();
             m_btnCancel = new System.Windows.Forms.Button();
@@ -48,118 +49,70 @@
             m_grpStartProcess.SuspendLayout();
             SuspendLayout();
             // 
-            // m_ProgressBar
+            // progressBar
             // 
-            progressBar.AccessibleName = "Progress Bar";
-            progressBar.Location = new System.Drawing.Point(21, 295);
-            progressBar.Name = "m_ProgressBar";
-            progressBar.Size = new System.Drawing.Size(911, 37);
-            progressBar.TabIndex = 10;
+            resources.ApplyResources(progressBar, "progressBar");
+            progressBar.Name = "progressBar";
             // 
             // m_btnStart
             // 
-            m_btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            m_btnStart.Location = new System.Drawing.Point(151, 21);
+            resources.ApplyResources(m_btnStart, "m_btnStart");
             m_btnStart.Name = "m_btnStart";
-            m_btnStart.Size = new System.Drawing.Size(158, 35);
-            m_btnStart.TabIndex = 12;
-            m_btnStart.Text = "&Start";
             m_btnStart.UseVisualStyleBackColor = true;
             m_btnStart.Click += m_btnStart_Click;
             // 
             // m_btnCancel
             // 
-            m_btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            m_btnCancel.Location = new System.Drawing.Point(448, 21);
+            resources.ApplyResources(m_btnCancel, "m_btnCancel");
             m_btnCancel.Name = "m_btnCancel";
-            m_btnCancel.Size = new System.Drawing.Size(168, 35);
-            m_btnCancel.TabIndex = 13;
-            m_btnCancel.Text = "&Cancel";
             m_btnCancel.UseVisualStyleBackColor = true;
             m_btnCancel.Click += m_btnCancel_Click;
             // 
-            // m_LogTxt
+            // txtLog
             // 
-            txtLog.Location = new System.Drawing.Point(21, 413);
-            txtLog.Multiline = true;
-            txtLog.Name = "m_LogTxt";
+            resources.ApplyResources(txtLog, "txtLog");
+            txtLog.Name = "txtLog";
             txtLog.ReadOnly = true;
-            txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            txtLog.Size = new System.Drawing.Size(911, 209);
-            txtLog.TabIndex = 14;
             // 
             // m_btnClose
             // 
-            m_btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            m_btnClose.Location = new System.Drawing.Point(382, 629);
+            resources.ApplyResources(m_btnClose, "m_btnClose");
             m_btnClose.Name = "m_btnClose";
-            m_btnClose.Size = new System.Drawing.Size(110, 35);
-            m_btnClose.TabIndex = 15;
-            m_btnClose.Text = "C&lose";
             m_btnClose.UseVisualStyleBackColor = true;
             m_btnClose.Click += m_btnClose_Click;
             // 
             // lstAudioFiles
             // 
-            lstAudioFiles.AccessibleName = "Audio files list box.";
+            resources.ApplyResources(lstAudioFiles, "lstAudioFiles");
             lstAudioFiles.FormattingEnabled = true;
-            lstAudioFiles.HorizontalScrollbar = true;
-            lstAudioFiles.Location = new System.Drawing.Point(18, 27);
-            lstAudioFiles.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             lstAudioFiles.Name = "lstAudioFiles";
-            lstAudioFiles.Size = new System.Drawing.Size(745, 144);
-            lstAudioFiles.TabIndex = 2;
             lstAudioFiles.SelectedIndexChanged += lstAudioFiles_SelectedIndexChanged;
             // 
             // m_btnRemove
             // 
-            m_btnRemove.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            m_btnRemove.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            m_btnRemove.Location = new System.Drawing.Point(791, 151);
-            m_btnRemove.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            resources.ApplyResources(m_btnRemove, "m_btnRemove");
             m_btnRemove.Name = "m_btnRemove";
-            m_btnRemove.Size = new System.Drawing.Size(99, 35);
-            m_btnRemove.TabIndex = 6;
-            m_btnRemove.Text = "&Remove";
             m_btnRemove.UseVisualStyleBackColor = true;
             m_btnRemove.Click += m_btnRemove_Click;
             // 
             // m_btnAdd
             // 
-            m_btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            m_btnAdd.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            m_btnAdd.Location = new System.Drawing.Point(791, 107);
-            m_btnAdd.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            resources.ApplyResources(m_btnAdd, "m_btnAdd");
             m_btnAdd.Name = "m_btnAdd";
-            m_btnAdd.Size = new System.Drawing.Size(99, 35);
-            m_btnAdd.TabIndex = 5;
-            m_btnAdd.Text = "&Add";
             m_btnAdd.UseVisualStyleBackColor = true;
             m_btnAdd.Click += m_btnAddAudio_Click;
             // 
             // m_btnMoveUp
             // 
-            m_btnMoveUp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            m_btnMoveUp.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            m_btnMoveUp.Location = new System.Drawing.Point(791, 20);
-            m_btnMoveUp.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            resources.ApplyResources(m_btnMoveUp, "m_btnMoveUp");
             m_btnMoveUp.Name = "m_btnMoveUp";
-            m_btnMoveUp.Size = new System.Drawing.Size(99, 35);
-            m_btnMoveUp.TabIndex = 3;
-            m_btnMoveUp.Text = "Move &UP";
             m_btnMoveUp.UseVisualStyleBackColor = true;
             m_btnMoveUp.Click += m_btnMoveUp_Click;
             // 
             // m_btnMoveDown
             // 
-            m_btnMoveDown.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            m_btnMoveDown.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            m_btnMoveDown.Location = new System.Drawing.Point(791, 64);
-            m_btnMoveDown.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            resources.ApplyResources(m_btnMoveDown, "m_btnMoveDown");
             m_btnMoveDown.Name = "m_btnMoveDown";
-            m_btnMoveDown.Size = new System.Drawing.Size(99, 35);
-            m_btnMoveDown.TabIndex = 4;
-            m_btnMoveDown.Text = "Move &Down";
             m_btnMoveDown.UseVisualStyleBackColor = true;
             m_btnMoveDown.Click += m_btnMoveDown_Click;
             // 
@@ -171,46 +124,29 @@
             m_grpAddFiles.Controls.Add(m_btnMoveUp);
             m_grpAddFiles.Controls.Add(m_btnAdd);
             m_grpAddFiles.Controls.Add(m_btnMoveDown);
-            m_grpAddFiles.Location = new System.Drawing.Point(19, 12);
+            resources.ApplyResources(m_grpAddFiles, "m_grpAddFiles");
             m_grpAddFiles.Name = "m_grpAddFiles";
-            m_grpAddFiles.Size = new System.Drawing.Size(911, 272);
-            m_grpAddFiles.TabIndex = 1;
             m_grpAddFiles.TabStop = false;
             // 
             // m_grpArrangeAudioFiles
             // 
-            m_grpArrangeAudioFiles.AccessibleName = "";
+            resources.ApplyResources(m_grpArrangeAudioFiles, "m_grpArrangeAudioFiles");
             m_grpArrangeAudioFiles.Controls.Add(m_btnDesendingOrder);
             m_grpArrangeAudioFiles.Controls.Add(m_btnAscendingOrder);
-            m_grpArrangeAudioFiles.Location = new System.Drawing.Point(18, 179);
             m_grpArrangeAudioFiles.Name = "m_grpArrangeAudioFiles";
-            m_grpArrangeAudioFiles.Size = new System.Drawing.Size(745, 75);
-            m_grpArrangeAudioFiles.TabIndex = 7;
             m_grpArrangeAudioFiles.TabStop = false;
             // 
             // m_btnDesendingOrder
             // 
-            m_btnDesendingOrder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            m_btnDesendingOrder.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            m_btnDesendingOrder.Location = new System.Drawing.Point(403, 27);
-            m_btnDesendingOrder.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            resources.ApplyResources(m_btnDesendingOrder, "m_btnDesendingOrder");
             m_btnDesendingOrder.Name = "m_btnDesendingOrder";
-            m_btnDesendingOrder.Size = new System.Drawing.Size(168, 35);
-            m_btnDesendingOrder.TabIndex = 9;
-            m_btnDesendingOrder.Text = "D&escending Order";
             m_btnDesendingOrder.UseVisualStyleBackColor = true;
             m_btnDesendingOrder.Click += m_btnDesendingOrder_Click;
             // 
             // m_btnAscendingOrder
             // 
-            m_btnAscendingOrder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            m_btnAscendingOrder.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            m_btnAscendingOrder.Location = new System.Drawing.Point(106, 27);
-            m_btnAscendingOrder.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            resources.ApplyResources(m_btnAscendingOrder, "m_btnAscendingOrder");
             m_btnAscendingOrder.Name = "m_btnAscendingOrder";
-            m_btnAscendingOrder.Size = new System.Drawing.Size(158, 35);
-            m_btnAscendingOrder.TabIndex = 8;
-            m_btnAscendingOrder.Text = "Asce&nding Order";
             m_btnAscendingOrder.UseVisualStyleBackColor = true;
             m_btnAscendingOrder.Click += m_btnAscendingOrder_Click;
             // 
@@ -218,24 +154,20 @@
             // 
             m_grpStartProcess.Controls.Add(m_btnStart);
             m_grpStartProcess.Controls.Add(m_btnCancel);
-            m_grpStartProcess.Location = new System.Drawing.Point(21, 337);
+            resources.ApplyResources(m_grpStartProcess, "m_grpStartProcess");
             m_grpStartProcess.Name = "m_grpStartProcess";
-            m_grpStartProcess.Size = new System.Drawing.Size(906, 71);
-            m_grpStartProcess.TabIndex = 11;
             m_grpStartProcess.TabStop = false;
             // 
             // CreateProjectFromAudio
             // 
-            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            resources.ApplyResources(this, "$this");
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(960, 668);
             Controls.Add(m_grpStartProcess);
             Controls.Add(m_grpAddFiles);
             Controls.Add(m_btnClose);
             Controls.Add(txtLog);
             Controls.Add(progressBar);
             Name = "CreateProjectFromAudio";
-            Text = "Create Project From Audio";
             m_grpAddFiles.ResumeLayout(false);
             m_grpArrangeAudioFiles.ResumeLayout(false);
             m_grpStartProcess.ResumeLayout(false);
--- Obi/Obi/Dialogs/ImportAudioUsingWhisper.Designer.cs
+++ Obi/Obi/Dialogs/ImportAudioUsingWhisper.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImportAudioUsingWhisper));
             m_LogTxt = new System.Windows.Forms.TextBox();
             m_ProgressBar = new System.Windows.Forms.ProgressBar();
             m_btnCancel = new System.Windows.Forms.Button();
@@ -40,86 +41,56 @@
             // 
             // m_LogTxt
             // 
-            m_LogTxt.Location = new System.Drawing.Point(32, 259);
-            m_LogTxt.Multiline = true;
+            resources.ApplyResources(m_LogTxt, "m_LogTxt");
             m_LogTxt.Name = "m_LogTxt";
             m_LogTxt.ReadOnly = true;
-            m_LogTxt.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            m_LogTxt.Size = new System.Drawing.Size(911, 209);
-            m_LogTxt.TabIndex = 6;
             // 
             // m_ProgressBar
             // 
-            m_ProgressBar.AccessibleName = "Progress Bar";
-            m_ProgressBar.Location = new System.Drawing.Point(32, 145);
+            resources.ApplyResources(m_ProgressBar, "m_ProgressBar");
             m_ProgressBar.Name = "m_ProgressBar";
-            m_ProgressBar.Size = new System.Drawing.Size(911, 37);
-            m_ProgressBar.TabIndex = 5;
             // 
             // m_btnCancel
             // 
-            m_btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            m_btnCancel.Location = new System.Drawing.Point(491, 506);
+            resources.ApplyResources(m_btnCancel, "m_btnCancel");
             m_btnCancel.Name = "m_btnCancel";
-            m_btnCancel.Size = new System.Drawing.Size(168, 35);
-            m_btnCancel.TabIndex = 8;
-            m_btnCancel.Text = "&Cancel";
             m_btnCancel.UseVisualStyleBackColor = true;
             m_btnCancel.Click += m_btnCancel_Click;
             // 
             // label1
             // 
-            label1.AutoSize = true;
-            label1.Location = new System.Drawing.Point(32, 27);
+            resources.ApplyResources(label1, "label1");
             label1.Name = "label1";
-            label1.Size = new System.Drawing.Size(157, 20);
-            label1.TabIndex = 1;
-            label1.Text = "Select Whisper Model:";
             // 
             // m_ModelCb
             // 
-            m_ModelCb.AccessibleName = "Select Whisper Model";
+            resources.ApplyResources(m_ModelCb, "m_ModelCb");
             m_ModelCb.FormattingEnabled = true;
-            m_ModelCb.Location = new System.Drawing.Point(250, 24);
             m_ModelCb.Name = "m_ModelCb";
-            m_ModelCb.Size = new System.Drawing.Size(226, 28);
-            m_ModelCb.TabIndex = 2;
             // 
             // m_btnStart
             // 
-            m_btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            m_btnStart.Location = new System.Drawing.Point(168, 506);
+            resources.ApplyResources(m_btnStart, "m_btnStart");
             m_btnStart.Name = "m_btnStart";
-            m_btnStart.Size = new System.Drawing.Size(168, 35);
-            m_btnStart.TabIndex = 7;
-            m_btnStart.Text = "&Start";
             m_btnStart.UseVisualStyleBackColor = true;
             m_btnStart.Click += m_btnStart_Click;
             // 
             // lblBookLanguage
             // 
-            lblBookLanguage.AutoSize = true;
-            lblBookLanguage.Location = new System.Drawing.Point(74, 70);
+            resources.ApplyResources(lblBookLanguage, "lblBookLanguage");
             lblBookLanguage.Name = "lblBookLanguage";
-            lblBookLanguage.Size = new System.Drawing.Size(115, 20);
-            lblBookLanguage.TabIndex = 3;
-            lblBookLanguage.Text = "Book Language:";
             // 
             // m_BookLanguageCb
             // 
-            m_BookLanguageCb.AccessibleName = "Select Book Language";
+            resources.ApplyResources(m_BookLanguageCb, "m_BookLanguageCb");
             m_BookLanguageCb.FormattingEnabled = true;
-            m_BookLanguageCb.Location = new System.Drawing.Point(250, 70);
             m_BookLanguageCb.Name = "m_BookLanguageCb";
-            m_BookLanguageCb.Size = new System.Drawing.Size(226, 28);
-            m_BookLanguageCb.TabIndex = 4;
             // 
             // ImportAudioUsingWhisper
             // 
-            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            resources.ApplyResources(this, "$this");
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             CancelButton = m_btnCancel;
-            ClientSize = new System.Drawing.Size(965, 572);
             Controls.Add(m_BookLanguageCb);
             Controls.Add(lblBookLanguage);
             Controls.Add(m_btnStart);
@@ -131,7 +102,6 @@
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "ImportAudioUsingWhisper";
-            Text = "Import Audio Using Whisper";
             ResumeLayout(false);
             PerformLayout();
         }
